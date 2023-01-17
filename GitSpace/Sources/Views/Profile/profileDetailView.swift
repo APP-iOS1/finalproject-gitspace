30ea4685c44d7538c31768ac5b39390282e2e43c
Yeseul Chung <amy7806@gmail.com>
2023-01-18 00:10:24 +0900

    [Chore] profileDetailView 뷰 위치 및 프레임 완성
    TODO : Button Action Function 들 짜야함.(모달, 레이블 변경 등)

--- GitSpace/Sources/Views/Profile/profileDetailView.swift
+++ GitSpace/Sources/Views/Profile/profileDetailView.swift
@@ -9,6 +9,72 @@ import SwiftUI
 
 struct profileDetailView: View {
     var body: some View {
+        VStack(alignment: .leading, spacing: 10){
+            HStack{
+                Image(systemName: "person")
+                    .resizable()
+                    .scaledToFit()
+                VStack(alignment: .leading){
+                    Text("여기에 사람 이름이 들어갈거임.")
+                        .bold()
+                    Spacer()
+                        .frame(height: 20)
+                    Text("@여기에 사람 닉네임 들어감.")
+                }
+            }
+            .frame(height: 100)
+            Text("아임 브라질 랜덤 가이..")
+                .padding(.vertical, 10)
+            HStack{
+                Image(systemName: "mappin.and.ellipse")
+                Text("Brazil, South America")
+                    .bold()
+                    .foregroundColor(.gray)
+            }
+            HStack{
+                Image(systemName: "link")
+                Text("yeseul-programming.tistory.com")
+                    .bold()
+            }
+            HStack{
+                Image(systemName: "person")
+                Text("1924 followers · 1272 following")
+                    .bold()
+            }
+            HStack{
+                Button {
+                    
+                } label: {
+                    Text("+ Follow")
+                        .bold()
+                        .frame(minWidth: 130)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.gray)
+                        .cornerRadius(15)
+                }
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Knock")
+                        .bold()
+                        .frame(minWidth: 130)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.gray)
+                        .cornerRadius(15)
+                }
+            }
+            .padding(.vertical, 15)
+            Divider()
+                .frame(height: 2)
+                .overlay(.gray)
+            Spacer()
+        }
+        .padding()
+    }
+    func followButtonAction(){
         
     }
 }
