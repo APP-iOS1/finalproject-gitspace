687406152d765dbe071a0d8cc4280f568ae5041d
valseLee <sollleky72@gmail.com>
2023-01-17 17:46:32 +0900

    [init] #3 iOS 15.0 SwiftUI 프로젝트 init

---
+++ GitSpace/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  GitSpace
+//
+//  Created by 이승준 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		TabView {
+			NavigationView {
+					MainHomeView()
+			}
+			.tabItem {
+				Image(systemName: "house")
+			}
+			
+			NavigationView {
+				MainPenpalView()
+			}
+			.tabItem {
+				Image(systemName: "bubble.right")
+			}
+			
+			NavigationView {
+				MainProfileView()
+			}
+			.tabItem {
+//				Image(systemName: "person")
+				Image("그사람프사")
+			}
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
