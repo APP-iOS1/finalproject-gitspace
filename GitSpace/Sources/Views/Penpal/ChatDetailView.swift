b732737a2d7aee7c9978374aa72424db6b36e257
Taeyoung Won <hexter1994@gmail.com>
2023-01-18 00:16:13 +0900

    [Chore] #8 Message Field Section 구현, ScrollView로 전환

--- GitSpace/Sources/Views/Penpal/ChatDetailView.swift
+++ GitSpace/Sources/Views/Penpal/ChatDetailView.swift
@@ -8,96 +8,128 @@
 import SwiftUI
 
 struct ChatDetailView: View {
+    
+    @State private var messageField : String = ""
+    @State private var messageCells : [MessageCell] = [
+        MessageCell()
+    ]
+    
     var body: some View {
-        VStack {
-            Section {
-                Divider()
-                
-                AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(Circle())
-                        .frame(width : 100)
-                } placeholder: {
-                    Text("불러오는 중입니다...")
+        LazyVStack {
+            ScrollView {
+                Section {
+                    Divider()
+                    
+                    AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .frame(width : 100)
+                    } placeholder: {
+                        Text("불러오는 중입니다...")
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.top, 10)
+                    
+                    
+                    Text("Taeyoung Won")
+                        .bold()
+                        .padding(.vertical, 20)
+                    
+                    Text("wontaeyoung,")
+                    Text("starred 3 repos,")
+                    Text("Airbnb-swift Repository Owner")
                 }
-                .padding(.vertical, 10)
-                .padding(.top, 10)
                 
                 
-                Text("Taeyoung Won")
-                    .bold()
-                    .padding(.vertical, 20)
-                
-                Text("wontaeyoung,")
-                Text("starred 3 repos,")
-                Text("Airbnb-swift Repository Owner")
-            }
-            
-            
-            Section {
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: UIScreen.main.bounds.width - 60, height: 5)
-                    .foregroundColor(Color(.systemGray5))
-                    .padding(.vertical, 15)
-                
-                Text("오늘")
-                    .foregroundColor(Color(uiColor: .systemGray4))
-                    .padding(.bottom, 15)
-                
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 250, height: 80)
-                    .foregroundColor(Color(uiColor: .systemGray3))
-                    .overlay(alignment : .bottomTrailing) {
-                        Text("17:53")
-                            .font(.caption2)
-                            .foregroundColor(.gray)
-                            .offset(x: 35)
-                    }
-                    .offset(x: -40)
-                    .padding(.bottom, 20)
+                Section {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: UIScreen.main.bounds.width - 60, height: 5)
+                        .foregroundColor(Color(.systemGray5))
+                        .padding(.vertical, 15)
                     
+                    Text("오늘")
+                        .foregroundColor(Color(uiColor: .systemGray4))
+                        .padding(.bottom, 15)
                     
-                
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 250, height: 80)
-                    .foregroundColor(.yellow)
-                    .overlay(alignment : .bottomLeading) {
-                        VStack {
-                            Text("5분 전에 읽음")
-                                .offset(x: -15)
-                            Text("17:54")
-                                
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 250, height: 80)
+                        .foregroundColor(Color(uiColor: .systemGray3))
+                        .overlay(alignment : .bottomTrailing) {
+                            Text("17:53")
+                                .font(.caption2)
+                                .foregroundColor(.gray)
+                                .offset(x: 35)
                         }
-                        .font(.caption2)
-                        .foregroundColor(.gray)
-                        .offset(x: -50)
-                            
-                    }
-                    .offset(x: 40)
-                    .padding(.bottom, 20)
-                
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 250, height: 80)
-                    .foregroundColor(.yellow)
-                    .overlay(alignment : .bottomLeading) {
-                        Text("18:01")
+                        .offset(x: -40)
+                        .padding(.bottom, 20)
+                    
+                    
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 250, height: 80)
+                        .foregroundColor(.yellow)
+                        .overlay(alignment : .bottomLeading) {
+                            VStack {
+                                Text("5분 전에 읽음")
+                                    .offset(x: -15)
+                                Text("17:54")
+                                
+                            }
                             .font(.caption2)
                             .foregroundColor(.gray)
-                            .offset(x: -35)
-                    }
-                    .offset(x: 40)
-                    .padding(.bottom, 20)
+                            .offset(x: -50)
+                            
+                        }
+                        .offset(x: 40)
+                        .padding(.bottom, 20)
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 250, height: 80)
+                        .foregroundColor(.yellow)
+                        .overlay(alignment : .bottomLeading) {
+                            Text("18:01")
+                                .font(.caption2)
+                                .foregroundColor(.gray)
+                                .offset(x: -35)
+                        }
+                        .offset(x: 40)
+                        .padding(.bottom, 20)
+                    
+                }
+                
+                Spacer()
+                
                 
             }
-            
-            Spacer()
-            
-            
+            RoundedRectangle(cornerRadius: 25)
+                .frame(width: 320, height: 50)
+                .foregroundColor(Color(uiColor: .systemGray5))
+                .overlay {
+                    HStack {
+                        Button {} label: {
+                            Image(systemName: "photo")
+                        }
+                        
+                        Button {} label: {
+                            Text("</>")
+                        }
+                        
+                        TextField("쪽지 작성", text: $messageField)
+                        
+                        Button {} label: {
+                            Image(systemName: "arrowtriangle.right.fill")
+                        }
+                    }
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 20)
+                }
         }
     }
 }
+
+
 
 struct ChatDetailView_Previews: PreviewProvider {
     static var previews: some View {
