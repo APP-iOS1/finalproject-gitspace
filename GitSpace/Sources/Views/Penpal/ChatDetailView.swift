44e71ec45a3ddbf0bdf941cdeeca04fb1546861b
Taeyoung Won <hexter1994@gmail.com>
2023-01-17 22:52:39 +0900

    [Add] #8 ChatDetailView 추가

---
+++ GitSpace/Sources/Views/Penpal/ChatDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatDetailView.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct ChatDetailView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ChatDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatDetailView()
+    }
+}
