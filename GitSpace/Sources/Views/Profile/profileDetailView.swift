76738810e17a8f39f03bc7b50cce9bce5f08144a
Yeseul Chung <amy7806@gmail.com>
2023-01-17 22:52:37 +0900

    [ADD] profileDerailView 추가

---
+++ GitSpace/Sources/Views/Profile/profileDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  profileDetailView.swift
+//  GitSpace
+//
+//  Created by 정예슬 on 2023/01/17.
+//
+
+import SwiftUI
+
+struct profileDetailView: View {
+    var body: some View {
+        
+    }
+}
+
+struct profileDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        profileDetailView()
+    }
+}
