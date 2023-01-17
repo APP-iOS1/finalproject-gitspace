9560f3b0c08a5163b5a14e834bc3cd5f4b6a7c7b
Taeyoung Won <hexter1994@gmail.com>
2023-01-17 22:53:38 +0900

    [Chore] #8 UserInfo Section 구현

--- GitSpace/Sources/Views/Penpal/ChatDetailView.swift
+++ GitSpace/Sources/Views/Penpal/ChatDetailView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct ChatDetailView: View {
     var body: some View {
-        
+        VStack {
+            
+            Divider()
+            
+            AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width : 100)
+            } placeholder: {
+                Text("불러오는 중입니다...")
+            }
+            .padding(.vertical, 10)
+            .padding(.top, 10)
+
+            
+            Text("Taeyoung Won")
+                .bold()
+                .padding(.vertical, 20)
+            
+            Text("wontaeyoung,")
+            Text("starred 3 repos,")
+            Text("Airbnb-swift Repository Owner")
+            
+            
+        }
     }
 }
 
