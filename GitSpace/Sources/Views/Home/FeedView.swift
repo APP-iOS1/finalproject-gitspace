c03912e18bfb4a6ff15d624684e96c8a75782556
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-01-18 10:36:55 +0900

    [Add]: FeedView에 NavigationLink 및 ContextMenu 추가 #5

--- GitSpace/Sources/Views/Home/FeedView.swift
+++ GitSpace/Sources/Views/Home/FeedView.swift
@@ -26,26 +26,72 @@ struct FeedView: View {
                     }
                 }
                 .foregroundColor(.primary)
+                .contextMenu(menuItems: {
+                    
+                    Button(role: .none) {
+                        // action
+                    } label: {
+                        Text("Penpal 보내기")
+                        Image(systemName: "paperplane")
+                            .renderingMode(.original)
+                    }
+                    
+                    Button(role: .destructive) {
+                        // action
+                    } label: {
+                        Text("Unfollow")
+                        Image(systemName: "person.badge.minus")
+                            .renderingMode(.original)
+                    }
+                })
+            
 
                 VStack(alignment: .leading, spacing: 10) {
                     HStack {
-                        Text("User \(userNumber)")
-                            .bold()
+                        NavigationLink {
+                            Text("특정유저의 프로필 입니다.")
+                        } label: {
+                            Text("User \(userNumber)")
+                                .bold()
+                        }
+                        .foregroundColor(.primary)
+                        
                         Spacer()
+                        
                         Text("\(userNumber) 시간 전")
                             .font(.footnote)
-                            .foregroundColor(.secondary)
+                            .foregroundColor(Color(.systemGray))
                     }
-                    .padding(.trailing)
                     // FIXME: - Repository Detail View와 연결하기
                     NavigationLink {
                         Text("특정 레포지토리의 디테일 뷰 입니다.")
                     } label: {
                         Text("User \(userNumber) starred **APPSCHOOL1-REPO/finalproject-gitspace**")
                     }
+                    .foregroundColor(.primary)
+                    .contextMenu(menuItems: {
+                        // 만약 사용자도 팔로우중인 레포지토리라면 unstar 버튼을 보여줌
+                        Button(role: .destructive) {
+                            // action
+                        } label: {
+                            Text("Unstar")
+                            Image(systemName: "star.slash")
+                                .renderingMode(.original)
+                        }
+                        
+                        // 사용자가 팔로우중인 레포지토리가 아니라면 star버튼을 보여줌
+                        Button(role: .none) {
+                            // action
+                        } label: {
+                            Text("Star")
+                                .foregroundColor(.red)
+                            Image(systemName: "star")
+                                .renderingMode(.original)
+                        }
+                    })
                 } // vstack
-                .foregroundColor(.primary)
             } // hstack
+            .padding()
     } // body
 
 }
