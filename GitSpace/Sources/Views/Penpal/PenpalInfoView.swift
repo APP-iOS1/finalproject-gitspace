c4b12bcc83db973d4dfddc0e52a4c9155c3b6184
Taeyoung Won <hexter1994@gmail.com>
2023-01-18 10:25:45 +0900

    [Add] #8 PenpalInfoView 파일 생성

---
+++ GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  PenpalInfoView.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct PenpalInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PenpalInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PenpalInfoView()
+    }
+}
