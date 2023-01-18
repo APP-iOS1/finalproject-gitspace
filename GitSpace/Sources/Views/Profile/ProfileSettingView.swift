e1bc709f20247d64b130eebb825e9a111515b3c5
Yeseul Chung <amy7806@gmail.com>
2023-01-18 15:11:06 +0900

    [ADD] #12 ProfileSettingView 추가

---
+++ GitSpace/Sources/Views/Profile/ProfileSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileSettingView.swift
+//  GitSpace
+//
+//  Created by 정예슬 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct ProfileSettingView: View {
+    var body: some View {
+        Text("Profile Setting View")
+    }
+}
+
+struct ProfileSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSettingView()
+    }
+}
