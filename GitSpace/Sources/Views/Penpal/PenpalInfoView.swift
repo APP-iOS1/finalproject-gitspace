260c238f173ceeb06a767ca8f32f4546539e8143
Taeyoung Won <hexter1994@gmail.com>
2023-01-18 15:13:59 +0900

    [Feat] #8 상대방 차단, 대화 삭제 로직 구현

--- GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
+++ GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
@@ -14,7 +14,8 @@ struct PenpalInfoView: View {
     @State private var showingBlockAlert : Bool = false
     @State private var showingUnblockAlert : Bool = false
     @State private var showingDeleteChatAlert : Bool = false
-    @Binding var naviIsActive: Bool
+    @State private var showingBlockMessage : Bool = false
+    @State private var showingDeleteMessage : Bool = false
     
     var body: some View {
         VStack {
@@ -63,28 +64,63 @@ struct PenpalInfoView: View {
             } label: {
                 Text("@wontaeyoung \(isBlocked ? "차단 해제하기" : "차단하기")")
             }
+            .padding(.vertical, 20)
 
             Button(role: .destructive) {
                 showingDeleteChatAlert.toggle()
             } label: {
                 Text("대화 삭제")
             }
-            
+
             Spacer()
         }
+        .overlay(alignment: .bottom) {
+            if showingBlockMessage {
+                Text(isBlocked ? "차단되었습니다" : "차단이 해제되었습니다")
+                    .font(.largeTitle.bold())
+            } else if showingDeleteMessage {
+                Text("대화가 삭제되었습니다")
+                    .font(.largeTitle.bold())
+            }
+        }
         .padding(.horizontal, 20)
-        .alert("차단하기", isPresented: $showingBlockAlert) {
-            Button("차단", role: .destructive) {
-                
+        .alert("차단", isPresented: $showingBlockAlert) {
+            Button("차단하기", role: .destructive) {
+                isBlocked.toggle()
+                showingBlockMessage = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showingBlockMessage = false
+                }
             }
         } message: {
-            Text("메세지를 삭제하면 상대방과 나 모두 이 메세지를 볼 수 없습니다. 삭제하시겠습니까?")
+            Text("상대방을 차단하면 상대방이 보내는 메세지를 더 이상 볼 수 없습니다.\n차단하시겠습니까?")
+        }
+        .alert("차단 해제", isPresented: $showingUnblockAlert) {
+            Button("해제하기", role: .destructive) {
+                isBlocked.toggle()
+                showingBlockMessage = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showingBlockMessage = false
+                }
+            }
+        } message: {
+            Text("차단을 해제하면 상대방이 보내는 메세지를 다시 받을 수 있습니다.\n차단 해제하시겠습니까?")
+        }
+        .alert("대화 삭제", isPresented: $showingDeleteChatAlert) {
+            Button("삭제하기", role: .destructive) {
+                showingDeleteMessage = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    showingDeleteMessage = false
+                }
+            }
+        } message: {
+            Text("대화를 삭제하면 지금까지 한 대화가 모두 사라지며 복구할 수 없습니다.\n삭제하시겠습니까?")
         }
     }
 }
 
 struct PenpalInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        PenpalInfoView(naviIsActive: .constant(false))
+        PenpalInfoView()
     }
 }
