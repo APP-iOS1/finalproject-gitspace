2b99d370c722ea92b824bd564648b92a40acd362
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-18 11:10:41 +0900

    [Add] #11 Add MyKnockSetting View
    Add MyKnockSetting View

---
+++ GitSpace/Sources/Views/Penpal/MyKnockBox/MyKnockSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  MyKnockSettingView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct MyKnockSettingView: View {
+    var body: some View {
+        Text("까 꿍 !")
+    }
+}
+
+struct MyKnockSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyKnockSettingView()
+    }
+}
