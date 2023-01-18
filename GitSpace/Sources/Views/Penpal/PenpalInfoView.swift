1ee70a9f2330284c180763c50b18cd3dd1387819
Taeyoung Won <hexter1994@gmail.com>
2023-01-18 11:32:18 +0900

    [Chore] #8 UserInfo Section, Alarm Toggle Section 구현

--- GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
+++ GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct PenpalInfoView: View {
+    
+    @State private var alarmStop : Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 80)
+            } placeholder: {
+                ProgressView()
+            }
+            Text("Taeyoung Won")
+                .bold()
+                .padding(.horizontal, -8)
+            
+            Text("@wontaeyoung")
+                .foregroundColor(Color(uiColor: .systemGray))
+            
+            Divider()
+                .padding(.horizontal, -10)
+            
+            VStack(alignment: .leading) {
+                Text("알림")
+                    .font(.title3)
+                    .bold()
+                
+                Toggle("알림 일시중지", isOn: $alarmStop)
+            }
+            
+            Divider()
+                .padding(.horizontal, -10)
+            
+            
+            
+            Spacer()
+        }
+        .padding(.horizontal, 20)
     }
 }
 
