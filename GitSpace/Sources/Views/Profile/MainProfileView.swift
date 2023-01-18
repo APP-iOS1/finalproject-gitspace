9ab76101c080d71784b1b5ab08356c8423a9c61f
Yeseul Chung <amy7806@gmail.com>
2023-01-18 15:13:59 +0900

    [Feat] #12 Main Profie Toolbar 삽입

--- GitSpace/Sources/Views/Profile/MainProfileView.swift
+++ GitSpace/Sources/Views/Profile/MainProfileView.swift
@@ -1,20 +1,37 @@
-//
-//  MainProfileView.swift
-//  GitSpace
-//
-//  Created by 이승준 on 2023/01/17.
-//
-
 import SwiftUI
+
+//TODO: - ToolBar Item들 네비게이션으로 다른 뷰 연결
 
 struct MainProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){ //MARK: - 처음부터 끝까지 모든 요소들을 아우르는 stack.
+            profileSectionView()
+            Spacer()
+        }
+        .toolbar{
+            ToolbarItemGroup(placement: .navigationBarTrailing){
+                NavigationLink {
+                    ProfileSettingView()
+                } label: {
+                    Image(systemName: "gearshape")
+                }
+                NavigationLink {
+                    ProfileSettingView() //추후 Send Knock View로 변경해야함
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+
+                
+            }
+        }
+        
     }
 }
 
 struct MainProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        MainProfileView()
+        NavigationView{
+            MainProfileView()
+        }
     }
 }
