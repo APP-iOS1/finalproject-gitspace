1120d86f777e6c49e17e4e4ba0346f01b09eb102
Taeyoung Won <hexter1994@gmail.com>
2023-01-18 11:49:43 +0900

    [Chore] #8 PenpalInfoView Section 그룹 분리

--- GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
+++ GitSpace/Sources/Views/Penpal/PenpalInfoView.swift
@@ -9,40 +9,44 @@ import SwiftUI
 
 struct PenpalInfoView: View {
     
-    @State private var alarmStop : Bool = false
+    @State private var notificationStop : Bool = false
     
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .frame(width: 80)
-            } placeholder: {
-                ProgressView()
-            }
-            Text("Taeyoung Won")
-                .bold()
-                .padding(.horizontal, -8)
-            
-            Text("@wontaeyoung")
-                .foregroundColor(Color(uiColor: .systemGray))
-            
-            Divider()
-                .padding(.horizontal, -10)
-            
-            VStack(alignment: .leading) {
-                Text("알림")
-                    .font(.title3)
+            Section{
+                AsyncImage(url: URL(string: "https://avatars.githubusercontent.com/u/45925685?v=4")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 80)
+                } placeholder: {
+                    ProgressView()
+                }
+                Text("Taeyoung Won")
                     .bold()
+                    .padding(.horizontal, -8)
                 
-                Toggle("알림 일시중지", isOn: $alarmStop)
+                Text("@wontaeyoung")
+                    .foregroundColor(Color(uiColor: .systemGray))
+                
+                Divider()
+                    .padding(.horizontal, -10)
             }
             
-            Divider()
-                .padding(.horizontal, -10)
-            
+            Section {
+                
+                VStack(alignment: .leading) {
+                    Text("알림")
+                        .font(.title3)
+                        .bold()
+                    
+                    Toggle("알림 일시중지", isOn: $notificationStop)
+                }
+                
+                Divider()
+                    .padding(.horizontal, -10)
+            }
             
             
             Spacer()
