7ee843f8ec2b5ba74a857babc29dcaa20aeaa44b
valseLee <sollleky72@gmail.com>
2023-01-18 15:02:19 +0900

    [Add] 노크 모달 뷰와 리스트 뷰 파일 생성

---
+++ GitSpace/Sources/Views/Penpal/PenpalList/PenpalListView.swift
@@ -0,0 +1,20 @@
+//
+//  PenpalListView.swift
+//  GitSpace
+//
+//  Created by 이승준 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct PenpalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PenpalListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PenpalListView()
+    }
+}
