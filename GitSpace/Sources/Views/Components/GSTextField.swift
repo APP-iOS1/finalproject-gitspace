b0a2c0a6ddf36b1e7fe3ef11df66718c1c2ab556
valseLee <sollleky72@gmail.com>
2023-01-20 01:34:55 +0900

    [Add] GSInputField Example Code 파일 생성

---
+++ GitSpace/Sources/Views/Components/GSTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GSTextField.swift
+//  GitSpace
+//
+//  Created by 이승준 on 2023/01/20.
+//
+
+import SwiftUI
+
+struct GSTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GSTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        GSTextField()
+    }
+}
