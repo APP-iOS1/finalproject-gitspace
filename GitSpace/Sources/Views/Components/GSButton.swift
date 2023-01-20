87ae3321a62a758a78a2ff6b95776d5a68759f95
valseLee <sollleky72@gmail.com>
2023-01-20 13:09:50 +0900

    [Add] GSButton 파일 추가

---
+++ GitSpace/Sources/Views/Components/GSButton.swift
@@ -0,0 +1,20 @@
+//
+//  GSButton.swift
+//  GitSpace
+//
+//  Created by 이승준 on 2023/01/20.
+//
+
+import SwiftUI
+
+struct GSButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GSButton_Previews: PreviewProvider {
+    static var previews: some View {
+        GSButton()
+    }
+}
