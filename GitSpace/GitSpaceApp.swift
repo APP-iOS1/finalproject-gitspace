204ede75a13bd6e70562274e01440cf6264a4826
valseLee <sollleky72@gmail.com>
2023-01-22 23:05:00 +0900

    [Update] #33 FCM 기반 코드만 우선 작성, 개발자 계정 생성 후 추가 작업 예정

--- GitSpace/GitSpaceApp.swift
+++ GitSpace/GitSpaceApp.swift
@@ -7,14 +7,69 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        
+		
+		// 원격 알림 등록
+		if #available(iOS 10.0, *) {
+			// For iOS 10 display notification (sent via APNS)
+			UNUserNotificationCenter.current().delegate = self
+			
+			let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+			UNUserNotificationCenter.current().requestAuthorization(
+				options: authOptions,
+				completionHandler: { _, _ in }
+			)
+		} else {
+			let settings: UIUserNotificationSettings =
+			UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+			application.registerUserNotificationSettings(settings)
+		}
+		
+		application.registerForRemoteNotifications()
+		
+		// 메세징 델리겟
+		Messaging.messaging().delegate = self
+		
+		// 푸시 포그라운드 설정
+		UNUserNotificationCenter.current().delegate = self
         return true
     }
+}
+
+// FCM의 메시지 토큰 관리
+extension AppDelegate: MessagingDelegate {
+	// fcm 토큰이 등록 되었을 때
+	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+		Messaging.messaging().apnsToken = deviceToken
+	}
+}
+
+// 알람 등록 후 반응 로직
+extension AppDelegate: UNUserNotificationCenterDelegate {
+	func userNotificationCenter(_ center: UNUserNotificationCenter,
+								willPresent notification: UNNotification,
+								withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+		
+		let userInfo = notification.request.content.userInfo
+		
+		print("willPresent: userInfo: ", userInfo)
+		
+		completionHandler([.banner, .sound, .badge])
+	}
+	
+	// 푸시메세지를 받았을 때
+	func userNotificationCenter(_ center: UNUserNotificationCenter,
+								didReceive response: UNNotificationResponse,
+								withCompletionHandler completionHandler: @escaping () -> Void) {
+		let userInfo = response.notification.request.content.userInfo
+		print("didReceive: userInfo: ", userInfo)
+		completionHandler()
+	}
 }
 
 @main
