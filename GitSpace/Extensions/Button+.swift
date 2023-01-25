b57fcff02b38c05d3f6e21cf8cdb3902b8a76e36
valseLee <sollleky72@gmail.com>
2023-01-25 17:38:39 +0900

    [Feat]#24 각 버튼의 스타일에 따라 다크모드, 라이트모드 분기 처리, tag 버튼은 isEditing 상태 등을 따로 받아서 진행 예정

--- GitSpace/Extensions/Button+.swift
+++ GitSpace/Extensions/Button+.swift
@@ -8,38 +8,73 @@
 import SwiftUI
 
 extension Button {
-	
-	struct GSButtonModifier: ViewModifier {
+	public struct GSButtonColorSchemeModifier: ViewModifier {
+		let style: GSButton.GSButtonStyle
 		@Environment(\.colorScheme) var colorScheme
 		
-		func body(content: Content) -> some View {
+		public func body(content: Content) -> some View {
 			switch colorScheme {
-			case .light:
-				content
-					.buttonBorderShape(.capsule)
-					.buttonStyle(.borderedProminent)
-					.tint(.gsGreenPrimary)
-					.shadow(color: .gsYellowPrimary.opacity(0.21),
-							radius: 9,
-							x: 0,
-							y: 9)
-			case .dark:
-				content
-					.buttonBorderShape(.capsule)
-					.buttonStyle(.borderedProminent)
-					.tint(.gsYellowPrimary)
-					.shadow(color: .gsGreenPrimary.opacity(0.29),
-							radius: 36,
-							x: 0,
-							y: 14)
-				
-			@unknown default:
+
+			// MARK: - LIGHT MODE
+			case .light: // light Mode
+				switch style { // Button Style
+				case .primary:
+					content
+				case .secondary(_, let isDisabled):
+					content
+						.buttonBorderShape(.capsule)
+						.buttonStyle(.borderedProminent)
+						.tint(isDisabled ? .gsLightGray1 : .gsGreenPrimary)
+						.shadow(color: .gsYellowPrimary.opacity(0.21),
+								radius: 9,
+								x: 0,
+								y: 9)
+				case .tag:
+					content
+						.buttonBorderShape(.capsule)
+						.buttonStyle(.borderedProminent)
+				case .plainText,
+						.homeTab:
+					content
+						.foregroundColor(.primary)
+				}
+			
+			// MARK: - DARK MODE
+			case .dark: // dark mode
+				switch style { // Button Style
+				case .primary,
+						.secondary:
+					content
+						.buttonBorderShape(.capsule)
+						.buttonStyle(.borderedProminent)
+						.tint(.gsYellowPrimary)
+						.shadow(color: .gsGreenPrimary.opacity(0.29),
+								radius: 36,
+								x: 0,
+								y: 14)
+				case .tag:
+					content
+						.buttonBorderShape(.capsule)
+						.buttonStyle(.borderedProminent)
+				case .plainText(let isDestructive):
+					content
+						.foregroundColor(isDestructive ? .gsRed : .white)
+				case .homeTab:
+					content
+						.colorInvert()
+				}
+			@unknown default: // neither Light nor Dark
 				content
 			}
 		}
+		
+		fileprivate init(style: GSButton.GSButtonStyle) {
+			self.style = style
+		}
 	}
 	
-	public func buttonColorSchemeModifier() -> some View {
-		modifier(GSButtonModifier())
+	/// - Important: ONLY USED IN DESIGN SYSTEM CODE
+	public func buttonColorSchemeModifier(style: GSButton.GSButtonStyle) -> some View {
+		modifier(GSButtonColorSchemeModifier(style: style))
 	}
 }
