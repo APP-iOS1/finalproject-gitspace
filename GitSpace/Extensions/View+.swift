67df5ac3740d9ca4db9b7b47e95972e6a42ae608
valseLee <sollleky72@gmail.com>
2023-01-25 17:39:16 +0900

    [Feat]#24 각 케이스의 연관값 상태에 따라 foreground 컬러 대응

--- GitSpace/Extensions/View+.swift
+++ GitSpace/Extensions/View+.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+// TODO: 어째서 뷰가 Button Modifier를 가져야 하는가..? 아니면 이걸 기똥차게 분리할 수 있나..?
 extension View {
-	public func buttonLabelSizeModifier(buttonLabelStyle: ButtonLabelType) -> some View {
-		modifier(ButtonLabelSizeModifier(buttonLabel: buttonLabelStyle))
-	}	
+	public func buttonLabelLayoutModifier(buttonLabelStyle: ButtonLabelType) -> some View {
+		modifier(ButtonLabelLayoutModifier(buttonLabel: buttonLabelStyle))
+	}
 }
 
-public struct ButtonLabelSizeModifier: ViewModifier {
+public struct ButtonLabelLayoutModifier: ViewModifier {
 	public let buttonLabel: ButtonLabelType
 	
 	public func body(content: Content) -> some View {
@@ -24,24 +25,36 @@ public struct ButtonLabelSizeModifier: ViewModifier {
 				.padding(.vertical, 18)
 				.frame(minWidth: 150)
 				.foregroundColor(.black)
-		case .secondary:
+		case .secondary(let isDisabled):
 			content
 				.padding(.horizontal, 23)
 				.padding(.vertical, 13)
 				.frame(minWidth: 80)
-				.foregroundColor(.black)
+				.foregroundColor(isDisabled ? .white : .black)
 		case .tag:
 			content
 				.padding(.horizontal, 12)
 				.padding(.vertical, 9)
 				.frame(minWidth: 62)
 				.foregroundColor(.black)
+		case .homeTab:
+			content
+				.foregroundColor(Color.primary)
+		case .plainText(let isDestructive):
+			content
+				.foregroundColor(isDestructive ? .gsRed : Color.primary)
 		}
+	}
+	
+	fileprivate init(buttonLabel: ButtonLabelType) {
+		self.buttonLabel = buttonLabel
 	}
 }
 
 public enum ButtonLabelType {
 	case primary
-	case secondary
+	case secondary(isDisabled: Bool)
 	case tag
+	case homeTab
+	case plainText(isDestructive: Bool)
 }
