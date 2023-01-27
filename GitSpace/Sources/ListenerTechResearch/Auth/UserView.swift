eaf9d676484309d1a00349d3c4ff490283309e7c
Yeseul Chung <amy7806@gmail.com>
2023-01-28 01:19:01 +0900

    [Chore] #36 UserView에서 ChatDetailView로 넘어가는 코드 추가

--- GitSpace/Sources/ListenerTechResearch/Auth/UserView.swift
+++ GitSpace/Sources/ListenerTechResearch/Auth/UserView.swift
@@ -13,29 +13,59 @@ import SwiftUI
 
 struct UserView: View {
     
-    @EnvironmentObject var userStore : UserStore
-    @EnvironmentObject var chatStore : ChatStore
+    @EnvironmentObject var userStore: UserStore
+    @EnvironmentObject var chatStore: ChatStore
+    @State var showZStack: Bool = false
+    @State var chatPersonID: String = ""
     
     var body: some View {
-        
-        ScrollView {
-            ForEach(userStore.users) { user in
-                NavigationLink {
-                    ChatDetailView(chat : chatStore.)
-                } label: {
-                    <#code#>
+        ZStack{
+            ScrollView {
+                ForEach(userStore.users) { user in
+                    Button {
+                        Task{
+                            chatPersonID = user.id
+                            print(chatStore.targetChat ?? "no target")
+                            chatStore.requestChatFromUserList(userIDs: [Utility.loginUserID, chatPersonID])
+                            print(chatStore.targetChat ?? "no target 2")
+                            showZStack = true
+                        }
+                    } label: {
+                        HStack{
+                            Image(systemName: "person")
+                            Text(user.name)
+                        }
+                    }
                 }
             }
+            if showZStack {
+                VStack{
+                    NavigationLink {
+                        //???: -targetChat is availiable 이거 왜 3번이나 출력 됨?
+                        if let targetChat = chatStore.targetChat {
+                            let _ = print("targetChat is availiable")
+                            ChatDetailView(chat: targetChat, isFromUserList: true)
+                        }
+                    } label: {
+                        Text("채팅 ㄱㄱ?")
+                            .modifier(ButtonModifier())
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.black.opacity(0.4))
+            }
         }
-        .task
-        
-        
-
+        .task {
+            userStore.fetchUsers()
+        }
     }
 }
+
 
 struct UserView_Previews: PreviewProvider {
     static var previews: some View {
         UserView()
+            .environmentObject(UserStore())
+            .environmentObject(ChatStore())
     }
 }
