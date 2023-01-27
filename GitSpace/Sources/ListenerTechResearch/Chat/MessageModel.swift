7b66247cd33f8ecd38f433b985ca8de70ea29d6d
Taeyoung Won <hexter1994@gmail.com>
2023-01-27 15:22:28 +0900

    [Add] #36 Message Model 추가

---
+++ GitSpace/Sources/ListenerTechResearch/Chat/MessageModel.swift
@@ -0,0 +1,26 @@
+//
+//  ChatCellModel.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/01/27.
+//
+
+import Foundation
+
+struct Message : Identifiable {
+    let id : String
+    let userID : String
+    let content : String
+    let date : Double
+    
+    var stringDate : String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let dateAt = Date(timeIntervalSince1970: date)
+        
+        return dateFormatter.string(from: dateAt)
+    }
+}
