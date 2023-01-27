429586d6bc31ce49b02e0ff86508790985076f06
Yeseul Chung <amy7806@gmail.com>
2023-01-28 01:17:43 +0900

    [Chore] #36 if let 수정
    해당 컬렉션의 하위 문서가 존재하지 않을 때, if let snapshot이 nil로 빠지지 않아서 조건을 documents count가 0인지 확인하는 코드로 수정.

--- GitSpace/Sources/ListenerTechResearch/Chat/ChatViewModel.swift
+++ GitSpace/Sources/ListenerTechResearch/Chat/ChatViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import FirebaseFirestore
 
 class ChatStore: ObservableObject {
-    @Published var targetChat : Chat
+    @Published var targetChat : Chat?
     @Published var chats: [Chat]
     
     let db = Firestore.firestore()
@@ -86,41 +86,43 @@ class ChatStore: ObservableObject {
                 
                 // MARK: -Logic : userIDs에 해당하는 (내 ID와 상대방 ID로 만들어진 채팅방이 이미 존재하면) 해당 채팅방을 return, 없으면 신규 채팅방을 add하고 해당 채팅방을 return.
                 if let snapshot {
-                    for document in snapshot.documents {
-                        let id: String = document.documentID
-                        let docData = document.data()
-                        let date = docData["date"] as? Double ?? 0.0
-                        let userIDList = docData["userIDs"] as? [String] ?? []
-                        let lastDate = docData["lastDate"] as? Double ?? 0.0
-                        let lastContent = docData["lastContent"] as? String ?? ""
-                        
-                        let userIDs : (String, String)
-                        
-                        // DB에 Array로 저장한 userIDs를 Tuple로 변환
-                        // 할당에 성공한 경우에만 Chat 구조체 추가
-                        if let open = userIDList.first, let join = userIDList.last {
-                            userIDs = (open, join)
-                            let chat = Chat(id: id,
-                                            date: date,
-                                            userIDs: userIDs,
-                                            lastDate: lastDate,
-                                            lastContent: lastContent)
-                            self.targetChat = chat
+                    if snapshot.documents.isEmpty {
+                        print("documents is Empty")
+                        let date = Date().timeIntervalSince1970
+                        if let open = userIDs.first, let join = userIDs.last {
+                            
+                            let newChat = Chat(id: UUID().uuidString,
+                                               date: date,
+                                               userIDs: (open, join),
+                                               lastDate: date,
+                                               lastContent: "")
+                            self.addChat(newChat)
+                            self.targetChat = newChat
+                        }
+                    } else {
+                        for document in snapshot.documents {
+                            let id: String = document.documentID
+                            let docData = document.data()
+                            let date = docData["date"] as? Double ?? 0.0
+                            let userIDList = docData["userIDs"] as? [String] ?? []
+                            let lastDate = docData["lastDate"] as? Double ?? 0.0
+                            let lastContent = docData["lastContent"] as? String ?? ""
+                            
+                            let userIDs : (String, String)
+                            
+                            // DB에 Array로 저장한 userIDs를 Tuple로 변환
+                            // 할당에 성공한 경우에만 Chat 구조체 추가
+                            if let open = userIDList.first, let join = userIDList.last {
+                                userIDs = (open, join)
+                                let chat = Chat(id: id,
+                                                date: date,
+                                                userIDs: userIDs,
+                                                lastDate: lastDate,
+                                                lastContent: lastContent)
+                                self.targetChat = chat
+                            }
                         }
                     }
-                } else {
-                    
-                    let date = Date().timeIntervalSince1970
-                    if let open = userIDs.first, let join = userIDs.last {
-                        let newChat = Chat(id: UUID().uuidString,
-                                           date: date,
-                                           userIDs: (open, join),
-                                           lastDate: date,
-                                           lastContent: "")
-                        self.addChat(newChat)
-                        self.targetChat = newChat
-                    }
-                    
                 }
             }
     }
