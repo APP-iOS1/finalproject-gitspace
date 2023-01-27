0b34050ce39690cd494e81c7ce510fc90bdede99
Taeyoung Won <hexter1994@gmail.com>
2023-01-27 18:59:43 +0900

    [Feat] #36 User List에서 연결되는 단일 채팅방 요청 함수 추가

--- GitSpace/Sources/ListenerTechResearch/Chat/ChatViewModel.swift
+++ GitSpace/Sources/ListenerTechResearch/Chat/ChatViewModel.swift
@@ -9,16 +9,17 @@ import Foundation
 import FirebaseFirestore
 
 class ChatStore: ObservableObject {
+    @Published var targetChat : Chat
     @Published var chats: [Chat]
     
-    let database = Firestore.firestore()
+    let db = Firestore.firestore()
     
     init() {
         chats = []
     }
     
     func fetchChats() {
-        database.collection("Chat").order(by: "lastDate", descending: true)
+        db.collection("Chat").order(by: "lastDate", descending: true)
             .getDocuments { (snapshot, error) in
                 self.chats.removeAll()
                 
@@ -51,7 +52,7 @@ class ChatStore: ObservableObject {
     
     // MARK: -Chat CRUD
     func addChat(_ chat: Chat) {
-        database.collection("Chat")
+        db.collection("Chat")
             .document(chat.id)
             .setData(["id" : chat.id,
                       "date" : chat.date,
@@ -62,7 +63,7 @@ class ChatStore: ObservableObject {
     }
     
     func updateChat(_ chat: Chat) {
-        database.collection("Chat")
+        db.collection("Chat")
             .document(chat.id)
             .updateData(["lastDate" : chat.lastDate,
                          "lastContent" : chat.lastContent])
@@ -70,9 +71,57 @@ class ChatStore: ObservableObject {
     }
     
     func removeChat(_ chat: Chat) {
-        database.collection("Chat")
+        db.collection("Chat")
             .document(chat.id).delete()
         
         fetchChats()
+    }
+    
+    // MARK: -Method : 유저 리스트를 통해서 채팅방을 Request해서 채팅방 객체를 Published 변수에 할당하는 함수
+    func requestChatFromUserList(userIDs : [String]) {
+        
+        db.collection("Chat")
+            .whereField("userIDs", arrayContainsAny: userIDs)
+            .getDocuments { (snapshot, error) in
+                
+                // MARK: -Logic : userIDs에 해당하는 (내 ID와 상대방 ID로 만들어진 채팅방이 이미 존재하면) 해당 채팅방을 return, 없으면 신규 채팅방을 add하고 해당 채팅방을 return.
+                if let snapshot {
+                    for document in snapshot.documents {
+                        let id: String = document.documentID
+                        let docData = document.data()
+                        let date = docData["date"] as? Double ?? 0.0
+                        let userIDList = docData["userIDs"] as? [String] ?? []
+                        let lastDate = docData["lastDate"] as? Double ?? 0.0
+                        let lastContent = docData["lastContent"] as? String ?? ""
+                        
+                        let userIDs : (String, String)
+                        
+                        // DB에 Array로 저장한 userIDs를 Tuple로 변환
+                        // 할당에 성공한 경우에만 Chat 구조체 추가
+                        if let open = userIDList.first, let join = userIDList.last {
+                            userIDs = (open, join)
+                            let chat = Chat(id: id,
+                                            date: date,
+                                            userIDs: userIDs,
+                                            lastDate: lastDate,
+                                            lastContent: lastContent)
+                            self.targetChat = chat
+                        }
+                    }
+                } else {
+                    
+                    let date = Date().timeIntervalSince1970
+                    if let open = userIDs.first, let join = userIDs.last {
+                        let newChat = Chat(id: UUID().uuidString,
+                                           date: date,
+                                           userIDs: (open, join),
+                                           lastDate: date,
+                                           lastContent: "")
+                        self.addChat(newChat)
+                        self.targetChat = newChat
+                    }
+                    
+                }
+            }
     }
 }
