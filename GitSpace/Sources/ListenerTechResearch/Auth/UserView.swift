bc36775ff0f8507b81a847af60070cbcd806a43d
Taeyoung Won <hexter1994@gmail.com>
2023-01-27 23:39:17 +0900

    [Chore] #36 chatDetail View 인자 입력

--- GitSpace/Sources/ListenerTechResearch/Auth/UserView.swift
+++ GitSpace/Sources/ListenerTechResearch/Auth/UserView.swift
@@ -5,6 +5,10 @@
 //  Created by 원태영 on 2023/01/27.
 //
 
+// TODO: 이거 얘기해야 됨
+/// 1. userList에서 타겟 채팅방 체크를 언제할지. 이거 ChatDetailView에서 Chat을 옵셔널로 할지도 고려!!
+///
+
 import SwiftUI
 
 struct UserView: View {
@@ -17,7 +21,7 @@ struct UserView: View {
         ScrollView {
             ForEach(userStore.users) { user in
                 NavigationLink {
-                    ChatDetailView()
+                    ChatDetailView(chat : chatStore.)
                 } label: {
                     <#code#>
                 }
