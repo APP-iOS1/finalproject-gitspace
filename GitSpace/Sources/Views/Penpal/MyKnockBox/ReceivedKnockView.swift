b350da28238a629f8525fb396459b0988abe0046
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 15:23:10 +0900

    [Add] #47 ReceivedKnockView

---
+++ GitSpace/Sources/Views/Penpal/MyKnockBox/ReceivedKnockView.swift
@@ -0,0 +1,20 @@
+//
+//  ReceivedKnockView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct ReceivedKnockView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct ReceivedKnockView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceivedKnockView()
+    }
+}
