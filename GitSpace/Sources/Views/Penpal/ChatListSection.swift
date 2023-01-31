4fa7b6cc7e4c859da04d145799ff16db22247665
Yeseul Chung <amy7806@gmail.com>
2023-02-01 01:01:06 +0900

    [Move] ListCellLabel 구조체를 ChatView에서 ChatListSection으로 이동

--- GitSpace/Sources/Views/Penpal/ChatListSection.swift
+++ GitSpace/Sources/Views/Penpal/ChatListSection.swift
@@ -1,10 +1,3 @@
-//
-//  ChatListSection.swift
-//  GitSpace
-//
-//  Created by 원태영 on 2023/01/31.
-//
-
 import SwiftUI
 
 struct ChatListSection: View {
@@ -16,7 +9,6 @@ struct ChatListSection: View {
         ScrollView {
             // 채팅방 목록 리스트
             ForEach(chatStore.chats) { chat in
-                
                 NavigationLink {
                     ChatDetailView(chat: chat)
                 } label: {
@@ -25,8 +17,70 @@ struct ChatListSection: View {
                 }
             }
         }
-        .onAppear{
+        .task{
             chatStore.fetchChats()
+        }
+    }
+}
+
+struct ListCellLabel : View {
+    
+    let chat : Chat
+    @EnvironmentObject var userStore : UserStore
+    @EnvironmentObject var chatStore: ChatStore
+    
+    var body: some View {
+        
+//        VStack(alignment: .leading) {
+//            HStack(alignment: .top) {
+//                VStack(alignment: .leading) {
+//                    Label(userStore.targetUserName ?? "No Name", systemImage: "person.fill")
+//                        .font(.title2)
+//                        .padding(.bottom, 10)
+//                }
+//
+//                Spacer()
+//                // 날짜, 시간
+//                VStack(alignment:.trailing) {
+//                    Text(chat.stringLastDate)
+//                    Text(chat.stringLastTime)
+//                }
+//                .font(.caption)
+//                .foregroundColor(.secondary)
+//            }
+//
+//            Divider()
+//
+//            // 마지막 채팅 메세지
+//            Text(chat.lastContent)
+//
+//        }
+//        .task {
+//            userStore.requestTargetUserName(targetID: chat.targetID)
+//        }
+//        .padding()
+//        .background(Color.backAccent)
+//        .cornerRadius(20)
+//        .padding()
+        VStack(alignment: .leading){
+            HStack{
+                Image(systemName: "globe.americas.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70)
+                    .padding(.trailing)
+                
+                VStack(alignment: .leading){
+                    Text(chat.users.receiverID == Utility.loginUserID ? chat.users.senderID : chat.users.receiverID)
+                        .font(.title2)
+                        .padding(.bottom, 5)
+                    Text(chat.lastContent)
+                        .foregroundColor(.gray)
+                }
+                
+            }
+            Divider()
+                .frame(width: 350)
         }
     }
 }
@@ -34,5 +88,7 @@ struct ChatListSection: View {
 struct ChatListSection_Previews: PreviewProvider {
     static var previews: some View {
         ChatListSection()
+            .environmentObject(ChatStore())
+            .environmentObject(UserStore())
     }
 }
--- GitSpace/Sources/Views/Penpal/ChatView.swift
+++ GitSpace/Sources/Views/Penpal/ChatView.swift
@@ -24,44 +24,5 @@ struct ChatView: View {
     }
 }
 
-struct ListCellLabel : View {
-    
-    let chat : Chat
-    @EnvironmentObject var userStore : UserStore
-    
-    var body: some View {
-        
-        VStack(alignment: .leading) {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading) {
-                    Label(userStore.targetUserName ?? "No Name", systemImage: "person.fill")
-                        .font(.title2)
-                        .padding(.bottom, 10)
-                }
-                
-                Spacer()
-                // 날짜, 시간
-                VStack(alignment:.trailing) {
-                    Text(chat.stringLastDate)
-                    Text(chat.stringLastTime)
-                }
-                .font(.caption)
-                .foregroundColor(.secondary)
-            }
-            
-            Divider()
-            
-            // 마지막 채팅 메세지
-            Text(chat.lastContent)
-            
-        }
-        .task {
-            userStore.requestTargetUserName(targetID: chat.targetID)
-        }
-        .padding()
-        .background(Color.backAccent)
-        .cornerRadius(20)
-        .padding()
-    }
-}
+
 
