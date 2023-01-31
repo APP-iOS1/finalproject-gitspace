0622a02684f0ee06ff8edd0ed5d442b46bec0c09
Taeyoung Won <hexter1994@gmail.com>
2023-01-31 17:43:11 +0900

    [Chore] #48 loginUserID 고정 상수로 변경

--- GitSpace/Sources/ListenerTechResearch/Utility.swift
+++ GitSpace/Sources/ListenerTechResearch/Utility.swift
@@ -9,13 +9,7 @@ import Foundation
 import FirebaseAuth
 
 enum Utility {
-    
     static var loginUserID : String {
-        guard let currentUser = Auth.auth().currentUser else {
-            return ""
-        }
-        return currentUser.uid
+        return "lX1YHIOZiLXVtB76QDIeekxKgt33"
     }
-    
-    
 }
