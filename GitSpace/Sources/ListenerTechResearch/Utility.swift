ec72233ae3ff0fa742a8dcde4cd067840325173c
Taeyoung Won <hexter1994@gmail.com>
2023-02-01 04:49:31 +0900

    [Chore] #48 MessageCell Width 전역상수 추가

--- GitSpace/Sources/ListenerTechResearch/Utility.swift
+++ GitSpace/Sources/ListenerTechResearch/Utility.swift
@@ -5,11 +5,15 @@
 //  Created by 원태영 on 2023/01/27.
 //
 
-import Foundation
+import SwiftUI
 import FirebaseAuth
 
 enum Utility {
     static var loginUserID : String {
         return "lX1YHIOZiLXVtB76QDIeekxKgt33"
+    }
+    
+    static var MessageCellWidth: CGFloat {
+        return UIScreen.main.bounds.width / 5 * 3
     }
 }
