0b5caa6fc97c274d7e7729a3342c5dd4c1bc4770
Yeseul Chung <amy7806@gmail.com>
2023-01-31 16:43:15 +0900

    [Move] #48 파일 이름 및 위치 변경

--- GitSpace/Sources/Models/Chat.swift
+++ GitSpace/Sources/Models/Chat.swift
@@ -6,23 +6,26 @@
 //
 
 import Foundation
+import Firebase
 
 // 채팅방 모델
 struct Chat : Identifiable {
     let id : String // 채팅방 ID
-    let date : Double // 생성 날짜
-    let userIDs : (open: String, join: String) // 채팅방 참여 유저 2명 ID (개설, 참여)
-    let lastDate : Double // 마지막 메세지 날짜
+    let date : Date // 생성 날짜
+    let users: (senderID: String, receiverID: String) // 채팅방 참여 유저 2명 ID (개설, 참여)
+    let lastDate : Date // 마지막 메세지 날짜
     let lastContent : String // 마지막 메세지 내용
+    let knockContent: String // 노크 메세지 내용
+    let knockDate: Date // 노크 메세지 날짜
     
     // MARK: -Computed Properties
     // 로그인 ID와 userIDs를 비교해서 상대방 유저 ID를 반환하는 연산 프로퍼티
     var targetID : String {
-        return userIDs.open == Utility.loginUserID ? userIDs.join : userIDs.open
+        return users.senderID == Utility.loginUserID ? users.receiverID : users.senderID
     }
     
     var userIDList : [String] {
-        return [userIDs.open, userIDs.join]
+        return [users.senderID, users.receiverID]
     }
     
     var stringDate : String {
@@ -30,8 +33,7 @@ struct Chat : Identifiable {
         dateFormatter.locale = Locale(identifier: "ko_kr")
         dateFormatter.timeZone = TimeZone(abbreviation: "KST")
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        let dateAt = Date(timeIntervalSince1970: date)
-        return dateFormatter.string(from: dateAt)
+        return dateFormatter.string(from: date)
     }
     
     var stringLastDate : String {
@@ -39,8 +41,7 @@ struct Chat : Identifiable {
         dateFormatter.locale = Locale(identifier: "ko_kr")
         dateFormatter.timeZone = TimeZone(abbreviation: "KST")
         dateFormatter.dateFormat = "yyyy년 MM월 dd일"
-        let dateAt = Date(timeIntervalSince1970: lastDate)
-        return dateFormatter.string(from: dateAt)
+        return dateFormatter.string(from: lastDate)
     }
     
     var stringLastTime : String {
@@ -48,7 +49,14 @@ struct Chat : Identifiable {
         dateFormatter.locale = Locale(identifier: "ko_kr")
         dateFormatter.timeZone = TimeZone(abbreviation: "KST")
         dateFormatter.dateFormat = "HH:mm"
-        let dateAt = Date(timeIntervalSince1970: lastDate)
-        return dateFormatter.string(from: dateAt)
+        return dateFormatter.string(from: lastDate)
+    }
+    
+    var stringKnockDate : String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일 HH:mm:ss"
+        return dateFormatter.string(from: knockDate) + (dateFormatter.timeZone.abbreviation() ?? "")
     }
 }
---
+++ GitSpace/Sources/ViewModel/ChatViewModel.swift
@@ -0,0 +1,136 @@
+// TODO: -해야할 거
+/// 1. snapshotListener에서 단일로 Cell을 추가하거나 삭제하는 것이 가능한지
+/// 2. 업데이트 된 단일 Cell이 View에 반영되는지
+/// 3. remove할 때 인덱스 번호로 찾아가서 걔만 지울 수 있는지
+
+import Foundation
+import FirebaseFirestore
+
+class ChatStore: ObservableObject {
+    @Published var targetChat : Chat?
+    @Published var chats: [Chat]
+    
+    let db = Firestore.firestore()
+    
+    init() {
+        chats = []
+    }
+    
+    func fetchChats() {
+        db.collection("Chat").order(by: "lastDate", descending: true)
+            .getDocuments { (snapshot, error) in
+                self.chats.removeAll()
+                
+                if let snapshot {
+                    for document in snapshot.documents {
+                        let id: String = document.documentID
+                        let docData = document.data()
+                        let date: Date = docData["date"] as? Date ?? Date()
+                        let users: [String: String] = docData["users"] as? [String: String] ?? [:]
+                        let lastDate: Date = docData["lastDate"] as? Date ?? Date()
+                        let lastContent: String = docData["lastContent"] as? String ?? ""
+                        let knockDate: Date = docData["knockDate"] as? Date ?? Date()
+                        let knockContent: String = docData["knockContent"] as? String ?? ""
+                        
+                        let userIDs : (String, String)
+                        
+                        // DB에 Array로 저장한 userIDs를 Tuple로 변환
+                        // 할당에 성공한 경우에만 Chat 구조체 추가
+                        if let senderID = users["senderID"], let receiverID = users["receiverID"] {
+                            userIDs = (senderID, receiverID)
+                            let chat = Chat(id: id,
+                                            date: date,
+                                            users: userIDs,
+                                            lastDate: lastDate,
+                                            lastContent: lastContent,
+                                            knockContent: knockContent,
+                                            knockDate: knockDate)
+                            self.chats.append(chat)
+                        }
+                    }
+                }
+            }
+    }
+    
+    // MARK: -Chat CRUD
+    func addChat(_ chat: Chat) {
+        db.collection("Chat")
+            .document(chat.id)
+            .setData(["id" : chat.id,
+                      "date" : chat.date,
+                      "users" : [chat.users.senderID, chat.users.receiverID],
+                      "lastDate" : chat.lastDate,
+                      "lastContent" : chat.lastContent,
+                      "knockContent" : chat.knockContent,
+                      "knockDate" : chat.knockDate])
+        fetchChats()
+    }
+    
+    func updateChat(_ chat: Chat) {
+        db.collection("Chat")
+            .document(chat.id)
+            .updateData(["lastDate" : chat.lastDate,
+                         "lastContent" : chat.lastContent])
+        fetchChats()
+    }
+    
+    func removeChat(_ chat: Chat) {
+        db.collection("Chat")
+            .document(chat.id).delete()
+        
+        fetchChats()
+    }
+}
+    
+    
+
+//MARK: - 이전 리스너 기술 검증을 위해 썼던 코드로, 이후 참고할 수도 있어서 주석으로 남겨놓았습니다. by. 예슬
+    // MARK: -Method : 유저 리스트를 통해서 채팅방을 Request해서 채팅방 객체를 Published 변수에 할당하는 함수
+//    func requestChatFromUserList(userIDs : [String]) {
+//
+//        db.collection("Chat")
+//            .whereField("users", arrayContainsAny: userIDs)
+//            .getDocuments { (snapshot, error) in
+//
+//                // MARK: -Logic : userIDs에 해당하는 (내 ID와 상대방 ID로 만들어진 채팅방이 이미 존재하면) 해당 채팅방을 return, 없으면 신규 채팅방을 add하고 해당 채팅방을 return.
+//                if let snapshot {
+//                    if snapshot.documents.isEmpty {
+//                        print("documents is Empty")
+//                        let date = Date()
+//                        if let open = chat.users.senderID, let join = chat.users.receiverID {
+//
+//                            let newChat = Chat(id: UUID().uuidString,
+//                                               date: date,
+//                                               userIDs: (open, join),
+//                                               lastDate: date,
+//                                               lastContent: "")
+//                            self.addChat(newChat)
+//                            self.targetChat = newChat
+//                        }
+//                    } else {
+//                        for document in snapshot.documents {
+//                            let id: String = document.documentID
+//                            let docData = document.data()
+//                            let date = docData["date"] as? Double ?? 0.0
+//                            let userIDList = docData["userIDs"] as? [String] ?? []
+//                            let lastDate = docData["lastDate"] as? Double ?? 0.0
+//                            let lastContent = docData["lastContent"] as? String ?? ""
+//
+//                            let userIDs : (String, String)
+//
+//                            // DB에 Array로 저장한 userIDs를 Tuple로 변환
+//                            // 할당에 성공한 경우에만 Chat 구조체 추가
+//                            if let open = userIDList.first, let join = userIDList.last {
+//                                userIDs = (open, join)
+//                                let chat = Chat(id: id,
+//                                                date: date,
+//                                                userIDs: userIDs,
+//                                                lastDate: lastDate,
+//                                                lastContent: lastContent)
+//                                self.targetChat = chat
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//    }
