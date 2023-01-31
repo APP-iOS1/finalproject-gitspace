cedfa8bbd8c3e412b96295cff8571b94e43f6e85
Taeyoung Won <hexter1994@gmail.com>
2023-02-01 01:55:11 +0900

    [Chore] #48 MessageViewModel DB Path Chat Collection 추가

--- GitSpace/Sources/ViewModel/MessageViewModel.swift
+++ GitSpace/Sources/ViewModel/MessageViewModel.swift
@@ -6,12 +6,15 @@
 //
 
 import Foundation
+import FirebaseCore
 import FirebaseFirestore
 
 class MessageStore : ObservableObject {
     @Published var messages : [Message]
-    var listener : ListenerRegistration?
     
+    var startMessagesCounter: Int = 0
+    var startMessagesRemoved: Bool = false
+    var listener : ListenerRegistration?
     let database = Firestore.firestore()
     
     init() {
@@ -19,63 +22,11 @@ class MessageStore : ObservableObject {
     }
     
     
-    // MARK: -Method : 채팅 ID를 받아서 메세지들을 불러오는 함수
-    func fetchMessages(chatID: String) {
-        self.database
-            .collection("Chat").document(chatID)
-            .collection("Message")
-            .order(by: "date")
-            .getDocuments { snapshot, error in
-                self.messages.removeAll()
-                
-                if let snapshot {
-                    for document in snapshot.documents {
-                        let id: String = document.documentID
-                        let docData = document.data()
-                        let userID: String = docData["userID"] as? String ?? ""
-                        let content: String = docData["content"] as? String ?? ""
-                        let date: Date = docData["date"] as? Date ?? Date()
-                        
-                        let newMessage = Message(id: id,
-                                                 userID: userID,
-                                                 content: content,
-                                                 date: date)
-                        
-                        self.messages.append(newMessage)
-                    }
-                }
-            }
-    }
-    
-    
-    // MARK: - Message CRUD
-    func addMessage(_ message: Message, chatID : String) {
-        
-        database
-            .collection("Chat")
-            .document(chatID)
-            .collection("Message")
-            .document(message.id)
-            .setData(["id" : message.id,
-                      "userID" : message.userID,
-                      "content" : message.content,
-                      "date" : message.date])
-    }
-    
-    func updateMessage(_ message: Message) {
-        database
-            .collection("Message")
-            .document(message.id)
-            .updateData(["id" : message.id,
-                         "userID" : message.userID,
-                         "content" : message.content,
-                         "date" : message.date])
-    }
-    
-    func removeMessage(_ message: Message) {
-        database.collection("Message")
-            .document(message.id)
-            .delete()
+    func removeListenerMessages() {
+        if !startMessagesRemoved {
+            messages.removeFirst(startMessagesCounter)
+            startMessagesRemoved = true
+        }
     }
     
     // MARK: Method : 추가된 문서 필드에 접근하여 Message 객체를 만들어 반환하는 함수
@@ -84,7 +35,8 @@ class MessageStore : ObservableObject {
         let data = change.data()
         let userID: String = data["userID"] as? String ?? ""
         let content: String = data["content"] as? String ?? ""
-        let date: Date = data["date"] as? Date ?? Date()
+        let timeStamp: Timestamp = data["date"] as? Timestamp ?? Timestamp()
+        let date: Date = Timestamp.dateValue(timeStamp)()
         let newMessage = Message(id: id, userID: userID, content: content, date: date)
         return newMessage
     }
@@ -107,10 +59,13 @@ class MessageStore : ObservableObject {
                     switch diff.type {
                     case .added:
                         print("added")
+                        print(diff.document.documentID)
                         let newMessage = self.fetchNewMessage(change: diff.document)
                         self.messages.append(newMessage)
+                        self.startMessagesCounter += 1
                     case .modified:
                         print("modified")
+                        print(diff.document.documentID)
                     case .removed:
                         print("removed")
                     }
@@ -123,5 +78,72 @@ class MessageStore : ObservableObject {
             return
         }
         listener!.remove()
+    }
+}
+
+
+extension MessageStore {
+    // MARK: -Method : 채팅 ID를 받아서 메세지들을 불러오는 함수
+    func fetchMessages(chatID: String) {
+        self.database
+            .collection("Chat").document(chatID)
+            .collection("Message")
+            .order(by: "date")
+            .getDocuments { snapshot, error in
+                self.messages.removeAll()
+                
+                if let snapshot {
+                    for document in snapshot.documents {
+                        let id: String = document.documentID
+                        let docData = document.data()
+                        let userID: String = docData["userID"] as? String ?? ""
+                        let content: String = docData["content"] as? String ?? ""
+                        let timeStamp: Timestamp = docData["date"] as? Timestamp ?? Timestamp()
+                        let date: Date = Timestamp.dateValue(timeStamp)()
+                        
+                        let newMessage = Message(id: id,
+                                                 userID: userID,
+                                                 content: content,
+                                                 date: date)
+                        
+                        self.messages.append(newMessage)
+                    }
+                }
+            }
+    }
+    
+    
+    
+    // MARK: - Message CRUD
+    func addMessage(_ message: Message, chatID : String) {
+        
+        database
+            .collection("Chat")
+            .document(chatID)
+            .collection("Message")
+            .document(message.id)
+            .setData(["id" : message.id,
+                      "userID" : message.userID,
+                      "content" : message.content,
+                      "date" : message.date])
+    }
+    
+    func updateMessage(_ message: Message, chatID: String) {
+        database
+            .collection("Chat")
+            .document(chatID)
+            .collection("Message")
+            .document(message.id)
+            .updateData(["content" : message.content,
+                         "date" : message.date])
+    }
+    
+    func removeMessage(_ message: Message, chatID: String) {
+        database
+            .collection("Chat")
+            .document(chatID)
+            .collection("Message")
+            .document(message.id)
+            .delete()
     }
 }
