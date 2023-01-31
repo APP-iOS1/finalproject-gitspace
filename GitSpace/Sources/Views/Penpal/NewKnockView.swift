65285c0b9abff48bc3b72ca0390557f23dc455eb
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 15:23:25 +0900

    [Add] #47 NewKnockView
    [Add] #47 NewKnockView

---
+++ GitSpace/Sources/Views/Penpal/NewKnockView.swift
@@ -0,0 +1,20 @@
+//
+//  NewKnockView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct NewKnockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewKnockView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewKnockView()
+    }
+}
