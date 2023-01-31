a3e1abd12f8776a049a2fa1cded886c90266c272
Taeyoung Won <hexter1994@gmail.com>
2023-01-31 17:44:48 +0900

    [Add] #48 ChatRecommandCardSection 파일 추가

---
+++ GitSpace/Sources/Views/Penpal/ChatRecommandCardSection.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRecommandCardSection.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct ChatRecommandCardSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatRecommandCardSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRecommandCardSection()
+    }
+}
