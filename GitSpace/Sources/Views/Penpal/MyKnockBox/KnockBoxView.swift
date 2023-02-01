134a7f7dfe263a1c5597a1b0138ed92a6614dbd3
valseLee <sollleky72@gmail.com>
2023-02-01 17:22:05 +0900

    [Fix] #57 파일이름 수정

---
+++ GitSpace/Sources/Views/Penpal/MyKnockBox/KnockBoxView.swift
@@ -0,0 +1,270 @@
+//
+//  MyKnockBoxView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/01/18.
+//
+
+import SwiftUI
+
+struct KnockBoxView: View {
+	@ObservedObject var knockHistoryViewModel = KnockHistoryViewModel()
+	
+	@State private var knocks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+								 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+	
+	@State private var userName = []
+	@State private var knockMsg = []
+	
+	@State private var knockMessenger: String = "Received"
+	@State private var receivedTab: String = "Received"
+	@State private var sendedTab: String = "Sended"
+	@State private var userFilteredKnockState: KnockStateFilter = .waiting
+	
+	@State var searchWord: String = ""
+	
+	@State var isEdit: Bool = false
+	@State var checked: Bool = false
+	@State var showingKnockSetting: Bool = false
+	
+	var body: some View {
+		VStack {
+			// MARK: - Tab Buttons
+			headerTabPagenationViewBuilder()
+			.overlay(alignment: .bottom) {
+				Divider()
+					.frame(height: 0.5)
+					.overlay(Color.primary)
+					.offset(y: 3.5)
+			}
+			.padding(.horizontal, 16)
+			
+			TabView(selection: $knockMessenger) {
+				ScrollView {
+					VStack {
+						// 노크를 한 사람에 대한 정보를 보려면 노크 메세지를 확인하세요.
+						Text("Check the message for information about who's Knocking on you.")
+							.foregroundColor(Color(.systemGray))
+						
+						// 상대방은 응답할 때까지 회원님의 노크 확인 여부를 알 수 없습니다.
+						Text("They won't know you've seen it until you respond.")
+							.foregroundColor(Color(.systemGray))
+						
+						Button {
+							showingKnockSetting.toggle()
+						} label: {
+							// 나에게 노크 할 수 있는 사람 설정하기
+							Text("Decide who can Knock on you")
+						}
+					}
+					.font(.caption2)
+					.padding(.top, 16)
+					
+					Divider()
+					
+					LazyVStack {
+						// MARK: - Knock Cell
+						ForEach(
+							knockHistoryViewModel.receivedKnockLists.sorted {
+								knockHistoryViewModel.compareTwoKnockWithStatus(lhs: $0, rhs: $1)
+							},
+							id: \.self) { eachKnock in
+								NavigationLink {
+									if eachKnock.knockStatus == "Waiting" {
+										ReceivedKnockView()
+									} else {
+										KnockHistoryView(
+											eachKnock: eachKnock,
+											knockMessenger: $knockMessenger
+										)
+									}
+								} label: {
+									switch userFilteredKnockState {
+									case .waiting,
+											.declined,
+											.accepted:
+										if eachKnock.knockStatus == userFilteredKnockState.rawValue {
+											MyKnockCell(
+												knockHistoryViewModel: knockHistoryViewModel,
+												eachKnock: eachKnock,
+												isEdit: $isEdit,
+												checked: $checked,
+												knockMessenger: $knockMessenger
+											)
+											.foregroundColor(.primary)
+										}
+									case .all:
+										MyKnockCell(
+											knockHistoryViewModel: knockHistoryViewModel,
+											eachKnock: eachKnock,
+											isEdit: $isEdit,
+											checked: $checked,
+											knockMessenger: $knockMessenger
+										)
+										.foregroundColor(.primary)
+									}
+								}
+							} // ForEach
+							.searchable(
+								text: $searchWord,
+								placement: .navigationBarDrawer(displayMode: .always),
+								prompt: "Search..."
+							)
+					} // LazyVStack
+				}
+				.tag(receivedTab)
+				
+				ScrollView {
+					VStack {
+						// 노크를 한 사람에 대한 정보를 보려면 노크 메세지를 확인하세요.
+						Text("You can check your knock history.")
+							.foregroundColor(Color(.systemGray))
+						
+						// 상대방은 응답할 때까지 회원님의 노크 확인 여부를 알 수 없습니다.
+						Text("You can't send other messages until your receiver has approved your knock")
+							.foregroundColor(Color(.systemGray))
+							.multilineTextAlignment(.center)
+					}
+					.font(.caption2)
+					.padding(.top, 16)
+					
+					Divider()
+					
+					LazyVStack {
+					// MARK: - Knock Cell
+					ForEach(
+						knockHistoryViewModel.sendedKnockLists.sorted {
+							knockHistoryViewModel.compareTwoKnockWithStatus(lhs: $0, rhs: $1)
+						},
+						id: \.self) { eachKnock in
+							NavigationLink {
+								KnockHistoryView(
+									eachKnock: eachKnock,
+									knockMessenger: $knockMessenger
+								)
+							} label: {
+								MyKnockCell(
+									knockHistoryViewModel: knockHistoryViewModel,
+									eachKnock: eachKnock,
+									isEdit: $isEdit,
+									checked: $checked,
+									knockMessenger: $knockMessenger
+								)
+								.foregroundColor(.black)
+							}
+							Divider()
+						} // ForEach
+						.searchable(
+							text: $searchWord,
+							placement: .navigationBarDrawer(displayMode: .always),
+							prompt: "Search..."
+						)
+				} // LazyVStack
+				}
+				.tag(sendedTab)
+			}
+			.tabViewStyle(.page)
+			.ignoresSafeArea(.container, edges: .bottom)
+		} // ScrollView
+		.navigationBarTitle(knockMessenger + " Knock", displayMode: .inline)
+		.toolbar {
+			ToolbarItem(placement: .navigationBarTrailing) {
+				Button(isEdit ? "Cancel" : "Edit") {
+					withAnimation(.easeIn(duration: 0.28)) {
+						isEdit.toggle()
+					}
+				}
+			}
+		}
+		.fullScreenCover(isPresented: $showingKnockSetting) {
+			MyKnockSettingView(showingKnockSetting: $showingKnockSetting)
+		}
+	} // body
+	
+	@ViewBuilder
+	private func headerTabPagenationViewBuilder() -> some View {
+		HStack(spacing: 12) {
+			GSButton.CustomButtonView(style: .homeTab(
+				tabName: receivedTab,
+				tabSelection: $knockMessenger)) {
+					withAnimation {
+						knockMessenger = receivedTab
+					}
+				} label: {
+					Text(receivedTab)
+						.font(.title3)
+						.foregroundColor(.primary)
+						.bold()
+						.padding(.bottom, 4)
+				}
+			
+			GSButton.CustomButtonView(style: .homeTab(
+				tabName: sendedTab,
+				tabSelection: $knockMessenger)) {
+					withAnimation {
+						knockMessenger = sendedTab
+					}
+				} label: {
+					Text(sendedTab)
+						.font(.title3)
+						.foregroundColor(.primary)
+						.bold()
+						.padding(.bottom, 4)
+				}
+			
+			Spacer()
+			
+			Menu {
+				Button {
+					withAnimation {
+						userFilteredKnockState = .all
+					}
+				} label: {
+					Text("See All")
+				}
+				
+				Button {
+					withAnimation {
+						userFilteredKnockState = .waiting
+					}
+				} label: {
+					Text("See Waiting")
+				}
+				
+				Button {
+					withAnimation {
+						userFilteredKnockState = .accepted
+					}
+				} label: {
+					Text("See Accepted")
+				}
+				
+				Button {
+					withAnimation {
+						userFilteredKnockState = .declined
+					}
+				} label: {
+					Text("See Declined")
+				}
+			} label: {
+				Image(systemName: "line.3.horizontal.decrease")
+					.foregroundColor(.primary)
+			}
+		}
+	}
+} // MyKnockBoxView()
+
+struct KnockBoxView_Previews: PreviewProvider {
+	static var previews: some View {
+		NavigationView {
+			KnockBoxView()
+		}
+	}
+}
+
+enum KnockStateFilter: String {
+	case waiting = "Waiting"
+	case accepted = "Accepted"
+	case declined = "Declined"
+	case all = "All"
+}
