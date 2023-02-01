698bb344b2d7947d03fc9fffc700ebaf320603b2
Taeyoung Won <hexter1994@gmail.com>
2023-02-01 17:41:57 +0900

    [Chore] #55 Async Image 재사용을 위해 Sub View로 분리

--- GitSpace/Sources/Views/Penpal/ChatDetailProfileSection.swift
+++ GitSpace/Sources/Views/Penpal/ChatDetailProfileSection.swift
@@ -14,18 +14,9 @@ struct ChatDetailProfileSection: View {
     
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: "https://w.namu.la/s/fb074c9e538edb0b41d818df3cb7b5499a844aeb5e8becc3ce1664468c885d883e8a8243a33eefc11e107b8d7dbbf77a410d78675770117a6654984ebe73f2f2eb846d97e660cdc8ab76067ddad22f9574647c8b25eaf5022f4f481b458094b9")) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .frame(width : 100)
-            } placeholder: {
-                // 불러오는 중입니다...
-                Text("Loading...")
-            }
-            .padding(.vertical, 10)
-            .padding(.top, 10)
+            ProfileAsyncImage(size: 100)
+                .padding(.vertical, 10)
+                .padding(.top, 10)
             
             Text("@"+targetName)
                 .font(.subheadline)
@@ -41,6 +32,27 @@ struct ChatDetailProfileSection: View {
             .font(.subheadline)
             .foregroundColor(.gsLightGray2)
             
+        }
+    }
+}
+
+// MARK: -View :
+struct ProfileAsyncImage: View {
+    
+    let urlStr: String = "https://w.namu.la/s/fb074c9e538edb0b41d818df3cb7b5499a844aeb5e8becc3ce1664468c885d883e8a8243a33eefc11e107b8d7dbbf77a410d78675770117a6654984ebe73f2f2eb846d97e660cdc8ab76067ddad22f9574647c8b25eaf5022f4f481b458094b9"
+    let size: CGFloat
+    
+    var body : some View {
+        AsyncImage(url: URL(string: urlStr)) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .border(.primary)
+                .clipShape(Circle())
+                .frame(width : size)
+        } placeholder: {
+            // 불러오는 중입니다...
+            Text("Loading...")
         }
     }
 }
