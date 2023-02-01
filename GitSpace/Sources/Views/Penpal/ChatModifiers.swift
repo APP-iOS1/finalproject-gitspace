0b9ef069955ce1a91794924b98ffdd58c13e6955
Taeyoung Won <hexter1994@gmail.com>
2023-02-01 17:12:01 +0900

    [Chore] #55 Knock Message Cell에 텍스트 글자 수 상관없이 고정 가로 사이즈를 가지도록 수정

--- GitSpace/Sources/Views/Penpal/ChatModifiers.swift
+++ GitSpace/Sources/Views/Penpal/ChatModifiers.swift
@@ -13,8 +13,8 @@ struct MessageModifier : ViewModifier {
     func body(content: Content) -> some View {
         content
             .font(.system(size: 16))
-            .padding(20)
-            .frame(maxWidth: Utility.MessageCellWidth)
+            .padding()
+//            .frame(maxWidth: Utility.MessageCellWidth)
             .background(isMine ? Color.gsGreenPrimary : Color(uiColor: .systemGray4))
             .cornerRadius(17)
             .overlay(alignment: isMine ? .trailing : .leading) {
@@ -37,12 +37,13 @@ struct MessageTimeModifier : ViewModifier {
 // MARK: Modifier : 노크 메세지 셀 속성
 struct KnockMessageModifier : ViewModifier {
     func body(content: Content) -> some View {
-        content
-            .font(.system(size: 12))
-            .padding(20)
-            .border(.gray.opacity(0.1))
-            .shadow(radius: 10)
-            .cornerRadius(17)
+            content
+                .font(.system(size: 12))
+                .padding(20)
+                .frame(minWidth: Utility.UIWidth - 40)
+                .border(.gray.opacity(0.1))
+                .shadow(radius: 10)
+                .cornerRadius(17)
     }
 }
 
