014acb603b376270fab0bd3910d8e98133ad4ab8
valseLee <sollleky72@gmail.com>
2023-02-02 23:39:43 +0900

    [Feat] #65 navigationLink가 버튼 스타일을 빌려올 수 있도록 구현 시작

--- GitSpace/Sources/Views/Components/GSButton.swift
+++ GitSpace/Sources/Views/Components/GSButton.swift
@@ -14,7 +14,7 @@ public struct GSButton {
 	 - Important: 각 케이스는 연관 값을 갖고 있으며 `hometab` 케이스의 경우, 그 연관값을 **필수**로 입력해야 합니다.
 	 `primary`와 `secondary` 케이스의 연관값은 필수가 아니며, 연관값을 할당하면 GSButton의 label 을 **비운 채로** UI를 그릴 수 있습니다.
 	*/
-	public enum GSButtonStyle {
+	public indirect enum GSButtonStyle {
 		case primary(isDisabled: Bool)
 		case secondary(isDisabled: Bool)
 		case tag(isEditing: Bool,
@@ -22,12 +22,14 @@ public struct GSButton {
 		case plainText(isDestructive: Bool)
 		case homeTab(tabName: String,
 					 tabSelection: Binding<String>)
+		case navigate(style: GSButtonStyle)
 	}
 	
 	struct CustomButtonView<CustomLabelType: View>: View {
 		public let style: GSButtonStyle
 		public let action: () -> Void
 		public var label: CustomLabelType?
+		public var destination: CustomLabelType?
 		
 		var body: some View {
 			switch style {
@@ -57,7 +59,7 @@ public struct GSButton {
 				.buttonColorSchemeModifier(style: style)
 				.disabled(isDisabled)
 				
-				
+			// MARK: - TODO : 태그 액션과 상태 기획 정리되면 추가
 			case .tag(let isEditing, let isSelected):
 				Button(action: action) {
 					label
@@ -95,17 +97,40 @@ public struct GSButton {
 							}
 						}
 				}
+				
+			// FIXME: destination이 작동을 안하는 상황이옵니다...
+			case .navigate(let style):
+				NavigationLink {
+					destination
+				} label: {
+					label
+						.buttonLabelLayoutModifier(
+							buttonLabelStyle: style
+						)
+				}
 			}
 		}
 		
 		// Simple Initializer
-		init(style: GSButtonStyle,
-			 action: @escaping () -> Void,
-			 @ViewBuilder label: () -> CustomLabelType) {
-			self.style = style
-			self.action = action
-			self.label = label()
+		init(
+			style: GSButtonStyle,
+			action: @escaping () -> Void,
+			@ViewBuilder label: () -> CustomLabelType) {
+				self.style = style
+				self.action = action
+				self.label = label()
 		}
+
+		// Navi Button Initializer
+		init(
+			style: GSButtonStyle,
+			@ViewBuilder destination: () -> CustomLabelType,
+			@ViewBuilder label: () -> CustomLabelType) {
+				self.style = style
+				self.destination = destination()
+				self.label = label()
+				self.action = { }
+			}
 	}
 }
 
@@ -120,96 +145,22 @@ struct Test2: View {
 	@Environment(\.colorScheme) var colorScheme
 	
 	var body: some View {
-		VStack {
-			GSButton.CustomButtonView(
-				style: .primary(
-					isDisabled: true
-				)
-			) {
-				withAnimation {
-					isDisabled.toggle()
+		NavigationView {
+			VStack {
+				NavigationLink(destination: Text("")) {
+					Text("HIHI")
 				}
-			} label: {
-				HStack {
-					Text("✨")
-					
-					Text("HiHI")
-				}
-			}
-			
-			GSButton.CustomButtonView(
-				style: .tag(
-					isEditing: isEditing,
-					isSelected: isSelected
-				)
-			) {
-				withAnimation {
-					isSelected.toggle()
-				}
-			} label: {
-				Text("?")
-					.font(.callout)
-					.bold()
-			}
-			.tag("HI")
-			
-			HStack {
-				GSButton.CustomButtonView(
-					style: .homeTab(
-						tabName: starTab,
-						tabSelection: $selectedHomeTab
-					)
-				) {
-					withAnimation {
-						selectedHomeTab = starTab
-					}
-				} label: {
-					Text(starTab)
-						.font(.title3)
-						.foregroundColor(.primary)
-						.bold()
-				}
-				.tag(starTab)
 				
-				GSButton.CustomButtonView(
-					style: .homeTab(
-						tabName: followTab,
-						tabSelection: $selectedHomeTab
-					)
-				) {
-					withAnimation {
-						selectedHomeTab = followTab
-					}
+				GSButton.CustomButtonView(style: .primary(isDisabled: false)) {
+					Text("")
 				} label: {
-					Text(followTab)
-						.font(.title3)
-						.foregroundColor(.primary)
-						.bold()
+					Text("GG")
 				}
-				.tag(followTab)
-				
-				Spacer()
+
 			}
-			.overlay(alignment: .bottom) {
-				Divider()
-					.frame(minHeight: 0.5)
-					.overlay(Color.primary)
-					.offset(y: 3.5)
-			}
-			.padding(16)
 			
-			GSButton.CustomButtonView(
-				style: .plainText(
-					isDestructive: false
-				)
-			) {
-				print()
-			} label: {
-				Text("??")
-			}
 		}
 	}
-	
 }
 
 struct GSButton_Previews: PreviewProvider {
@@ -217,60 +168,90 @@ struct GSButton_Previews: PreviewProvider {
 		Test2()
     }
 }
-
-//		VStack {
-//			Text(tabSelection)
 //
-//			GSButton.ContentView(
-//				style: .tabPage,
-//				action: tabMoveTo,
-//				tabSelectionTag: $tabSelection,
-//				tabMoveTo: followTab
-//			) {
-//				Text(starTab)
-//			}
+//GSButton.CustomButtonView(
+//	style: .primary(
+//		isDisabled: true
+//	)
+//) {
+//	withAnimation {
+//		isDisabled.toggle()
+//	}
+//} label: {
+//	HStack {
+//		Text("✨")
 //
-//			GSButton.ContentView(
-//				style: .tabPage,
-//				action: tabMoveTo,
-//				tabSelectionTag: $tabSelection,
-//				tabMoveTo: starTab
-//			) {
-//				Text(followTab)
-//			}
+//		Text("HiHI")
+//	}
+//}
 //
-//			GSButton.ContentView(
-//				style: .primary,
-//				action: tabMoveTo
-//			) {
-//				Text("Knock")
-//			}
+//GSButton.CustomButtonView(
+//	style: .tag(
+//		isEditing: isEditing,
+//		isSelected: isSelected
+//	)
+//) {
+//	withAnimation {
+//		isSelected.toggle()
+//	}
+//} label: {
+//	Text("?")
+//		.font(.callout)
+//		.bold()
+//}
+//.tag("HI")
 //
-//			GSButton.ContentView(
-//				style: .secondary,
-//				action: tabMoveTo
-//			) {
-//				Text(followTab)
-//			}
-//
-//			GSButton.ContentView(
-//				style: .textConfirmation,
-//				action: tabMoveTo
-//			) {
-//				Text("Block @wontaeyoung")
-//			}
-//
-//			GSButton.ContentView(
-//				style: .textCancelation,
-//				action: tabMoveTo
-//			) {
-//				Text("Delete Chat")
-//			}
-//
-//			GSButton.ContentView(
-//				style: .tag,
-//				action: tabMoveTo
-//			) {
-//				Text("Tag")
-//			}
+//HStack {
+//	GSButton.CustomButtonView(
+//		style: .homeTab(
+//			tabName: starTab,
+//			tabSelection: $selectedHomeTab
+//		)
+//	) {
+//		withAnimation {
+//			selectedHomeTab = starTab
 //		}
+//	} label: {
+//		Text(starTab)
+//			.font(.title3)
+//			.foregroundColor(.primary)
+//			.bold()
+//	}
+//	.tag(starTab)
+//
+//	GSButton.CustomButtonView(
+//		style: .homeTab(
+//			tabName: followTab,
+//			tabSelection: $selectedHomeTab
+//		)
+//	) {
+//		withAnimation {
+//			selectedHomeTab = followTab
+//		}
+//	} label: {
+//		Text(followTab)
+//			.font(.title3)
+//			.foregroundColor(.primary)
+//			.bold()
+//	}
+//	.tag(followTab)
+//
+//	Spacer()
+//}
+//.overlay(alignment: .bottom) {
+//	Divider()
+//		.frame(minHeight: 0.5)
+//		.overlay(Color.primary)
+//		.offset(y: 3.5)
+//}
+//.padding(16)
+//
+//GSButton.CustomButtonView(
+//	style: .plainText(
+//		isDestructive: false
+//	)
+//) {
+//	print()
+//} label: {
+//	Text("??")
+//	}
