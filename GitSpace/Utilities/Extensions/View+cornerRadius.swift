3f649a36aa40c59fee78f3582af2b31c9111eb5c
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-03 22:53:13 +0900

    [Add] #82 View의 각 corner에 round를 줄 수 있는 extension 코드 추가

--- GitSpace/Utilities/Extensions/View+cornerRadius.swift
+++ GitSpace/Utilities/Extensions/View+cornerRadius.swift
@@ -5,4 +5,20 @@
 //  Created by 박제균 on 2023/02/03.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius:CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius,corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
