a063ec14aa24bdd670264a773007305f11bd7db1
valseLee <sollleky72@gmail.com>
2023-02-03 15:16:01 +0900

    [Feat] #65 확장 파일에서 modifier 호출하도록 구현

--- GitSpace/Utilities/Extensions/Button+ColorScheme.swift
+++ GitSpace/Utilities/Extensions/Button+ColorScheme.swift
@@ -8,90 +8,14 @@
 import SwiftUI
 
 extension Button {
-	public struct GSButtonColorSchemeModifier: ViewModifier {
-		let style: GSButton.GSButtonStyle
-		@Environment(\.colorScheme) var colorScheme
-		
-		public func body(content: Content) -> some View {
-			switch colorScheme {
-				
-				// MARK: - LIGHT MODE
-			case .light: // light Mode
-				switch style { // Button Style
-				case .primary(let isDisabled):
-					content
-						.buttonBorderShape(.capsule)
-						.buttonStyle(.borderedProminent)
-						.tint(isDisabled ? .gsLightGray1 : .gsGreenPrimary)
-						.shadow(color: .gsYellowPrimary.opacity(0.21),
-								radius: 9,
-								x: 0,
-								y: 9)
-				case .secondary(let isDisabled):
-					content
-						.buttonBorderShape(.capsule)
-						.buttonStyle(.borderedProminent)
-						.tint(isDisabled ? .gsLightGray1 : .gsGreenPrimary)
-						.shadow(color: .gsYellowPrimary.opacity(0.21),
-								radius: 9,
-								x: 0,
-								y: 9)
-				case .tag(let isEditing, let isSelected):
-					content
-						.buttonBorderShape(.capsule)
-						.buttonStyle(.borderedProminent)
-						.tint(isSelected ? Color.gsGreenPrimary : Color.primary)
-				case .plainText,
-						.homeTab:
-					content
-						.foregroundColor(.primary)
-					
-				}
-					
-				// MARK: - DARK MODE
-			case .dark: // dark mode
-				switch style { // Button Style
-				case .primary,
-						.secondary:
-					content
-						.buttonBorderShape(.capsule)
-						.buttonStyle(.borderedProminent)
-						.tint(.gsYellowPrimary)
-						.shadow(color: .gsGreenPrimary.opacity(0.29),
-								radius: 36,
-								x: 0,
-								y: 14)
-				case .tag(let isEditing, let isSelected):
-					content
-						.buttonBorderShape(.capsule)
-						.buttonStyle(.borderedProminent)
-						.tint(isSelected ? Color.gsYellowPrimary : Color.gsDarkGray)
-					//						.tint(tagButtonColorModifier(isEditing: isEditing, isSelected: isSelected))
-				case .plainText(let isDestructive):
-					content
-						.foregroundColor(isDestructive ? .gsRed : .white)
-				case .homeTab:
-					content
-						.colorInvert()
-				}
-			@unknown default: // neither Light nor Dark
-				content
-			}	
-		}
-		
-		fileprivate init(style: GSButton.GSButtonStyle) {
-			self.style = style
-		}
-		
-		// TODO: - branch Processing for each Bool Value
-		private func tagButtonColorModifier(isEditing: Bool, isSelected: Bool) -> Color {
-			if isEditing, isSelected { return .primary }
-			else { return .gsGreenPrimary }
-		}
-	}
-	
 	/// - Important: ONLY USED IN DESIGN SYSTEM CODE
-	public func buttonColorSchemeModifier(style: GSButton.GSButtonStyle) -> some View {
+	public func buttonColorSchemeModifier(style: GSButtonStyle) -> some View {
 		modifier(GSButtonColorSchemeModifier(style: style))
+	}
+}
+
+extension PrimitiveButtonStyle {
+	var gsHighlight: ViewHighlightColorStyle {
+		.init()
 	}
 }
