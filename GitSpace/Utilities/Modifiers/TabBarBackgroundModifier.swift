7a4b87fdd95a576e45b5acff8acd5120a8288c75
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-03 22:58:12 +0900

    [Add] #82 커스텀 탭바 배경을 생성할때 사용하는 ViewModifier 추가

--- GitSpace/Utilities/Modifiers/TabBarBackgroundModifier.swift
+++ GitSpace/Utilities/Modifiers/TabBarBackgroundModifier.swift
@@ -5,4 +5,26 @@
 //  Created by 박제균 on 2023/02/03.
 //
 
-import Foundation
+import SwiftUI
+
+struct TabBarBackgroundModifier: ViewModifier {
+    
+    let style: GSTabBarBackGround.GSTabBarBackGroundStyle
+    
+    init(backgroundStyle: GSTabBarBackGround.GSTabBarBackGroundStyle) {
+        self.style = backgroundStyle
+    }
+    
+    func body(content: Content) -> some View {
+        switch style {
+        case .rectangle(let backGroundColor):
+            content
+                .background(backGroundColor)
+        case .roundedRectangle(let backGroundColor, let cornerRadius):
+            content
+                .background(backGroundColor)
+                .cornerRadius(cornerRadius, corners: [.topLeft, .topRight])
+        }
+    }
+    
+}
