984c05823a63a3bfa33458bce7bacffbf516cfbd
valseLee <sollleky72@gmail.com>
2023-02-03 21:22:56 +0900

    [Add] #65 버튼에 적용할 shadow 모디파이어 구현, 다크모드 대응 가능

---
+++ GitSpace/Utilities/Modifiers/ShadowColorSchemeModifier.swift
@@ -0,0 +1,20 @@
+//
+//  ShadowColorSchemeModifier.swift
+//  GitSpace
+//
+//  Created by 이승준 on 2023/02/03.
+//
+
+import SwiftUI
+
+struct ShadowColorSchemeModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShadowColorSchemeModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        ShadowColorSchemeModifier()
+    }
+}
