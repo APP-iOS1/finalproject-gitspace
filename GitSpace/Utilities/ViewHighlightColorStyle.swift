c16f0dfaa489c529fe17c9a30f3032c48c471aa1
valseLee <sollleky72@gmail.com>
2023-02-03 21:26:50 +0900

    [Feat] #65 NavigationLink 버튼 스타일 구현, 다크모드 대응

--- GitSpace/Utilities/ViewHighlightColorStyle.swift
+++ GitSpace/Utilities/ViewHighlightColorStyle.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ViewHighlightColorStyle: ButtonStyle {
+	let style: Constant.LabelHierarchy
 	@Environment(\.colorScheme) var colorScheme
 	
 	func makeBody(configuration: Self.Configuration) -> some View {
 		configuration.label
-			.foregroundColor(.gsGreenPrimary)
-			.background(configuration.isPressed ? Color.gsGreenPrimary : Color.gsGreenPressed)
+			.background(
+				configuration.isPressed
+				? Color.gsGreenPressed
+				: colorSchemeBackgroundColor()
+			)
 			.cornerRadius(.infinity)
+	}
+	
+	private func colorSchemeBackgroundColor() -> Color {
+		switch colorScheme {
+		case .light:
+			return Color.gsGreenPrimary
+		case .dark:
+			return Color.gsYellowPrimary
+		@unknown default:
+			return Color.clear
+		}
 	}
 }
