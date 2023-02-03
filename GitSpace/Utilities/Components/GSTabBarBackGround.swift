862ace870d896ef49c55d6cfe88bb9457b17764b
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-03 22:56:42 +0900

    [Feat] #82 커스텀 탭바 배경에 사용되는 View 코드를 추상화하여 구현

--- GitSpace/Utilities/Components/GSTabBarBackGround.swift
+++ GitSpace/Utilities/Components/GSTabBarBackGround.swift
@@ -1,5 +1,5 @@
 //
-//  GSTabBarCanvas.swift
+//  GSTabBarBackGround.swift
 //  GitSpace
 //
 //  Created by 박제균 on 2023/02/03.
@@ -10,31 +10,46 @@ import SwiftUI
 struct GSTabBarBackGround {
     
     /**
-     커스텀 탭바의 배경이 Rectangle인지 RoundedRectangle인지 선택할 수 있습니다.
+     커스텀 탭바의 배경이 Rectangle인지 RoundedRectangle인지 선택할 수 있습니다. 또한 연관값으로 배경색상과, RoundedRectangle의 경우 cornerRadius값을 입력해주시면 됩니다.
      */
-    public enum GSTabBarCanvasStyle {
-        case rectangle
-        case roundedRectangle()
+    public enum GSTabBarBackGroundStyle {
+        case rectangle(backGroundColor: Color)
+        case roundedRectangle(backGroundColor: Color, cornerRadius: CGFloat)
     }
     
-    struct CustomTabBarBackgroundView: View {
-        public let style: GSTabBarCanvasStyle
-        public let action: () -> Void
+    /**
+     탭바는 가로 고정이기 때문에 HStack으로 두었습니다.
+     */
+    struct CustomTabBarBackgroundView<Content: View>: View {
+        
+        public let style: GSTabBarBackGroundStyle
+        public let content: Content
      
         var body: some View {
             
             switch style {
                 
-            case .rectangle:
+            case .rectangle(let backGroundColor):
                 HStack {
-                    
+                    content
                 }
-            case .roundedRectangle:
+                .edgesIgnoringSafeArea(.bottom)
+                .tabBarBackGroundModifier(style: .rectangle(backGroundColor: backGroundColor))
+                
+            case .roundedRectangle(let backGroundColor, let cornerRadius):
                 HStack {
-                    
+                    content
                 }
+                .edgesIgnoringSafeArea(.bottom)
+                .tabBarBackGroundModifier(style: .roundedRectangle(backGroundColor: backGroundColor, cornerRadius: cornerRadius))
             }
         }
+        
+        init(style: GSTabBarBackGroundStyle, @ViewBuilder content: () -> Content) {
+            self.style = style
+            self.content = content()
+        }
+        
     }
     
 }
