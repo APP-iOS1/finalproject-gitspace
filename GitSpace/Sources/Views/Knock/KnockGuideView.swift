4ac5f8590da7fc1b86ee8445464bad0c4e1116e3
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-04 12:39:03 +0900

    [Add] #47  모달로 띄워 줄 Knock Guide View 추가
    # 모달로 띄워 줄 Knock Guide View 추가

---
+++ GitSpace/Sources/Views/Knock/KnockGuideView.swift
@@ -0,0 +1,20 @@
+//
+//  KnockGuideView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/04.
+//
+
+import SwiftUI
+
+struct KnockGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KnockGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        KnockGuideView()
+    }
+}
