0f9cd84fc238188dd6a0d1b15bef15fc796d6043
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-06 01:04:28 +0900

    [Chore] #100 GitHubAPIError에 주석 추가

--- GitSpace/Sources/Network/GitHubAPIError.swift
+++ GitSpace/Sources/Network/GitHubAPIError.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum GitHubAPIError: String, Error {
+    /// URLComponents를 통해 URL 생성에 실패한 경우 보여줄 에러 메시지
     case invalidURL = "URL was invalid, Please try again."
     case invalidResponse = "Invalid response from the server. Please try again."
 }
