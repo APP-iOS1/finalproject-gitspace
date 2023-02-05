00981b4cc10700cdda8cf5205e39e6193052ebe6
Taeyoung Won <hexter1994@gmail.com>
2023-02-06 00:15:50 +0900

    [Chore] #90 font 옵셔널 제거, init에 기본값 body 할당 활용

--- GitSpace/Utilities/Components/GSTextEditor.swift
+++ GitSpace/Utilities/Components/GSTextEditor.swift
@@ -9,22 +9,19 @@ struct GSTextEditor {
     // MARK: Stored Properties
     let style: GSTextEditorStyle
     let text: Binding<String>
-    let font: Font?
+    let font: Font
     let lineSpace: CGFloat?
     
     // MARK: Computed Properties
     // font 사이즈 관련 프로퍼티를 활용하기 위해 Font -> UIFont로 변환
     private var mainUIFont: UIFont {
-        if let font {
-            return UIFont.fontToUIFont(from: font)
-        }
         return UIFont.fontToUIFont(from: .body)
     }
     
     init (
         style: GSTextEditorStyle,
         text: Binding<String>,
-        font: Font? = .body,
+        font: Font = .body,
         lineSpace: CGFloat? = 2
     ) {
         self.style = style
