8fdf1b6d8853c073c48a49ecfde097983e461f37
Taeyoung Won <hexter1994@gmail.com>
2023-02-05 23:26:14 +0900

    [Chore] #90 GSTextEditor 프로퍼티 추가

--- GitSpace/Utilities/Components/GSTextEditor.swift
+++ GitSpace/Utilities/Components/GSTextEditor.swift
@@ -4,5 +4,13 @@ struct GSTextEditor {
     enum GSTextEditorStyle {
         case message
     }
+    
+    // MARK: -Properties
+    let style: GSTextEditorStyle
+    let text: Binding<String>
+    let font: Font?
+    let lineSpace: CGFloat?
+    
+    
 }
 
