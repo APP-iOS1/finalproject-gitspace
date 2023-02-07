df35e4ada32c73cdffedd421fe4f0d4c05b600f8
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-07 17:25:09 +0900

    [Add] #122 NotificationView 추가

--- GitSpace/Sources/Views/Notification/NotificationView.swift
+++ GitSpace/Sources/Views/Notification/NotificationView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct NotificationView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            
+            ForEach(0..<10) { _ in
+                KnockNotificationCell()
+                Divider()
+                SystemNotificationCell()
+                Divider()
+            }
+            
+        }
+//        .padding(.top, 20)
+        .navigationBarTitle("Notifications")
+        .navigationBarTitleDisplayMode(.inline)
+        
+        
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        NavigationView {
+            NotificationView()
+        }
     }
 }
