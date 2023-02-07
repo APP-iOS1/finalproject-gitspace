d81687e26849cb570e90c531876f8b5a508de91e
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-07 11:32:31 +0900

    [Add] #96 GuideCenter 폴더 생성 및 KnockGuideView 위치 이동
    # GuideCenter 폴더 생성 및 KnockGuideView 위치 이동

--- GitSpace/Sources/Views/Knock/SendKnockView.swift
+++ GitSpace/Sources/Views/Knock/SendKnockView.swift
@@ -89,7 +89,7 @@ struct SendKnockView: View {
                                 
                                 withAnimation(.easeInOut.speed(1.5)) { proxy.scrollTo(bottomID) }
                                 
-                                    .becomeFirstResponder()
+                                    //.becomeFirstResponder()
                                 
                             } label: {
                                 Text("🚀 Offer")
