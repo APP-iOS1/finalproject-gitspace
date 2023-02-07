fac315bba27d342c8bdf0acd7b4908d43cf13eaa
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-07 15:22:07 +0900

    [Add] #96 GuidBlockSection / BlockGuideView 생성
    # GuidBlockSection / BlockGuideView 생성

---
+++ GitSpace/Sources/Views/GuideCenter/GuideBlock/GuideBlockSection.swift
@@ -0,0 +1,20 @@
+//
+//  GuideBlockSection.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct GuideBlockSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuideBlockSection_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideBlockSection()
+    }
+}
