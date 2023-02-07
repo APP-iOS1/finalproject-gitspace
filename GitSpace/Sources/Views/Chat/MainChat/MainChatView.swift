fd11e79b333427ed250158a78171cbdf7e773813
Taeyoung Won <hexter1994@gmail.com>
2023-02-07 16:40:52 +0900

    [Chore] #132 코드 라인 컨벤션 반영

--- GitSpace/Sources/Views/Chat/MainChat/MainChatView.swift
+++ GitSpace/Sources/Views/Chat/MainChat/MainChatView.swift
@@ -16,9 +16,7 @@ struct MainChatView: View {
         ScrollView {
             ChatUserRecommendationSection()
 				.padding()
-            
             Divider()
-            
             ChatListSection()
         }
         // FIXME: - 추후 네비게이션 타이틀 지정 (작성자: 제균)
