77eb3c81b60dcd7822c190677a85b72d342ad5b2
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-07 15:05:31 +0900

    [Add] #96 GuideReportSection / ReportGuideView 추가
    # GuideReportSection / ReportGuideView 추가

---
+++ GitSpace/Sources/Views/GuideCenter/GuideReport/GuideReportSection.swift
@@ -0,0 +1,20 @@
+//
+//  GuideReportSection.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct GuideReportSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuideReportSection_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideReportSection()
+    }
+}
---
+++ GitSpace/Sources/Views/GuideCenter/GuideReport/ReportGuideView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportGuideView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct ReportGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReportGuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportGuideView()
+    }
+}
