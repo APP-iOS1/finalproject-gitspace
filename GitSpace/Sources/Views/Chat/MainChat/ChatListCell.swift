348d1cff16ade0084e02d257028e231c10c24b01
Taeyoung Won <hexter1994@gmail.com>
2023-02-07 11:52:00 +0900

    [Chore] #112 Chat List Cell 파일 분리

---
+++ GitSpace/Sources/Views/Chat/MainChat/ChatListCell.swift
@@ -0,0 +1,51 @@
+//
+//  ChatListCell.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/02/07.
+//
+
+import SwiftUI
+
+struct ChatListCell : View {
+    
+    var chat: Chat
+    var targetUserID: String
+    @State private var targetName: String = ""
+    @EnvironmentObject var userStore : UserStore
+    @EnvironmentObject var chatStore: ChatStore
+    @State var opacity: Double = 0.4
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            HStack{
+                ProfileAsyncImage(size: 55)
+                    .padding(.trailing)
+
+                
+                VStack(alignment: .leading) {
+                    Text("@\(targetUserID)")
+                        .font(.title3)
+                        .bold()
+                        .padding(.bottom, 5)
+                        .lineLimit(1)
+                        .modifier(BlinkingSkeletonModifier(opacity: opacity, shouldShow: !chatStore.isFetchFinished))
+                    
+                    Text(chat.lastContent)
+                        .foregroundColor(.gray)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .modifier(BlinkingSkeletonModifier(opacity: opacity, shouldShow: !chatStore.isFetchFinished))
+                }
+            }
+            .frame(width: 330,height: 100, alignment: .leading)
+            Divider()
+                .frame(width: 350)
+        }
+        .task {
+            withAnimation(.linear(duration: 1.0).repeatForever(autoreverses: true)){
+                self.opacity = opacity == 0.4 ? 0.8 : 0.4
+            }
+        }
+
+    }
+}
