2a497076c97a093bea319bff59a56b3686e81a6f
Taeyoung Won <hexter1994@gmail.com>
2023-02-07 11:46:20 +0900

    [Move] #112 Chat 하위 디렉토리 폴더링 분리

---
+++ GitSpace/Sources/Views/Chat/MainChat/ChatListSection.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct ChatListSection: View {
+    
+    @EnvironmentObject var chatStore: ChatStore
+    
+    var body: some View {
+        // MARK: -Constant : 채팅방 리스트를 최근순으로 정렬한 리스트
+        VStack(alignment: .leading) {
+            Text("My Penpals")
+                .font(.footnote)
+                .foregroundColor(Color.gray)
+            
+            if chatStore.isFetchFinished {
+                // 채팅방 목록 리스트
+                ForEach(chatStore.targetUserID.indices, id: \.self) { i in
+                    
+                    NavigationLink {
+                        ChatRoomView(chat: chatStore.chats[i])
+                    } label: {
+                        ChatListCell(chat: chatStore.chats[i],
+                                     targetUserID: chatStore.targetUserID[i])
+                            .foregroundColor(.black)
+                    }
+                }
+            } else {
+                ForEach(0..<5, id: \.self) { i in
+                    ChatListSkeletonCell()
+                }
+            }
+        }
+        .task{
+            await chatStore.fetchChats()
+        }
+    }
+}
+
+struct ChatListSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatListSection()
+            .environmentObject(ChatStore())
+            .environmentObject(UserStore())
+    }
+}
