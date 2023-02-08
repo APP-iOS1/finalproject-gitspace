eaca1c2eb86875fd7b7488edd9a662551df31fc6
dahae0320 <ldh990320ldh@gmail.com>
2023-02-08 16:21:07 +0900

    [Add] #144 Add SigninView

---
+++ GitSpace/Sources/Views/Auth/SigninView.swift
@@ -0,0 +1,41 @@
+//
+//  SigninView.swift
+//  GitSpace
+//
+//  Created by Da Hae Lee on 2023/02/08.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack {
+                Image("GitSpace-Activity")
+                    .padding(.top, 20)
+                GSText.CustomTextView(style: .title1, string: "Hello, GitSpacer!")
+                GSText.CustomTextView(style: .body1, string: "Welcome to GitSpace.")
+                    .padding(5)
+            }
+            
+            Spacer()
+            
+            GSButton.CustomButtonView(
+                style: .primary(isDisabled: false)
+            ) {
+                print("signin button tapped")
+            } label: {
+                Text("**GitHub Signin**")
+            }
+            .padding()
+
+        }
+    }
+}
+
+struct SigninView_Previews: PreviewProvider {
+    static var previews: some View {
+        SigninView()
+    }
+}
