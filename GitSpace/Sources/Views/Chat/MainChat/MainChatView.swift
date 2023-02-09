7f0588d98ce98c3e1dcefcd6770af43771f54c9e
Taeyoung Won <hexter1994@gmail.com>
2023-02-09 15:52:58 +0900

    [Chore] #132 로그인 상태 확인용 Text View 추가

--- GitSpace/Sources/Views/Chat/MainChat/MainChatView.swift
+++ GitSpace/Sources/Views/Chat/MainChat/MainChatView.swift
@@ -17,6 +17,12 @@ struct MainChatView: View {
         ScrollView {
             ChatUserRecommendationSection()
 				.padding()
+            
+            Divider()
+            
+            Text("채팅방 개수 : \(chatStore.chats.count)")
+            Text("로그인 유저 ID : \(Utility.loginUserID)")
+            
             Divider()
             ChatListSection()
         }
