1bb1e46fb0e27ad20bd191b767f44d15ac3f5ad3
Yeseul Chung <amy7806@gmail.com>
2023-02-10 01:52:28 +0900

    [Fix] #132 새로운 메시지 추가 시 targetUserName 수정 안되는 버그 해결
    Chat 배열과 targetUserName 배열을 따로 두고 관리하였으나 새로운 메시지 추가 시 Chat과 targetUserName의 매칭에 실패하는 버그가 발생해왔음.
    targetUserName 배열을 삭제하고 ChatID와 targetUserName을 딕셔너리로 연결하여 ChatID의 순서가 바뀔 때 참조하는 targetUserName은 그대로 유지되도록 변경하여 버그를 수정하였음.

--- GitSpace/Sources/ViewModels/ChatViewModel.swift
+++ GitSpace/Sources/ViewModels/ChatViewModel.swift
@@ -4,9 +4,9 @@
 /// 3. remove할 때 인덱스 번호로 찾아가서 걔만 지울 수 있는지
 ///
 // TODO: 채팅 관련 작업
-// 1. 채팅방 전역으로 알림 띄우기
+// 1. 채팅방 전역으로 알림 띄우기 : 쥬니네 FCM 기술연구 후에..
 // 2. ScrollView == 카카오톡
-// 3. 채팅방 데이터 추가하고 채팅 리스트 Listener 테스트하기
+// 3. 채팅방 데이터 추가하고 채팅 리스트 Listener 테스트하기 : 완~
 
 // MEMO: Chat List Listener 단일 패치와 노크 승인 시점에 대한 논의
 /// 1. 노크 승인에 의해 새로운 채팅방이 개설되었을 때, .added에 의해서 해당 채팅방 하나만 로컬 배열에 추가하고, 로컬에서 정렬한다.
@@ -15,13 +15,21 @@
 /// 3-1. Knock 쪽에서 승인 시점에 Knock 메세지를 Chat 모델 lastContent에 업데이트 해주는 방법
 /// 3-2. lastContent가 ""일 때 혹은 메세지 배열이 empty일 때, knock message를 View에서 표시
 
+// TODO: 230210 기준 남은 작업 리스트
+/// 1. ChatRoomInfoView 구현
+/// 2. remove에 대한 lastContent 업데이트 분기 처리
+/// 3. ScrollView Reader 완성 (개어려움)
+/// 4. 메세지 인앱 알림 처리
+/// 5. TextEditor 로직 구현 + 이미지 디자인 시스템 구현 (영이꺼)
+/// 6. 안읽은 메시지 (리스트에선 갯수, chat room에선 스크롤 시작 위치)
+
 import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 final class ChatStore: ObservableObject {
     
-    @Published var targetUserNames: [String]
+    var targetNameDict: [String : String]
     @Published var chats: [Chat]
     @Published var isDoneFetch: Bool // 스켈레톤 UI를 종료하기 위한 변수
     
@@ -30,7 +38,7 @@ final class ChatStore: ObservableObject {
     
     init() {
         chats = []
-        targetUserNames = []
+        targetNameDict = [:]
         isDoneFetch = false
     }
 }
@@ -146,7 +154,6 @@ extension ChatStore {
         let snapshot = await getChatDocuments()
         // MARK: Memo - 함수 내부 배열에 추가 -> Published에 덮어쓰기 로직으로 removeAll 없이 정상 작동함 by.태영
         var newChats: [Chat] = []
-        var newTargetUserNames: [String] = []
         
         if let snapshot {
             for document in snapshot.documents {
@@ -154,7 +161,7 @@ extension ChatStore {
                     let chat: Chat = try document.data(as: Chat.self)
                     let targetUserName: String = await chat.targetUserName
                     newChats.append(chat)
-                    newTargetUserNames.append(targetUserName)
+                    targetNameDict[chat.id] = targetUserName
                 } catch {
                     print("Fetch Chat Error : \(error)")
                 }
@@ -164,35 +171,29 @@ extension ChatStore {
         // MARK: Memo - Published에 관여하는 파트만 main thread를 사용하기 위해 @MainActor를 삭제하고 부분적으로 main thread 사용 by. 태영
         // 230207 추가 : 동시성 코드에서 변경할 수 없는 프로퍼티 혹은 인스턴스를 변경하는 것은 불가. MainActor로 다시 교체
         chats = newChats
-        targetUserNames = newTargetUserNames
         self.isDoneFetch = true
     }
     
     // MARK: -Chat CRUD
-    func addChat(_ chat: Chat) async {
+    func addChat(_ chat: Chat) {
         do {
             try db.collection("Chat")
                 .document(chat.id)
                 .setData(from: chat.self)
-            await fetchChats()
         } catch { }
     }
     
-    func updateChat(_ chat: Chat) async {
-        do {
-            try await db.collection("Chat")
-                .document(chat.id)
-                .updateData(["lastDate" : chat.lastDate,
-                             "lastContent" : chat.lastContent])
-            await fetchChats()
-        } catch { }
+    func updateChat(_ chat: Chat) {
+        db.collection("Chat")
+            .document(chat.id)
+            .updateData(["lastDate" : chat.lastDate,
+                         "lastContent" : chat.lastContent])
+        
     }
     
-    func removeChat(_ chat: Chat) async {
-        do {
-            try await db.collection("Chat")
-                .document(chat.id).delete()
-            await fetchChats()
-        } catch { }
+    func removeChat(_ chat: Chat) {
+        db.collection("Chat")
+            .document(chat.id)
+            .delete()
     }
 }
