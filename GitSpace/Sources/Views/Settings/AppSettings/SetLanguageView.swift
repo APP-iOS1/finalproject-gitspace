6d855b212cff59051c962ce6cc6c151ccb29a402
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-09 13:15:58 +0900

    [Add] #146 SetLanguageView 파일 추가
    # SetLanguageView 파일 추가

---
+++ GitSpace/Sources/Views/Settings/AppSettings/SetLanguageView.swift
@@ -0,0 +1,20 @@
+//
+//  SetLanguageView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/09.
+//
+
+import SwiftUI
+
+struct SetLanguageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetLanguageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetLanguageView()
+    }
+}
