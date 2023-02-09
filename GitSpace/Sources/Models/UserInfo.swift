f6cf8eb787042555aecfa10b8dc87b28eae05827
dahae0320 <ldh990320ldh@gmail.com>
2023-02-09 16:15:09 +0900

    [Chore] #144 GitHubUser 모델 변경

--- GitSpace/Sources/Models/UserInfo.swift
+++ GitSpace/Sources/Models/UserInfo.swift
@@ -24,19 +24,9 @@ struct UserInfo : Identifiable, Codable {
     }
 }
 
-struct UserInfoTemp : Identifiable, Codable {
-    // MARK: -Properties
-    var id : Int // 유저 ID
-    var name : String // 유저 닉네임
-    var email : String? // 유저 이메일
-//    var date : Date // 유저 회원가입 일시
-    
-    // MARK: -Func : Double 타입 Date를 문자열로 반환하는 함수
-//    var stringDate : String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.locale = Locale(identifier: "ko_kr")
-//        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        return dateFormatter.string(from: date)
-//    }
+struct GitHubUser: Identifiable, Codable {
+    var id : Int       // 고유 id
+    var login: String   // github id
+    var name : String?  // username
+    var email : String? // email
 }
