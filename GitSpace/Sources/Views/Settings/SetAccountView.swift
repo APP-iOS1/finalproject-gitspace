dc03edfa377f907764fd8683787db701cdf7238d
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-02-09 11:48:33 +0900

    [Add] #146 SetAccount / SetPrivacySafetyView 파일 추가
    # SetAccount / SetPrivacySafetyView 파일 추가

---
+++ GitSpace/Sources/Views/Settings/SetAccountView.swift
@@ -0,0 +1,20 @@
+//
+//  SetAccountView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/09.
+//
+
+import SwiftUI
+
+struct SetAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetAccountView()
+    }
+}
---
+++ GitSpace/Sources/Views/Settings/SetPrivacySafetyView.swift
@@ -0,0 +1,20 @@
+//
+//  SetPrivacySafetyView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/02/09.
+//
+
+import SwiftUI
+
+struct SetPrivacySafetyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetPrivacySafetyView: PreviewProvider {
+    static var previews: some View {
+        SetPrivacySafetyView()
+    }
+}
