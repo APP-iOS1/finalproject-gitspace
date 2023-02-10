f0f18d032723cbdcca094f3acfe31e42a0650ab5
Taeyoung Won <hexter1994@gmail.com>
2023-02-10 18:51:47 +0900

    [Chore] #135 프로퍼티 수정, 더미데이터에서 변수 참조로 변경

--- GitSpace/Sources/Views/Chat/ChatRoom/ChatRoomInfoView.swift
+++ GitSpace/Sources/Views/Chat/ChatRoom/ChatRoomInfoView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct ChatRoomInfoView: View {
-
-    let targetName: String
-    @State private var notificationStop : Bool = false
-    @State private var isBlocked : Bool = false
-    @State private var showingBlockAlert : Bool = false
-    @State private var showingUnblockAlert : Bool = false
-    @State private var showingDeleteChatAlert : Bool = false
-    @State private var showingBlockMessage : Bool = false
-    @State private var showingDeleteMessage : Bool = false
     
+    let chatID: String
+    let targetName: String // 음... 그러면 유저 ID나 객.......... 로그인한 유저 객체? 패치받아야되나
+    @State var isBlocked: Bool // UserInfo에서 blockedUserIDs를 통해서 계산해서 init 필요
+    @State var isNotificationReceiveEnable: Bool // UserDefault에서 읽어와서 할당해야 함
+    
+    @State private var showingBlockAlert: Bool = false
+    @State private var showingUnblockAlert: Bool = false
+    @State private var showingDeleteChatAlert: Bool = false
+    @State private var showingBlockMessage: Bool = false
+    @State private var showingDeleteMessage: Bool = false
     
     var body: some View {
         VStack {
@@ -32,12 +33,11 @@ struct ChatRoomInfoView: View {
                 
                 VStack(alignment: .leading) {
                     // 알림
-                    Text("Notifications")
-                        .font(.title3)
-                        .bold()
+                    GSText.CustomTextView(style: .title2,
+                                          string: "Notification")
                     
                     // 알림 일시중지
-                    Toggle("Snooze notifications", isOn: $notificationStop)
+                    Toggle("Snooze notifications", isOn: $isNotificationReceiveEnable)
                 }
                 
                 Divider()
@@ -52,7 +52,9 @@ struct ChatRoomInfoView: View {
                 }
                 
             } label: {
-                Text("\(isBlocked ? "Unblock" : "Block") @wontaeyoung")
+                Text(isBlocked ? "Unblock" : "Block") +
+                Text(" \(targetName)")
+                    .bold()
             }
             .padding(.vertical, 20)
 
@@ -60,33 +62,25 @@ struct ChatRoomInfoView: View {
                 showingDeleteChatAlert.toggle()
             } label: {
                 Text("Delete conversation")
+                    .foregroundColor(.gsRed)
             }
 
             Spacer()
         }
-        .overlay(alignment: .bottom) {
-            if showingBlockMessage {
-                Text(isBlocked ? "Blocked" : "Unblocked")
-                    .font(.largeTitle.bold())
-            } else if showingDeleteMessage {
-                Text("Conversation has been deleted.")
-                    .font(.largeTitle.bold())
-            }
-        }
         .padding(.horizontal, 20)
-        .alert("Block @wontaeyoung", isPresented: $showingBlockAlert) {
+        .onChange(of: isNotificationReceiveEnable, perform: { newValue in
+            UserDefaults().set(newValue, forKey: Constant.AppStorageConst.CHATROOM_NOTIFICATION + chatID)
+        })
+        .alert("Block @\(targetName)", isPresented: $showingBlockAlert) {
             Button("Block", role: .destructive) {
                 isBlocked.toggle()
                 showingBlockMessage = true
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    showingBlockMessage = false
-                }
             }
         } message: {
 //상대방을 차단하면 상대방이 보내는 메세지를 더 이상 볼 수 없습니다. 차단하시겠습니까?
-            Text("@wontaeyoung will no longer be able to follow or message you, and you will not see notifications from @wontaeyoung")
+            Text("@\(targetName) will no longer be able to follow or message you, and you will not see notifications from @wontaeyoung")
         }
-        .alert("Unblock @wontaeyoung", isPresented: $showingUnblockAlert) {
+        .alert("Unblock @\(targetName)", isPresented: $showingUnblockAlert) {
             Button("Unblock", role: .destructive) {
                 isBlocked.toggle()
                 showingBlockMessage = true
@@ -96,7 +90,7 @@ struct ChatRoomInfoView: View {
             }
         } message: {
             // 차단을 해제하면 상대방이 보내는 메세지를 다시 받을 수 있습니다. 차단 해체하시겠습니까?
-            Text("@wontaeyoung will be able to follow or message you, and you will see notifications from @wontaeyoung")
+            Text("@\(targetName) will be able to follow or message you, and you will see notifications from @\(targetName)")
         }
         .alert("Delete conversation?", isPresented: $showingDeleteChatAlert) {
             Button("Delete", role: .destructive) {
@@ -114,7 +108,9 @@ struct ChatRoomInfoView: View {
 
 struct ChatRoomInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatRoomInfoView(targetName: "test")
+        ChatRoomInfoView(chatID: "a", targetName: "test", isBlocked: false)
         
     }
 }
+
+
