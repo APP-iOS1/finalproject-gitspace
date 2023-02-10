cf72bbd0e5000f0481360817ec206cfd84566268
valseLee <sollleky72@gmail.com>
2023-02-10 18:50:00 +0900

    [Feat] #134 push 알람을 탭 했을 때 trigger할 바인딩 불 구현

--- GitSpace/AppState.swift
+++ GitSpace/AppState.swift
@@ -5,4 +5,19 @@
 //  Created by 이승준 on 2023/02/09.
 //
 
-import Foundation
+import SwiftUI
+
+final class AppState: ObservableObject {
+	static let shared = AppState()
+	@Published var pageNavigationTo: GSTabBarRouter.Page?
+	
+	var navigationBindingActive: Binding<Bool> {
+		.init { () -> Bool in
+			self.pageNavigationTo != nil
+		} set: { (newValue) in
+			if !newValue { self.pageNavigationTo = nil }
+		}
+	}
+	
+	private init () {}
+}
