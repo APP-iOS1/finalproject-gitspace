4300d7c09bc3f1a037b408d8e1c9207d34aad59e
Taeyoung Won <hexter1994@gmail.com>
2023-02-10 18:53:23 +0900

    [Chore] #135 상대방 이름 요청 로직 구현내용 반영

--- GitSpace/Sources/Views/Chat/MainChat/ChatListCell.swift
+++ GitSpace/Sources/Views/Chat/MainChat/ChatListCell.swift
@@ -17,8 +17,8 @@ struct ChatListCell : View {
     @State var opacity: Double = 0.4
     
     var body: some View {
-        VStack(alignment: .leading){
-            HStack{
+        VStack(alignment: .leading) {
+            HStack {
                 ProfileAsyncImage(size: 55)
                     .padding(.trailing)
 
@@ -36,16 +36,22 @@ struct ChatListCell : View {
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .modifier(BlinkingSkeletonModifier(opacity: opacity, shouldShow: !chatStore.isDoneFetch))
                 }
-            }
-            .frame(width: 330,height: 100, alignment: .leading)
+            }            .frame(width: 330,height: 100, alignment: .leading)
             Divider()
                 .frame(width: 350)
         }
         .task {
             targetName = await chat.targetUserName
-            withAnimation(.linear(duration: 1.0).repeatForever(autoreverses: true)){
+            withAnimation(.linear(duration: 1.0).repeatForever(autoreverses: true)) {
                 self.opacity = opacity == 0.4 ? 0.8 : 0.4
+            }
+        }
+        .onChange(of: chatStore.isListenerModified) { n in
+            Task {
+                targetName = await chat.targetUserName
             }
         }
     }
 }
+
+
