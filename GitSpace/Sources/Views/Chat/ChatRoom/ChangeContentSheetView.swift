9929a501afd433615f4821570de18486c1e3793d
Taeyoung Won <hexter1994@gmail.com>
2023-02-12 02:18:45 +0900

    [Chore] #169 Message 모델 프로퍼티 이름 변경 내용 반영

--- GitSpace/Sources/Views/Chat/ChatRoom/ChangeContentSheetView.swift
+++ GitSpace/Sources/Views/Chat/ChatRoom/ChangeContentSheetView.swift
@@ -18,7 +18,7 @@ struct ChangeContentSheetView : View {
     var body: some View {
         VStack(spacing : 50) {
             
-            TextField(message.content, text: $changeContentField)
+            TextField(message.textContent, text: $changeContentField)
                 .textFieldStyle(.roundedBorder)
             
             Button {
@@ -32,7 +32,7 @@ struct ChangeContentSheetView : View {
         }
         .padding(.horizontal, 20)
         .onAppear {
-            changeContentField = message.content
+            changeContentField = message.textContent
         }
     }
 }
