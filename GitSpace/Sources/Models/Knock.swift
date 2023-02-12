64625fd16abc29dc5a6f9fb01d6dd73d8e013a7e
valseLee <sollleky72@gmail.com>
2023-02-12 22:23:36 +0900

    [Update] #154 Knock 구조체 모델링, 속성 이름은 변동 가능

--- GitSpace/Sources/Models/Knock.swift
+++ GitSpace/Sources/Models/Knock.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Knock: Codable, Hashable {
+	var id: String = UUID().uuidString
+	var date: Date
+	var knockMessage: String
+	var knockStatus: String
+	var knockCategory: String
+	var declineMessage: String?
+	var receivedUserName: String
+	var sentUserName: String
+	
+	var dateDiff: Int {
+		get {
+			let diff = Date.now - date
+			return diff.minute ?? 0
+		}
+	}
+}
