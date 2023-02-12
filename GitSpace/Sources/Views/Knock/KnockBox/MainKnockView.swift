6b0c084e30af76d70392650573c7f986f85e69e0
valseLee <sollleky72@gmail.com>
2023-02-13 00:51:46 +0900

    [Feat/Refactor] #154 메인 뷰의 셀을 그리는 로직 리팩토링 완료, 섹션헤더에 검색 결과와 각 상태의 노크가 몇 개인지 보여주는 로직 구현 완료

--- GitSpace/Sources/Views/Knock/KnockBox/MainKnockView.swift
+++ GitSpace/Sources/Views/Knock/KnockBox/MainKnockView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MainKnockView: View {
     @ObservedObject var knockHistoryViewModel = KnockHistoryViewModel()
+	@ObservedObject var knockViewManager = KnockViewManager()
     @StateObject private var keyboardHandler = KeyboardHandler()
     
 	// !!!: TAB Branch
@@ -24,7 +25,7 @@ struct MainKnockView: View {
     @State private var searchText: String = ""
     
     @State var isEdit: Bool = false
-    @State var checked: Bool = false
+    @State var isEditChecked: Bool = false
     @State var isDisplayedKnockSettingView: Bool = false
     
     var body: some View {
@@ -56,24 +57,70 @@ struct MainKnockView: View {
                         .offset(y: 3.5)
                 }
                 .padding(.top, 10)
-                .padding(.horizontal, 16)
+                .padding(.horizontal, 20)
             
 			// 검색중 vs 기본 조건분기
 			// 셀 그리는 로직은 고정
-			if isSearching {
-				ScrollView {
-					LazyVStack(pinnedViews: .sectionHeaders) {
+			ScrollView {
+					// MARK: - 수신 탭
+					if userSelectedTab == Constant.KNOCK_RECEIVED {
+						LazyVStack(pinnedViews: .sectionHeaders) {
+							switch userFilteredKnockState {
+							case .waiting, .accepted, .declined:
+								eachCellSection(
+									knockList: knockViewManager.receivedKnockList
+								)
+							case .all:
+								eachCellSection(
+									knockList: knockViewManager.receivedKnockList,
+									eachFilteredState: .waiting
+								)
+								
+								eachCellSection(
+									knockList: knockViewManager.receivedKnockList,
+									eachFilteredState: .accepted
+								)
+								
+								eachCellSection(
+									knockList: knockViewManager.receivedKnockList,
+									eachFilteredState: .declined
+								)
+							}
+						}
+						.transition(knockViewManager.leadingTransition)
 						
+					} else if userSelectedTab == Constant.KNOCK_SENT {
+						LazyVStack(pinnedViews: .sectionHeaders) {
+							switch userFilteredKnockState {
+							case .waiting, .accepted, .declined:
+								eachCellSection(
+									knockList: knockViewManager.sentKnockList
+								)
+							case .all:
+								eachCellSection(
+									knockList: knockViewManager.sentKnockList,
+									eachFilteredState: .waiting
+								)
+								
+								eachCellSection(
+									knockList: knockViewManager.sentKnockList,
+									eachFilteredState: .accepted
+								)
+								
+								eachCellSection(
+									knockList: knockViewManager.sentKnockList,
+									eachFilteredState: .declined
+								)
+							}
+						}
+						.transition(knockViewManager.trailingTransition)
 					}
-				}
-			} else {
-				ScrollView {
-					LazyVStack(pinnedViews: .sectionHeaders) {
-						Text("/")
-					}
-				}
+				
 			}
         } // VStack
+		.task {
+			await knockViewManager.requestKnockList()
+		}
         .onTapGesture {
             self.endTextEditing()
         }
@@ -233,6 +280,123 @@ struct MainKnockView: View {
         }
     }
     
+	@ViewBuilder
+	private func eachCellSection(
+		// 받은 노크 혹은 보낸 노크 리스트가 들어온다.
+		knockList: [Knock],
+		
+		// all 케이스를 그릴 때는 각각 상태를 전달해서 그린다.
+		eachFilteredState: KnockStateFilter? = nil
+	) -> some View {
+		Section {
+			ForEach(
+				!isSearching
+				? knockList
+					.sorted { knockViewManager.compareTwoKnockWithStatus(lhs: $0, rhs: $1) }
+				
+				: knockList
+					.sorted { knockViewManager.compareTwoKnockWithStatus(lhs: $0, rhs: $1) }
+					.filter {
+						userSelectedTab == Constant.KNOCK_RECEIVED
+						? $0.sentUserName.contains(searchText, isCaseInsensitive: true)
+						: $0.receivedUserName.contains(searchText, isCaseInsensitive: true)
+					}
+				,
+				id: \.id) { eachKnock in
+					if let eachFilteredState,
+					   eachFilteredState.rawValue == eachKnock.knockStatus {
+						Text(eachKnock.receivedUserName)
+					} else if userFilteredKnockState.rawValue == eachKnock.knockStatus {
+						Text(eachKnock.receivedUserName)
+					}
+			}
+		} header: {
+			switch userFilteredKnockState {
+			case .waiting, .accepted, .declined:
+				if getKnockCountInKnockList(
+					knockList: knockList,
+					equalsToKnockStatus: userFilteredKnockState.rawValue
+				) == 0 {
+					EmptyView()
+				} else {
+					HStack {
+						Text(userFilteredKnockState.rawValue)
+							.bold()
+							.font(.headline)
+						
+						Spacer()
+						
+						Text(
+							getKnockCountInKnockList(
+								knockList: knockList,
+								equalsToKnockStatus: userFilteredKnockState.rawValue
+							)
+							.description
+						)
+						.bold()
+					}
+					.modifier(PinnedViewHeaderModifier())
+				}
+			case .all:
+				if getKnockCountInKnockList(
+					knockList: knockList,
+					equalsToKnockStatus: eachFilteredState?.rawValue ?? ""
+				) == 0 {
+					EmptyView()
+				} else {
+					HStack {
+						Text(eachFilteredState?.rawValue ?? "")
+							.bold()
+						
+						Spacer()
+						
+						Text(
+							getKnockCountInKnockList(
+								knockList: knockList,
+								equalsToKnockStatus: eachFilteredState?.rawValue ?? "")
+							.description
+						)
+						.foregroundColor(Color.gsGray2)
+					}
+					.modifier(PinnedViewHeaderModifier())
+				}
+			}
+		}
+	}
+	
+	private func getKnockCountInKnockList(
+		knockList: [Knock],
+		equalsToKnockStatus: String
+	) -> Int {
+		knockList
+			.filter {
+				filteringSearchText(
+					knock: $0,
+					equalsToKnockStatus: equalsToKnockStatus
+				)
+			}
+			.count
+	}
+	
+	// 검색하고 상태값 맞춘거에 따라 필터링 할 수 있도록 구현해라
+	// 370~372 자리에 들어갈 로직 짜면댐
+	private func filteringSearchText(
+		knock: Knock,
+		equalsToKnockStatus: String
+	) -> Bool {
+		if isSearching, userSelectedTab == Constant.KNOCK_RECEIVED { // 검색중 + 내 수신함
+			// 현재 내가 선택한 필터 옵션과 같아야 하고, 내 수신함에는 보낸 사람의 정보를 가져야 한다.
+			return knock.knockStatus == equalsToKnockStatus && knock.sentUserName.contains(searchText, isCaseInsensitive: true)
+		} else if isSearching, userSelectedTab == Constant.KNOCK_SENT { // 검색중 + 내 발신함
+			return knock.knockStatus == equalsToKnockStatus && knock.receivedUserName.contains(searchText, isCaseInsensitive: true)
+		} else if userSelectedTab == Constant.KNOCK_RECEIVED {
+			return knock.knockStatus == equalsToKnockStatus
+		} else if userSelectedTab == Constant.KNOCK_SENT {
+			return knock.knockStatus == equalsToKnockStatus
+		}
+		return false
+	}
+	
     @ViewBuilder
     private func eachCellSectionBuilder(
         knockList: [Knock],
@@ -267,20 +431,20 @@ struct MainKnockView: View {
                         } label: {
                             if userSelectedTab == receivedKnockTab {
                                 EachKnockCell(
-                                    knockMessenger: "from: \(eachKnock.senderID)",
+									userSelectedTab: "from: \(eachKnock.sentUserName)",
                                     knockHistoryViewModel: knockHistoryViewModel,
                                     eachKnock: eachKnock,
                                     isEdit: $isEdit,
-                                    checked: $checked
+                                    checked: $isEditChecked
                                 )
                                 .foregroundColor(.primary)
                             } else {
                                 EachKnockCell(
-                                    knockMessenger: "to: \(eachKnock.receiverID)",
+									userSelectedTab: "to: \(eachKnock.receivedUserName)",
                                     knockHistoryViewModel: knockHistoryViewModel,
                                     eachKnock: eachKnock,
                                     isEdit: $isEdit,
-                                    checked: $checked
+                                    checked: $isEditChecked
                                 )
                                 .foregroundColor(.primary)
                             }
@@ -312,7 +476,7 @@ struct MainKnockView: View {
             Divider()
         }
     }
-} // MyKnockBoxView()
+} // KnockBoxView()
 
 struct KnockBoxView_Previews: PreviewProvider {
     static var previews: some View {
