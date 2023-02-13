22f80aa23124a0bd09470fa2ba681afb21577c9c
valseLee <sollleky72@gmail.com>
2023-02-13 16:21:35 +0900

    [Feat] #154 영어 소대문자 구분 없이 글자 포함여부를 확인할 수 있는 확장 구현

--- GitSpace/Utilities/Extensions/String+.swift
+++ GitSpace/Utilities/Extensions/String+.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-extension Array where Element == String {
-	/// 배열 원소가 문자열일 때, 영어의 소대문자 구분 없이 문자열을 포함하는지를 확인 후 Bool 을 리턴하는 메소드.
-	public func caseInsensitiveContains(_ element: Element) -> Bool {
-		contains { $0.caseInsensitiveCompare(element) == .orderedSame }
+extension String {
+	/// 영어의 소대문자 구분 없이 문자열을 포함하는지를 확인 후 Bool 을 리턴하는 메소드.
+	public func contains(
+		_ string: String,
+		isCaseInsensitive caseInsensitive: Bool
+	) -> Bool {
+		caseInsensitive
+		? range(of: string, options: .caseInsensitive) != nil
+		: contains(string)
 	}
 }
