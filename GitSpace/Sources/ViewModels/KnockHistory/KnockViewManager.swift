28840fd8d889e1b2ce47c5b9a3804c97357802c2
valseLee <sollleky72@gmail.com>
2023-02-13 16:20:13 +0900

    [Feat] #154 Network CRUD 일부, 뷰 그리는 로직을 함께 정리하여 구현

--- GitSpace/Sources/ViewModels/KnockHistory/KnockViewManager.swift
+++ GitSpace/Sources/ViewModels/KnockHistory/KnockViewManager.swift
@@ -9,7 +9,7 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import SwiftUI
 
-final class KnockNetworkManager: ObservableObject {
+final class KnockViewManager: ObservableObject {
 	@Published var eachKnock: Knock?
 	@Published var receivedKnockList: [Knock] = []
 	@Published var sentKnockList: [Knock] = []
@@ -28,7 +28,63 @@ final class KnockNetworkManager: ObservableObject {
 			insertion: .move(edge: .leading),
 			removal: .move(edge: .leading)
 		)
+}
+
+extension KnockViewManager {
+	public func compareTwoKnockWithStatus(lhs: Knock, rhs: Knock) -> Bool {
+		if lhs.knockStatus == Constant.KNOCK_WAITING, rhs.knockStatus == Constant.KNOCK_DECLINED { return true }
+		else if lhs.knockStatus == Constant.KNOCK_WAITING, rhs.knockStatus == Constant.KNOCK_ACCEPTED { return true }
+		else if lhs.knockStatus == Constant.KNOCK_ACCEPTED, rhs.knockStatus == Constant.KNOCK_DECLINED { return true }
+		else if lhs.knockStatus == Constant.KNOCK_ACCEPTED, rhs.knockStatus == Constant.KNOCK_WAITING { return false }
+		else if lhs.knockStatus == Constant.KNOCK_DECLINED, rhs.knockStatus == Constant.KNOCK_ACCEPTED { return false }
+		else if lhs.knockStatus == Constant.KNOCK_DECLINED, rhs.knockStatus == Constant.KNOCK_WAITING { return false }
+		
+		return true
+	}
 	
+	public func getKnockCountInKnockList(
+		knockList: [Knock],
+		equalsToKnockStatus: String,
+		isSearching: Bool,
+		searchText: String,
+		userSelectedTab: String
+	) -> Int {
+		knockList
+			.filter {
+				filteringSearchText(
+					knock: $0,
+					equalsToKnockStatus: equalsToKnockStatus,
+					isSearching: isSearching,
+					searchText: searchText,
+					userSelectedTab: userSelectedTab
+				)
+			}
+			.count
+	}
+	
+	private func filteringSearchText(
+		knock: Knock,
+		equalsToKnockStatus: String,
+		isSearching: Bool,
+		searchText: String,
+		userSelectedTab: String
+	) -> Bool {
+		if isSearching, userSelectedTab == Constant.KNOCK_RECEIVED { // 검색중 + 내 수신함
+																	 // 현재 내가 선택한 필터 옵션과 같아야 하고, 내 수신함에는 보낸 사람의 정보를 가져야 한다.
+			return knock.knockStatus == equalsToKnockStatus && knock.sentUserName.contains(searchText, isCaseInsensitive: true)
+		} else if isSearching, userSelectedTab == Constant.KNOCK_SENT { // 검색중 + 내 발신함
+			return knock.knockStatus == equalsToKnockStatus && knock.receivedUserName.contains(searchText, isCaseInsensitive: true)
+		} else if userSelectedTab == Constant.KNOCK_RECEIVED {
+			return knock.knockStatus == equalsToKnockStatus
+		} else if userSelectedTab == Constant.KNOCK_SENT {
+			return knock.knockStatus == equalsToKnockStatus
+		}
+		return false
+	}
+}
+
+// MARK: - Network CRUD
+extension KnockViewManager {
 	// MARK: - METHODS
 	public func addSnapshotToKnock() async {
 		firebaseDatabase
@@ -38,7 +94,7 @@ final class KnockNetworkManager: ObservableObject {
 					print("Error fetching documents: \(err!)")
 					return
 				}
-
+				
 				eachSnap.documentChanges.forEach { diff in
 					switch diff.type {
 					case .added:
@@ -103,7 +159,7 @@ final class KnockNetworkManager: ObservableObject {
 			try await document.setData([
 				"id": knock.id,
 				"date": Timestamp(date: knock.date),
-				"declineMessage": knock.declineMessage,
+				"declineMessage": knock.declineMessage ?? "",
 				"knockCategory": knock.knockCategory,
 				"knockStatus": Constant.KNOCK_WAITING,
 				"knockMessage": knock.knockMessage,
@@ -115,4 +171,3 @@ final class KnockNetworkManager: ObservableObject {
 		}
 	}
 }
-
