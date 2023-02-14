5f84fec382fc67b473b4fa8eda14dcb54fe429f8
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-14 23:19:52 +0900

    [Add] #190 기기별 대응을 위한 UIScreen Extension 추가

--- GitSpace/Utilities/Extensions/UIScreen+.swift
+++ GitSpace/Utilities/Extensions/UIScreen+.swift
@@ -5,4 +5,11 @@
 //  Created by 박제균 on 2023/02/14.
 //
 
-import Foundation
+import UIKit
+
+extension UIScreen {
+  /// - Mini, SE: 375.0
+  /// - pro: 390.0
+  /// - pro max: 428.0
+  var isWiderThan375pt: Bool { self.bounds.size.width > 375 }
+}
