2e1365c13f807559235cbae17bf646047cb5f8b2
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-14 23:16:36 +0900

    [Add] #190 Owner 구조체 생성

--- GitSpace/Sources/Network/APIModels/Owner.swift
+++ GitSpace/Sources/Network/APIModels/Owner.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+// MARK: - Owner
+struct Owner: Codable {
+    
+    let login: String
+    let id: Int
+    let nodeID: String
+    let avatarURL: String
+    let gravatarID: String?
+    let url, htmlURL, followersURL: String
+    let followingURL, gistsURL, starredURL: String
+    let subscriptionsURL, organizationsURL, reposURL: String
+    let eventsURL: String
+    let receivedEventsURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case login, id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+    }
+}
+
