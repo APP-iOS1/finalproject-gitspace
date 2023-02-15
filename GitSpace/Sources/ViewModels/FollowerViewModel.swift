461ced0af933b3f9a1c91e57d5e108b3fb5b3839
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-15 18:39:38 +0900

    [Add] #212 MainChatView에서 추천목록 제공을 위한 ViewModel 추가

--- GitSpace/Sources/ViewModels/FollowerViewModel.swift
+++ GitSpace/Sources/ViewModels/FollowerViewModel.swift
@@ -1,8 +1,26 @@
 //
-//  FollowersViewModel.swift
+//  FollowerViewModel.swift
 //  GitSpace
 //
 //  Created by 박제균 on 2023/02/15.
 //
 
 import Foundation
+
+final class FollowerViewModel: ObservableObject {
+    @Published var responses: [FollowersResponse] = []
+    @Published var followers: [GitHubUser] = []
+    
+    @MainActor func requestUsers() async {
+        followers.removeAll()
+        for response in responses {
+            let result = await GitHubService().requestUserInformation(userName: response.login)
+            switch result {
+            case .success(let user):
+                followers.append(user)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
