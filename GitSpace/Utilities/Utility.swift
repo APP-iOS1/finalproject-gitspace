ffee9679ed162dde0da1fef4fd5089320b6ce8c4
Taeyoung Won <hexter1994@gmail.com>
2023-02-15 13:18:41 +0900

    [Chore] #191 loginUserID 기본값 할당

--- GitSpace/Utilities/Utility.swift
+++ GitSpace/Utilities/Utility.swift
@@ -16,8 +16,9 @@ import FirebaseAuth
 
 
 enum Utility {
+    // FIXME: loginUserID -> 유저 객체 참조로 변경하려하였으나, Model 연산 프로퍼티에서 유저 ID가 필요한 로직으로 구성하여 바로 포팅할 수 없음. 이후에 로직 수정 후 유저 객체로 변경 예정 230215 By. 태영
 //     static var loginUserID: String = "aw6BYepF9EhL50sPbhL3NsaD0Fz1"
-    static var loginUserID: String = "PJjxY5xHGZXXsMGqpyWExd50iDP2"
+    static var loginUserID: String = ""
     
     static var MessageCellWidth: CGFloat {
         return UIScreen.main.bounds.width / 5 * 3
