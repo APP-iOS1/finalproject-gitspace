a742554a3c3f68aaeec24212515096229498c434
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-02-15 18:36:21 +0900

    [Add] #212 팔로워 목록 수신을 위한 구조체 생성

--- GitSpace/Sources/Network/APIModels/FollowersResponse.swift
+++ GitSpace/Sources/Network/APIModels/FollowersResponse.swift
@@ -7,182 +7,20 @@
 
 import Foundation
 
-struct FollowersResponse: Codable {
+struct FollowersResponse: Codable, Identifiable, Equatable {
+    
     let id: Int
     let name: String?
     let url: String
     let login: String
     let avatarURL: String
     
-    enum Codingkeys: String, CodingKey {
+    enum CodingKeys: String, CodingKey {
         case id, name, url, login
         case avatarURL = "avatar_url"
     }
     
-    // 이름, 팔로워수, 프로필사진, 레포지토리 수
+    // 이름, 프로필사진,
+    // 팔로워수, 레포지토리 수
     
 }
-//{
-//  "type": "array",
-//  "items": {
-//    "title": "Simple User",
-//    "description": "A GitHub user.",
-//    "type": "object",
-//    "properties": {
-//      "name": {
-//        "type": [
-//          "string",
-//          "null"
-//        ]
-//      },
-//      "email": {
-//        "type": [
-//          "string",
-//          "null"
-//        ]
-//      },
-//      "login": {
-//        "type": "string",
-//        "examples": [
-//          "octocat"
-//        ]
-//      },
-//      "id": {
-//        "type": "integer",
-//        "examples": [
-//          1
-//        ]
-//      },
-//      "node_id": {
-//        "type": "string",
-//        "examples": [
-//          "MDQ6VXNlcjE="
-//        ]
-//      },
-//      "avatar_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://github.com/images/error/octocat_happy.gif"
-//        ]
-//      },
-//      "gravatar_id": {
-//        "type": [
-//          "string",
-//          "null"
-//        ],
-//        "examples": [
-//          "41d064eb2195891e12d0413f63227ea7"
-//        ]
-//      },
-//      "url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat"
-//        ]
-//      },
-//      "html_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://github.com/octocat"
-//        ]
-//      },
-//      "followers_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat/followers"
-//        ]
-//      },
-//      "following_url": {
-//        "type": "string",
-//        "examples": [
-//          "https://api.github.com/users/octocat/following{/other_user}"
-//        ]
-//      },
-//      "gists_url": {
-//        "type": "string",
-//        "examples": [
-//          "https://api.github.com/users/octocat/gists{/gist_id}"
-//        ]
-//      },
-//      "starred_url": {
-//        "type": "string",
-//        "examples": [
-//          "https://api.github.com/users/octocat/starred{/owner}{/repo}"
-//        ]
-//      },
-//      "subscriptions_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat/subscriptions"
-//        ]
-//      },
-//      "organizations_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat/orgs"
-//        ]
-//      },
-//      "repos_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat/repos"
-//        ]
-//      },
-//      "events_url": {
-//        "type": "string",
-//        "examples": [
-//          "https://api.github.com/users/octocat/events{/privacy}"
-//        ]
-//      },
-//      "received_events_url": {
-//        "type": "string",
-//        "format": "uri",
-//        "examples": [
-//          "https://api.github.com/users/octocat/received_events"
-//        ]
-//      },
-//      "type": {
-//        "type": "string",
-//        "examples": [
-//          "User"
-//        ]
-//      },
-//      "site_admin": {
-//        "type": "boolean"
-//      },
-//      "starred_at": {
-//        "type": "string",
-//        "examples": [
-//          "\"2020-07-09T00:17:55Z\""
-//        ]
-//      }
-//    },
-//    "required": [
-//      "avatar_url",
-//      "events_url",
-//      "followers_url",
-//      "following_url",
-//      "gists_url",
-//      "gravatar_id",
-//      "html_url",
-//      "id",
-//      "node_id",
-//      "login",
-//      "organizations_url",
-//      "received_events_url",
-//      "repos_url",
-//      "site_admin",
-//      "starred_url",
-//      "subscriptions_url",
-//      "type",
-//      "url"
-//    ]
-//  }
-//}
