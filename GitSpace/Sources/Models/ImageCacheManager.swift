b740bfe8ed44f24d75b53f725dec75aebd8d975e
Taeyoung Won <hexter1994@gmail.com>
2023-02-20 23:54:01 +0900

    [Chore] #240 SwiftUI 의존성 추가, ImageCacheManager 클래스 추가, 싱글턴 구현, 메모리 부족 알림 상수 추가

--- GitSpace/Sources/Models/ImageCacheManager.swift
+++ GitSpace/Sources/Models/ImageCacheManager.swift
@@ -5,4 +5,9 @@
 //  Created by 원태영 on 2023/02/20.
 //
 
-import Foundation
+import SwiftUI
+
+final class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    private let memoryWarningNotification = UIApplication.didReceiveMemoryWarningNotification
+}
