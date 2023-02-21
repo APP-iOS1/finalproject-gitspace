dcede0cd88b5b7f84dd5b1fad0926995e8e5880c
valseLee <sollleky72@gmail.com>
2023-02-21 18:13:55 +0900

    [Refactor] #218 protocol로 model 정리, 내부 메소드 분리하여 캡슐화

--- GitSpace/Sources/Models/PushNotificationManager.swift
+++ GitSpace/Sources/Models/PushNotificationManager.swift
@@ -7,65 +7,110 @@
 
 import SwiftUI
 
-// TODO: 하위 인스턴스 분리 및 추상화 필요
+protocol GSPushNotificationSendable {
+	var url: URL? { get }
+	
+	/**
+	Notification을 생성하여 발송하는 메소드입니다.
+	 */
+	func sendNotification(
+		with message: PushNotificationMessageType,
+		to userInfo: UserInfo
+	) async -> Void
+	
+	/**
+	 Notification의 payload 데이터를 생성합니다.
+	 - Returns: Data?
+	 */
+	func makeNotificationData(
+		pushNotificationBody: PushNotificationMessageBody,
+		to userInfo: UserInfo
+	) -> Data?
+	
+	/**
+	 Notification의 HTTP Request를 생성합니다.
+	 - Returns: URLRequest
+	 */
+	func configureHTTPRequest(
+		url: URL
+	) -> URLRequest
+}
+
+enum PushNotificationMessageType {
+	case knock(title: String, body: String, fromUser: String, knockID: String)
+	case chat(title: String, body: String, fromUser: String, chatID: String)
+}
+
 final class PushNotificationManager: ObservableObject {
 	private(set) var currentUserDeviceToken: String?
+	private(set) var viewBuildID: String? = "DOCPATH"
 	
-	/// 예시 코드에서 API 키와 테스트용 Device Token은 xcconfig 파일로 캡슐화 하여 사용했습니다.
-	private let serverKey = Bundle.main.object(forInfoDictionaryKey: "SERVER_KEY") as? String ?? ""
+	/// Test를 위한 개인 디바이스 키
 	private let valseDevice = Bundle.main.object(forInfoDictionaryKey: "VALSE_DEVICE_TOKEN") as? String ?? ""
-	private let endpoint = Bundle.main.object(forInfoDictionaryKey: "PUSH_NOTIFICATION_ENDPOINT") as? String ?? ""
-	
+
+	// MARK: - Methods
+	/// private(set) 속성에 접근하여 DeviceToken을 할당합니다.
 	public func setCurrentUserDeviceToken(token: String) {
 		currentUserDeviceToken = token
 	}
 	
-	/// Button을 탭할 때, 아래 메소드를 호출합니다.
-	public func sendPushNotification(
-		with message: GSTabBarRouter.MessageType,
+	/// private(set) 속성에 접근하여 view를 그릴 때 필요한 데이터 ID를 할당합니다.
+	public func assignViewBuildID(_ id: String) {
+		self.viewBuildID = id
+	}
+	
+	// MARK: LIFECYCLE
+	init(
+		currentUserDeviceToken: String?
+	) {
+		self.currentUserDeviceToken = currentUserDeviceToken
+	}
+}
+
+extension PushNotificationManager: GSPushNotificationSendable {
+	var url: URL? {
+		URL(string: "https://\(Constant.PushNotification.PUSH_NOTIFICATION_ENDPOINT)")
+	}
+	
+	public func sendNotification(
+		with message: PushNotificationMessageType,
 		to userInfo: UserInfo
 	) async -> Void {
-		/// 이 url 에는 Legacy HTTP의 엔드포인트가 아규먼트로 전달됩니다.
+		guard let url else { return }
+		let messageBody = PushNotificationMessageBody(message)
+		let httpBody = makeNotificationData(pushNotificationBody: messageBody, to: userInfo)
+		let httpRequest = configureHTTPRequest(url: url)
 		
-		guard let url = URL(string: "https://\(endpoint)" ?? "") else {
-			print("Error: \(#file)-\(#function): NO URL FOR PUSH NOTIFICATION")
-			return
+		do {
+			guard let httpBody else { return }
+			
+			/// 비동기 함수로 정의된 URLSession upload(for:from:) 메소드를 호출합니다.
+			/// uploadPayload는 (Data, Response) 를 갖고 있는 튜플 타입 입니다.
+			let uploadPayload = try await URLSession.shared.upload(
+				for: httpRequest,
+				from: httpBody
+			)
+		} catch {
+			/// POST가 실패했을 경우 에러를 확인할 수 있도록 dump를 호출합니다.
+			dump("DEBUG: PUSH POST FAILED - \(error)")
 		}
-		
-		var messageTitle: String = ""
-		var messageBody: String = ""
-		var fromUserName: String = ""
-		var navigateTo: String = ""
-		var viewBuildID: String = ""
-		
-		switch message {
-		case let .knock(title, body, fromUser, id):
-			messageTitle = title
-			messageBody = body
-			navigateTo = "knock"
-			fromUserName = fromUser
-			viewBuildID = id
-		case let .chat(title, body, fromUser, id):
-			messageTitle = title
-			messageBody = body
-			navigateTo = "chat"
-			fromUserName = fromUser
-			viewBuildID = id
-		}
-		
+	}
+	
+	internal func makeNotificationData(
+		pushNotificationBody: PushNotificationMessageBody,
+		to userInfo: UserInfo
+	) -> Data? {
 		/// HTTP Request의 body로 전달할 data를 딕셔너리로 선언한 후, JSON으로 변환합니다.
 		let json: [AnyHashable: Any] = [
 			/// 특정 기기에 알람을 보내기 위해 "to"를 사용합니다.
 			/// 경우에 따라 Topic 등 다른 용도로 활용할 수 있습니다.
-			/// valse 폰으로 확인하려면  각주를 해제합니다.
-//			"to": valseDevice,
 			"to": userInfo.deviceToken,
 			
 			/// 알람의 내용을 구성하는 키-밸류 입니다.
 			"notification": [
-				"title": messageTitle,
-				"body": messageBody,
-				"subtitle": fromUserName,
+				"title": pushNotificationBody.messageTitle,
+				"body": pushNotificationBody.messageBody,
+				"subtitle": pushNotificationBody.fromUserName,
 				"badge": "1"
 			],
 			
@@ -75,8 +120,70 @@ final class PushNotificationManager: ObservableObject {
 				"sentDeviceToken": currentUserDeviceToken ?? "보낸 이의 디바이스토큰",
 				"sentUserName": userInfo.githubUserName,
 				"sentUserID": userInfo.id,
-				"navigateTo": navigateTo,
-				"viewBuildID": viewBuildID,
+				"navigateTo": pushNotificationBody.navigateTo,
+				"viewBuildID": pushNotificationBody.viewBuildID,
+				"date": Date.now.description
+			]
+		]
+		let httpBody = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+		
+		return httpBody
+	}
+	
+	internal func configureHTTPRequest(url: URL) -> URLRequest {
+		var request = URLRequest(url: url)
+		request.httpMethod = HTTPRequestMethod.post.rawValue
+		request.setValue(
+			"application/json",
+			forHTTPHeaderField: "Content-Type"
+		)
+		
+		/// serverKey 는 3번 과정에서 저장해둔 키를 사용합니다.
+		request.setValue(
+			"key=\(Constant.PushNotification.SERVER_KEY)",
+			forHTTPHeaderField: "Authorization"
+		)
+		
+		return request
+	}
+}
+
+
+extension PushNotificationManager {
+	/// Button을 탭할 때, 아래 메소드를 호출합니다.
+	public func sendPushNotification(
+		with message: PushNotificationMessageType,
+		to userInfo: UserInfo
+	) async -> Void {
+		guard let url = URL(string: "https://\(Constant.PushNotification.PUSH_NOTIFICATION_ENDPOINT)") else {
+			print("Error: \(#file)-\(#function): NO URL ENDPOINT FOR PUSH NOTIFICATION")
+			return
+		}
+		
+		let pushNotificationBody = PushNotificationMessageBody(message)
+		
+		/// HTTP Request의 body로 전달할 data를 딕셔너리로 선언한 후, JSON으로 변환합니다.
+		let json: [AnyHashable: Any] = [
+			/// 특정 기기에 알람을 보내기 위해 "to"를 사용합니다.
+			/// 경우에 따라 Topic 등 다른 용도로 활용할 수 있습니다.
+			"to": userInfo.deviceToken,
+			
+			/// 알람의 내용을 구성하는 키-밸류 입니다.
+			"notification": [
+				"title": pushNotificationBody.messageTitle,
+				"body": pushNotificationBody.messageBody,
+				"subtitle": pushNotificationBody.fromUserName,
+				"badge": "1"
+			],
+			
+			/// 알람을 보내며 함께 전달할 데이터를 삽입합니다.
+			"data": [
+				"userName": "CurrentUserID",
+				"sentDeviceToken": currentUserDeviceToken ?? "보낸 이의 디바이스토큰",
+				"sentUserName": userInfo.githubUserName,
+				"sentUserID": userInfo.id,
+				"navigateTo": pushNotificationBody.navigateTo,
+				"viewBuildID": pushNotificationBody.viewBuildID,
 				"date": Date.now.description
 			]
 		]
@@ -90,7 +197,7 @@ final class PushNotificationManager: ObservableObject {
 		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 		
 		/// serverKey 는 3번 과정에서 저장해둔 키를 사용합니다.
-		request.setValue("key=\(serverKey)", forHTTPHeaderField: "Authorization")
+		request.setValue("key=\(Constant.PushNotification.SERVER_KEY)", forHTTPHeaderField: "Authorization")
 		
 		do {
 			guard let httpBody else { return }
@@ -105,14 +212,6 @@ final class PushNotificationManager: ObservableObject {
 			/// POST가 실패했을 경우 에러를 확인할 수 있도록 dump를 호출합니다.
 			dump("DEBUG: PUSH POST FAILED - \(error)")
 		}
-		
-	}
-	
-	// MARK: LIFECYCLE
-	init(
-		currentUserDeviceToken: String?
-	) {
-		self.currentUserDeviceToken = currentUserDeviceToken
 	}
 }
 
@@ -122,8 +221,7 @@ struct GSPushNotification: Codable {
 		userName, sentDeviceToken, sentUserName, sentUserID, navigateTo, viewBuildID, date: String
 	
 	enum CodingKeys: String, CodingKey {
-		case aps
-		case userName, sentDeviceToken, sentUserName, sentUserID, navigateTo, viewBuildID, date
+		case aps, userName, sentDeviceToken, sentUserName, sentUserID, navigateTo, viewBuildID, date
 		
 		case googleCAE = "google.c.a.e"
 		case googleCFid = "google.c.fid"
@@ -143,6 +241,7 @@ struct GSNotificationDetail: Codable {
 	let body, title, subtitle: String
 }
 
+/// TEST할 때 사용하는 뷰
 struct PushNotificationTestView: View {
 	
 	var body: some View {
@@ -152,11 +251,16 @@ struct PushNotificationTestView: View {
 					let instance = PushNotificationManager(currentUserDeviceToken: "H")
 					let valseDevice = Bundle.main.object(forInfoDictionaryKey: "VALSE_DEVICE_TOKEN") as? String ?? ""
 					
-					dump("\(#function), \(instance)")
 //					await instance.sendPushNotification(
-//						with: .knock(title: "knockMessage", body: "Knock 내용", knockID: "jri5guzFI47hLmZjVFJU"),
+//						with: .knock(title: "knockMessage", body: "Knock 내용", fromUser: "", knockID: ""),
 //						to: UserInfo(id: UUID().uuidString, createdDate: .now, githubUserName: "Valselee", githubID: 000, deviceToken: valseDevice, emailTo: "", blockedUserIDs: [""])
 //					)
+					
+					await instance.sendNotification(
+						with: .knock(title: "", body: "", fromUser: "", knockID: ""),
+						to: UserInfo(id: UUID().uuidString, createdDate: .now, githubUserName: "Valselee", githubID: 000, deviceToken: valseDevice, emailTo: "", blockedUserIDs: [""])
+						)
+					
 //					await instance.sendPushNotification(
 //						with: .chat(title: "chatMessage", body: "chat 내용", chatID: "6FBB214E-C5EE-46F6-9670-4DCA5B73AF17"),
 //						to: UserInfo(id: UUID().uuidString, createdDate: .now, githubUserName: "Valselee",
--- GitSpace/Sources/Models/PushNotificationMessageBody.swift
+++ GitSpace/Sources/Models/PushNotificationMessageBody.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+/**
+ Push Notification의 Payload 데이터 구조체.
+ 메세지의 제목, 바디, 발신자, 뷰가 도착해야 할 뷰, 도착하여 그려야 할 데이터의 정보를 담습니다.
+ 
+ - Author: @Valselee
+ */
+struct PushNotificationMessageBody {
+	let messageTitle: String
+	let messageBody: String
+	let fromUserName: String
+	let navigateTo: String
+	let viewBuildID: String
+	
+	init(_ messageType: PushNotificationMessageType) {
+		switch messageType {
+		case let .knock(title, body, fromUser, knockID):
+			messageTitle = title
+			messageBody = body
+			navigateTo = "knock"
+			fromUserName = fromUser
+			viewBuildID = knockID
+		case let .chat(title, body, fromUser, chatID):
+			messageTitle = title
+			messageBody = body
+			navigateTo = "chat"
+			fromUserName = fromUser
+			viewBuildID = chatID
+		}
+	}
+}
