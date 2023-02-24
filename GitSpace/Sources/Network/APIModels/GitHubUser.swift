73b75e591fff074766dc631ae1e486e6dfa50ddf
Taeyoung Won <hexter1994@gmail.com>
2023-02-24 14:56:02 +0900

    [Chore] #245 주석 간격 조정

--- GitSpace/Sources/Network/APIModels/GitHubUser.swift
+++ GitSpace/Sources/Network/APIModels/GitHubUser.swift
@@ -8,17 +8,17 @@
 import Foundation
 
 struct GitHubUser: Identifiable, Codable {
-    let id : Int       // 고유 id
-    let login: String   // github id
-    let name : String?  // username
-    let email : String? // email
-    let avatar_url: String // profile image
-    let bio: String? // bio, intro message
-    let company: String? // company
-    let location: String? // location
-    let blog: String? // blog url
-    let public_repos: Int // public repos
-    let followers: Int // followers
-    let following: Int // following
+    let id : Int            // 고유 id
+    let login: String       // github id
+    let name : String?      // username
+    let email : String?     // email
+    let avatar_url: String  // profile image
+    let bio: String?        // bio, intro message
+    let company: String?    // company
+    let location: String?   // location
+    let blog: String?       // blog url
+    let public_repos: Int   // public repos
+    let followers: Int      // followers
+    let following: Int      // following
 }
 
