d55438e8da94b11a902e05c0f185db7663a1f362
Taeyoung Won <hexter1994@gmail.com>
2023-02-24 14:57:02 +0900

    [Chore] #245 테스트용 출력문 삭제

--- GitSpace/Utilities/Extensions/Image+.swift
+++ GitSpace/Utilities/Extensions/Image+.swift
@@ -19,19 +19,18 @@ extension Image {
             var urlComponents = URLComponents(url: url,
                                               resolvingAgainstBaseURL: false)
         else {
-            print("URL이 유효하지 않음 1")
             return defaultImage
         }
         
         // MEMO: url scheme이 http인 경우 iOS에서 보안성 이슈로 이미지를 요청하지 않기 때문에, scheme을 강제로 https로 세팅
         urlComponents.scheme = "https"
+        
         // URL로 이미지 데이터를 요청 -> UIImage로 변환합니다.
         guard
             let imageURL = urlComponents.url,
             let imageData = try? Data(contentsOf: imageURL),
             let reponseUIImage = UIImage(data: imageData)
         else {
-            print("URL이 유효하지 않음 2")
             return defaultImage
         }
         
@@ -40,7 +39,6 @@ extension Image {
         
         // 서버에서 불러온 이미지를 Image 타입으로 변환하고 return Image에 할당합니다.
         let reponseImage = Image(uiImage: reponseUIImage)
-        print("서버 요청 이미지 반환!")
         return reponseImage
     }
     
@@ -57,12 +55,10 @@ extension Image {
             return image
         }
         
-        
         // 결과 이미지를 반환합니다.
         /// 1. 캐시 이미지가 있는 경우 -> 해당 캐시 이미지를 반환
         /// 2. 캐시 이미지가 없는 경우 -> 서버에 URL을 통해서 이미지를 요청하고 응답 이미지를 반환
         /// 3. 캐시 이미지가 없음 + URL이 유효하지 않음 -> Asset의 기본 플레이스홀더 이미지를 반환
-        print("이미지 반환하기")
         return await requestProfileImage(key: key, defaultImage: returnImage)
     }
 }
