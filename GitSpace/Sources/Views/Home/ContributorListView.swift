25c8aa08cbd956b6c043e2092df99f219d77ae08
valseLee <sollleky72@gmail.com>
2023-03-04 02:00:17 +0900

    [Chore] Merge 중 작업사항 추가

--- GitSpace/Sources/Views/Home/ContributorListView.swift
+++ GitSpace/Sources/Views/Home/ContributorListView.swift
@@ -26,16 +26,6 @@ struct ContributorListView: View {
     var body: some View {
         
         ScrollView {
-            Spacer()
-                .frame(height: 20)
-            
-            GSText.CustomTextView(
-                style: .title2,
-                string: "Who do you want to chat with?"
-			)
-            .padding(.leading, 10)
-            .padding(.bottom, -5)
-            
             // MARK: - 상황별 마스코트 이미지
             /* 노트 시나리오의 시각적 힌트 제공 */
             HStack {
@@ -58,9 +48,6 @@ struct ContributorListView: View {
                 Spacer()
             }
             .padding(.leading, 20)
-            
-            
-            
             
             // MARK: - 컨트리뷰터 명단 스크롤 뷰
             /* 서브 캡션 */
@@ -110,23 +97,12 @@ Please select a User to start chatting with.
             }
             .padding(.leading, 20)
             
-            
-            
             ForEach(contributorManager.contributors) { user in
-                
                 ContributorListCell(targetUser: user)
                     .padding(.horizontal, 20)
             } // ForEach
-            
         } // ScrollView
     } // body
-	
-	// MARK: - LIFECYCLE
-	init(service: GitHubService, repository: Repository, contributorManager: ContributorViewModel) {
-		self.gitHubService = service
-		self.repository = repository
-		self.contributorManager = contributorManager
-	}
 }
 
 //struct ContributorListView_Previews: PreviewProvider {
