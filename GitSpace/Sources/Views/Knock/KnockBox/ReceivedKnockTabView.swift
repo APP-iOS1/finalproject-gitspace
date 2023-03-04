a03eaa6ff07fd60920ce7b48c69463ee6ea9e576
valselee <sollleky72@gmail.com>
2023-03-05 00:46:19 +0900

    [Chore] #258 파일 이름과 경로 수정

---
+++ GitSpace/Sources/Views/Knock/KnockBox/ReceivedKnockTabView.swift
@@ -0,0 +1,20 @@
+//
+//  ReceivedKnockTabView.swift
+//  GitSpace
+//
+//  Created by Celan on 2023/03/04.
+//
+
+import SwiftUI
+
+struct ReceivedKnockTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReceivedKnockTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceivedKnockTabView()
+    }
+}
---
+++ GitSpace/Sources/Views/Knock/KnockBox/SentKnockTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SentKnockTabView.swift
+//  GitSpace
+//
+//  Created by Celan on 2023/03/04.
+//
+
+import SwiftUI
+
+struct SentKnockTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SentKnockTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SentKnockTabView()
+    }
+}
