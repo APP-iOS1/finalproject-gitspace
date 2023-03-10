b88934ec9f147fb9ba59b777c5c6090f5e94d257
Yeseul Chung <amy7806@gmail.com>
2023-03-11 01:28:26 +0900

    [Feat] #281 SignInState에 pending을 추가함으로써 실제 로그인 진행시에만 로딩뷰를 띄울 수 있도록 코드 추가

--- GitSpace/Sources/ViewModels/GitHubAuthManager.swift
+++ GitSpace/Sources/ViewModels/GitHubAuthManager.swift
@@ -36,6 +36,7 @@ final class GitHubAuthManager: ObservableObject {
     
     enum SignInState {
         case signedIn
+        case pending
         case signedOut
     }
     
@@ -72,14 +73,20 @@ final class GitHubAuthManager: ObservableObject {
             }
             
             if let credential {
+                self.state = .pending
                 self.authentification.signIn(with: credential) { authResult, error in
+                    
                     if error != nil {
                         print("SignIn Error: \(String(describing: error?.localizedDescription))")
                     }
                     // User is signed in.
-                    guard let oauthCredential = authResult?.credential as? OAuthCredential else { return }
-                    guard KeyChainManager.create(authCredential: credential) == true else { return }
-                    print(KeyChainManager.read())
+                    guard let oauthCredential = authResult?.credential as? OAuthCredential else {
+                        return
+                        
+                    }
+                    // 아래 guard문을 삭제하지 않으면 로그인 시 무한 로딩에 걸림.
+//                    guard KeyChainManager.create(authCredential: credential) == true else { return }
+//                    print(KeyChainManager.read())
                     
                     // Github 사용자 데이터(name, email)을 가져오기 위해서 GitHub REST API request가 필요하다.
                     guard let githubAuthenticatedUserURL = URL(string: "https://api.github.com/user") else {
