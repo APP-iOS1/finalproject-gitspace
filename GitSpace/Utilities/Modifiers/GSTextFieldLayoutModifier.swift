8626240dc2d5912779d8d97b75c7c4bdce5f6927
Taeyoung Won <hexter1994@gmail.com>
2023-03-13 04:31:11 +0900

    [Chore] #287 colorScheme 의존 삭제, Asset의 다크모드 대응 컬러 적용

--- GitSpace/Utilities/Modifiers/GSTextFieldLayoutModifier.swift
+++ GitSpace/Utilities/Modifiers/GSTextFieldLayoutModifier.swift
@@ -22,9 +22,7 @@ struct GSTextFieldLayoutModifier: ViewModifier {
                                     bottom: const.SEARCHBAR_INSET_VERTICAL,
                                     trailing: const.SEARCHBAR_INSET_HORIZONTAL))
                 // FIXME: gsGray3 추가 후 변경 필요
-                .background(colorScheme == .light
-                            ? Color.gsGray3
-                            : Color.primary)
+                .background(Color.gsGray3)
                 .cornerRadius(const.SEARCHBAR_CORNER_RADIUS)
             
         case .addTagField:
@@ -35,9 +33,7 @@ struct GSTextFieldLayoutModifier: ViewModifier {
                                     bottom: const.ADDTAG_INSET_VERTICAL,
                                     trailing: const.ADDTAG_INSET_HORIZONTAL))
             // FIXME: gsGray3 추가 후 변경 필요
-                .background(colorScheme == .light
-                            ? Color.gsLightGray2
-                            : .gsLightGray1)
+                .background(Color.gsGray3)
                 .cornerRadius(const.ADDTAG_CORNER_RADIUS)
         }
         
