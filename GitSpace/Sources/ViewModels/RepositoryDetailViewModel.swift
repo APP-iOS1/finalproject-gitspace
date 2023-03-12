f4db215d3ba18a9c61dbe2a5a850e37387421c69
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-03-12 18:29:33 +0900

    [Add] #268 readme 정보를 요청하는 메서드 추가

--- GitSpace/Sources/ViewModels/RepositoryDetailViewModel.swift
+++ GitSpace/Sources/ViewModels/RepositoryDetailViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+final class RepositoryDetailViewModel: ObservableObject {
+    
+    private let service: GitHubService
+    
+    init(service: GitHubService) {
+        self.service = service
+    }
+    
+    @MainActor
+    func requestReadMe(repository: Repository) async -> String {
+        let readMeResult = await service.requestRepositoryReadme(owner: repository.owner.login, repositoryName: repository.name)
+        
+        switch readMeResult {
+            
+        case .success(let response):
+            guard let content = Data(base64Encoded: response.content, options: .ignoreUnknownCharacters) else {
+                return GitHubAPIError.failToLoadREADME.errorDescription
+            }
+            
+            guard let decodeContent = String(data: content, encoding: .utf8) else {
+                return GitHubAPIError.failToLoadREADME.errorDescription
+            }
+            
+            let htmlResult = await service.requestMarkdownToHTML(content: decodeContent)
+            
+            switch htmlResult {
+            case .success(let result):
+                return result
+            case .failure(let error):
+                return error.errorDescription
+            }
+            
+        case .failure(let error):
+            return error.errorDescription
+        }
+    }
+    
+    
+}
