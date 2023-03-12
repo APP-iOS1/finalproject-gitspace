0c28fd5ce1eaa5af36feccf1760ca77f919462db
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-03-12 18:32:23 +0900

    [Add] #268 유저정보를 불러오는 뷰에 사용될 UserProfileViewModel 추가

--- GitSpace/Sources/ViewModels/UserProfileViewModel.swift
+++ GitSpace/Sources/ViewModels/UserProfileViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+final class UserProfileViewModel: ObservableObject {
+    
+    private let service: GitHubService
+    
+    init(service: GitHubService) {
+        self.service = service
+    }
+    
+    @MainActor
+    func requestUserReadMe(user: GithubUser) async -> Result<String, GitHubAPIError> {
+        let result = await service.requestRepositoryReadme(owner: user.login, repositoryName: user.login)
+        
+        switch result {
+            
+        case .success(let readme):
+            guard let content = Data(base64Encoded: readme.content, options: .ignoreUnknownCharacters) else {
+                return .failure(.failToDecode)
+            }
+            
+            guard let decodeContent = String(data: content, encoding: .utf8) else {
+                return .failure(.failToDecode)
+            }
+            
+            let htmlResult = await service.requestMarkdownToHTML(content: decodeContent)
+            
+            switch htmlResult {
+                
+            case .success(let result):
+                return .success(result)
+            // markdown을 html로 변환 실패
+            case .failure:
+                return .failure(.failToDecode)
+            }
+            
+            // repository의 markdown을 요청 실패
+        case .failure:
+            return .failure(.failToLoadREADME)
+        }
+    }
+    
+}
