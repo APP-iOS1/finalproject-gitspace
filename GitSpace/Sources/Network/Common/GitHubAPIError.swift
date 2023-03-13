0e92e1ed02f51c463a147ce55ac601a3bec38fd0
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-03-13 15:47:34 +0900

    [Add] #268 GitHub API를 통해 통신할 때 발견되는 에러 케이스 추가

--- GitSpace/Sources/Network/Common/GitHubAPIError.swift
+++ GitSpace/Sources/Network/Common/GitHubAPIError.swift
@@ -11,7 +11,8 @@ enum GitHubAPIError: Error {
     /// URLComponents를 통해 URL 생성에 실패한 경우 보여줄 에러 메시지
     case invalidURL
     case invalidResponse
-    case failToDecode
+    case failToDecoding
+    case failToEncoding
     case failToLoadREADME
     case unauthorized
     case unexpectedStatusCode
@@ -19,13 +20,15 @@ enum GitHubAPIError: Error {
     case requiresAuthentification
     case forbidden
     case notFound
+    case failToRequest
     case unknown
     
     var errorDescription: String {
         switch self {
             case .invalidURL: return "URL was invalid, Please try again."
             case .invalidResponse: return "Invalid response from the server. Please try again."
-            case .failToDecode: return "Fail to decode data. Please try again"
+            case .failToDecoding: return "Fail to decode data. Please try again"
+            case .failToEncoding: return "Fail to encode data. Please try again"
             case .unauthorized: return "Authorization Failed. Please try again"
             case .unexpectedStatusCode: return "Unexpected Error occured. Please try again"
             case .notModified: return "No data modification has been made. Please try again"
@@ -33,6 +36,7 @@ enum GitHubAPIError: Error {
             case .forbidden: return "This is an invalid permission. Please Try Again"
             case .notFound: return "Resources are not found. Please Try Again"
             case .failToLoadREADME: return "Fail to load README.md from Server. Please Try Again"
+            case .failToRequest: return "Fail to Request Network. Please Try Again"
             default: return "Unknown Error. Please try Again"
         }
     }
