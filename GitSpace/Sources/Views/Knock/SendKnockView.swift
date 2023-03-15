467d99d16460f5852b0c62d13bbbc7f0554bc7b2
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-03-16 00:42:31 +0900

    [Chore] #271 TargetUserProfileView의 Initializer 수정에 따른 코드 변경

--- GitSpace/Sources/Views/Knock/SendKnockView.swift
+++ GitSpace/Sources/Views/Knock/SendKnockView.swift
@@ -393,7 +393,7 @@ struct SendKnockView: View {
             ToolbarItemGroup(placement: .principal) {
                 NavigationLink {
                     if let sendKnockToGitHubUser {
-                        TargetUserProfileView(service: GitHubService(), user: sendKnockToGitHubUser)
+                        TargetUserProfileView(user: sendKnockToGitHubUser)
                     } else {
                         GSText.CustomTextView(style: .body1, string: "Cannot Find User")
                     }
