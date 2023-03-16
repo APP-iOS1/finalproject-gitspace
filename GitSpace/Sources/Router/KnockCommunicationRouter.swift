b757729ec95d103c5be0a3db78c8e4b17415e4a1
valselee <sollleky72@gmail.com>
2023-03-16 20:44:21 +0900

    [Add] #298 노크 소통 현황에 따라 각각의 뷰로 라우팅 할 수 있는 뷰 파일 생성

---
+++ GitSpace/Sources/Router/KnockCommunicationRouter.swift
@@ -0,0 +1,20 @@
+//
+//  KnockCommunicationRouter.swift
+//  GitSpace
+//
+//  Created by Celan on 2023/03/16.
+//
+
+import SwiftUI
+
+struct KnockCommunicationRouter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KnockCommunicationRouter_Previews: PreviewProvider {
+    static var previews: some View {
+        KnockCommunicationRouter()
+    }
+}
