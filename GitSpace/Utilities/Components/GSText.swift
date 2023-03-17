1cc89da7fc8c937a181562eb7e2b583c5f39f316
Yeseul Chung <amy7806@gmail.com>
2023-03-17 23:47:59 +0900

    [Chore] #295 GSText에서 title5를 ButtonTitle1으로 변경.

--- GitSpace/Utilities/Components/GSText.swift
+++ GitSpace/Utilities/Components/GSText.swift
@@ -24,7 +24,6 @@ public struct GSText {
         case title2
         case title3
         case title4
-        case title5
         case sectionTitle
         case body1
         case body2
@@ -34,6 +33,7 @@ public struct GSText {
         case captionPrimary2
         case description
         case description2
+        case buttonTitle1
     }
     
     struct CustomTextView: View {
@@ -66,11 +66,6 @@ public struct GSText {
                     .font(.system(size: Constant.GSTextConst.TITLE4_SIZE))
                     .fontWeight(Constant.GSTextConst.TITLE4_FONT_WEIGHT)
                     .foregroundColor(Constant.GSTextConst.TITLE4_COLOR)
-            case .title5:
-                Text(.init(string))
-                    .font(.system(size: Constant.GSTextConst.TITLE5_SIZE))
-                    .fontWeight(Constant.GSTextConst.TITLE5_FONT_WEIGHT)
-                    .foregroundColor(Constant.GSTextConst.TITLE5_COLOR)
             case .sectionTitle:
                 Text(.init(string))
                     .font(.system(size: Constant.GSTextConst.SECTION_TITLE_SIZE))
@@ -121,6 +116,11 @@ public struct GSText {
                     .font(.system(size: Constant.GSTextConst.DESCRIPTION2_SIZE))
                     .fontWeight(Constant.GSTextConst.DESCRIPTION2_FONT_WEIGHT)
                     .foregroundColor(Constant.GSTextConst.DESCRIPTION2_COLOR)
+            case .buttonTitle1:
+                Text(.init(string))
+                    .font(.system(size: Constant.GSTextConst.BUTTON_TITLE1_SIZE))
+                    .fontWeight(Constant.GSTextConst.BUTTON_TITLE1_FONT_WEIGHT)
+                    .foregroundColor(Constant.GSTextConst.BUTTON_TITLE1_COLOR)
             }
         }
         
