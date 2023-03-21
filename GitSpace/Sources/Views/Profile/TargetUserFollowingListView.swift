7bbd79426fa621feee7497993aecfbb6b8a58f4a
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-22 01:03:43 +0900

    [Add] #327 TargetUserFollowerListView 추가
    # TargetUserFollowerListView 추가

--- GitSpace/Sources/Views/Profile/TargetUserFollowingListView.swift
+++ GitSpace/Sources/Views/Profile/TargetUserFollowingListView.swift
@@ -7,35 +7,133 @@
 
 import SwiftUI
 
-struct TargetUserFollowingListView: View {
+struct TargetUserFollowerListView: View {
     
-    @StateObject var followingViewModel = FollowingViewModel(service: GitHubService())
-    
+    @StateObject var followerViewModel = FollowerViewModel(service: GitHubService())
     let gitHubService: GitHubService
-    let targetUser: GithubUser
+    let targetUserLogin: String
     
-    init(service: GitHubService, targetUser: GithubUser) {
+    init(service: GitHubService, targetUserLogin: String) {
         self.gitHubService = service
-        self.targetUser = targetUser
+        self.targetUserLogin = targetUserLogin
     }
     
     var body: some View {
-        ScrollView() {
+        ScrollView {
+            VStack {
+                ForEach(followerViewModel.followers) { user in
+                    
+                    NavigationLink(destination: TargetUserProfileView(user: user)) {
+                        HStack(spacing: 20) {
+                            GithubProfileImage(urlStr: user.avatar_url, size: 50)
+                            
+                            VStack(alignment: .leading, spacing: 3) {
+                                /* 유저네임 */
+                                GSText.CustomTextView(
+                                    style: .title3,
+                                    string: user.name ?? user.login)
+                                
+                                /* 유저ID */
+                                GSText.CustomTextView(
+                                    style: .sectionTitle,
+                                    string: user.login)
+                            } // VStack
+                            .multilineTextAlignment(.leading)
+                            
+                            Spacer()
+                        } // HStack
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    
+                    Divider()
+                }
+            } // VStack
+        }
+        .navigationBarTitle(targetUserLogin, displayMode: .inline)
+        .task {
+            requestTargetUserFollowerList()
+        }
+        .refreshable {
+            requestTargetUserFollowerList()
+        }
+    } // body
+    
+    private func requestTargetUserFollowerList() -> Void {
+        followerViewModel.followers.removeAll()
+        followerViewModel.temporaryFollowers.removeAll()
+        
+        Task {
+            let followerListResult = await followerViewModel.requestFollowers(user: targetUserLogin, perPage: 100, page: 1)
+            
+            switch followerListResult {
+            case .success():
+                followerViewModel.followers = followerViewModel.temporaryFollowers
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+struct TargetUserFollowingListView: View {
+    
+    @StateObject var followingViewModel = FollowingViewModel(service: GitHubService())
+    let gitHubService: GitHubService
+    let targetUserLogin: String
+    
+    init(service: GitHubService, targetUserLogin: String) {
+        self.gitHubService = service
+        self.targetUserLogin = targetUserLogin
+    }
+    
+    var body: some View {
+        ScrollView {
             VStack {
                 ForEach(followingViewModel.followings) { user in
                     
                     NavigationLink(destination: TargetUserProfileView(user: user)) {
-                        GithubProfileImage(urlStr: user.avatar_url, size: 40)
+                        HStack(spacing: 20) {
+                            GithubProfileImage(urlStr: user.avatar_url, size: 50)
+                            
+                            VStack(alignment: .leading, spacing: 3) {
+                                /* 유저네임 */
+                                GSText.CustomTextView(
+                                    style: .title3,
+                                    string: user.name ?? user.login)
+                                
+                                /* 유저ID */
+                                GSText.CustomTextView(
+                                    style: .sectionTitle,
+                                    string: user.login)
+                            } // VStack
+                            .multilineTextAlignment(.leading)
+                            
+                            Spacer()
+                        } // HStack
                     }
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                    
+                    Divider()
                 }
-            }
+            } // VStack
         }
-        .navigationBarTitle("Following", displayMode: .inline)
+        .navigationBarTitle(targetUserLogin, displayMode: .inline)
         .task {
-            followingViewModel.followings.removeAll()
-            followingViewModel.temporaryFollowings.removeAll()
-            
-            let followingListResult = await followingViewModel.requestFollowings(user: targetUser, perPage: 30, page: 1)
+            requestTargetUserFollowingList()
+        }
+        .refreshable {
+            requestTargetUserFollowingList()
+        }
+    } // body
+    
+    private func requestTargetUserFollowingList() -> Void {
+        followingViewModel.followings.removeAll()
+        followingViewModel.temporaryFollowings.removeAll()
+        
+        Task {
+            let followingListResult = await followingViewModel.requestFollowings(user: targetUserLogin, perPage: 100, page: 1)
             
             switch followingListResult {
             case .success():
@@ -49,6 +147,6 @@ struct TargetUserFollowingListView: View {
 
 struct TargetUserFollowingListView_Previews: PreviewProvider {
     static var previews: some View {
-        TargetUserFollowingListView(service: GitHubService(), targetUser: GithubUser(id: 123, login: "alexandrethsilva", name: "Alexandre Theodoro da Silva helaksdkfjslekfkfkfkllllllllkkkk", email: "asdf@mnawe.com", avatar_url: "asdf", bio: "asdf", company: "asdf", location: "asdf", blog: "asdf", public_repos: 123, followers: 123, following: 123))
+        TargetUserFollowingListView(service: GitHubService(), targetUserLogin: "guguhanogu")
     }
 }
