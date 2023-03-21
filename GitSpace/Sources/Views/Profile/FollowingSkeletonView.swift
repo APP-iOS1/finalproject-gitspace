66a2944760fc615e9e192d60e8bb9b01075d75b8
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-22 00:54:51 +0900

    [Add] #327 FollowingSkeletonView 추가
    # FollowingSkeletonView 추가

---
+++ GitSpace/Sources/Views/Profile/FollowingSkeletonView.swift
@@ -0,0 +1,20 @@
+//
+//  FollowingSkeletonView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/03/22.
+//
+
+import SwiftUI
+
+struct FollowingSkeletonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FollowingSkeletonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowingSkeletonView()
+    }
+}
