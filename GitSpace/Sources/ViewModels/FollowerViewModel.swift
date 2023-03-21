9364c68ba1c305d2b31aeae0a3a9c4b411daf47a
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-21 22:22:41 +0900

    [Feat] #327 FollowerViewModel > FollowingViewModel 클래스 추가 및 구현
    # FollowerViewModel > FollowingViewModel 클래스 추가 및 구현

--- GitSpace/Sources/ViewModels/FollowerViewModel.swift
+++ GitSpace/Sources/ViewModels/FollowerViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 final class FollowerViewModel: ObservableObject {
     @Published var responses: [FollowerResponse] = []
@@ -21,6 +22,51 @@ final class FollowerViewModel: ObservableObject {
             case .failure(let error):
                 print(error)
             }
+        }
+    }
+}
+
+final class FollowingViewModel: ObservableObject {
+    
+    private let service: GitHubService
+    
+    init(service: GitHubService) {
+        self.service = service
+    }
+    
+    @Published var followings: [GithubUser] = []
+    @Published var isLoading: Bool = true
+    var temporaryFollowings: [GithubUser] = []
+    
+    public func getFollowing(with userID: Int) -> GithubUser? {
+        let result = followings.filter { $0.id == userID }.first
+        print(#file, #function, result?.name ?? "FAILED: getFollowing")
+        return result
+    }
+    
+    @MainActor
+    func requestFollowings(user: GithubUser, perPage: Int, page: Int) async -> Result<Void, GitHubAPIError> {
+        
+        let followingsResult = await service.requestUserFollowerList(userName: user.login, perPage: perPage, page: page)
+        
+        switch followingsResult {
+        case .success(let users):
+            for user in users {
+                let result = await service.requestUserInformation(userName: user.login)
+                
+                switch result {
+                case .success(let user):
+                    temporaryFollowings.append(user)
+                case .failure(let error):
+                    return .failure(error)
+                }
+            }
+            withAnimation(.easeInOut) {
+                self.isLoading = false
+            }
+            return .success(())
+        case .failure(let error):
+            return .failure(error)
         }
     }
 }
