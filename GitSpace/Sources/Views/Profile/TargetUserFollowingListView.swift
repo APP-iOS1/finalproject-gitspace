a770bf574a7f0958882672043ba8fb33f7179fa8
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-03-22 23:52:40 +0900

    [Feat] #327 TargetUserFollowing/FollowerListView 무한 스크롤 적용
    # TargetUserFollowing/FollowerListView 무한 스크롤 적용

--- GitSpace/Sources/Views/Profile/TargetUserFollowingListView.swift
+++ GitSpace/Sources/Views/Profile/TargetUserFollowingListView.swift
@@ -21,69 +21,93 @@ struct TargetUserFollowerListView: View {
     }
     
     @State var isLoading: Bool = true
+    @State var perPage: Int = 20
+    @State var currentPage: Int = 1
     
     var body: some View {
         ScrollView {
-            if !isLoading {
-                VStack {
-                    ForEach(followerViewModel.followers) { user in
-                        
-                        NavigationLink(destination: TargetUserProfileView(user: user)) {
-                            HStack(spacing: 20) {
-                                GithubProfileImage(urlStr: user.avatar_url, size: 50)
-                                
-                                VStack(alignment: .leading, spacing: 3) {
-                                    /* 유저네임 */
-                                    GSText.CustomTextView(
-                                        style: .title3,
-                                        string: user.name ?? user.login)
-                                    
-                                    /* 유저ID */
-                                    GSText.CustomTextView(
-                                        style: .caption1,
-                                        string: user.login)
-                                    
-                                    /* 유저 Bio */
-                                    if user.bio != nil {
-                                        Text("\(user.bio!)")
-                                            .font(.system(size: 12))
-                                            .foregroundColor(.primary)
-                                    }
-                                } // VStack
-                                .multilineTextAlignment(.leading)
-                                
-                                Spacer()
-                            } // HStack
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
-                        
-                        Divider()
-                    }
-                } // VStack
-            } else {
+            
+            if isLoading && currentPage == 1 {
                 FollowingSkeletonView()
-            }
-        }
+            } else {
+                VStack {
+                    ForEach(Array(zip(followerViewModel.followers.indices, followerViewModel.followers)), id: \.0) { index, user in
+                        
+                        LazyVStack {
+                            NavigationLink(destination: TargetUserProfileView(user: user)) {
+                                HStack(spacing: 20) {
+                                    
+                                    VStack {
+                                        GithubProfileImage(urlStr: user.avatar_url, size: 50)
+                                        Spacer()
+                                    } // VStack
+                                    
+                                    VStack(alignment: .leading, spacing: 3) {
+                                        /* 유저네임 */
+                                        GSText.CustomTextView(
+                                            style: .title3,
+                                            string: user.name ?? user.login)
+                                        
+                                        /* 유저ID */
+                                        GSText.CustomTextView(
+                                            style: .caption1,
+                                            string: user.login)
+                                        
+                                        /* 유저 Bio */
+                                        if user.bio != nil {
+                                            Text("\(user.bio!)")
+                                                .font(.system(size: 12))
+                                                .foregroundColor(.primary)
+                                        }
+                                    } // VStack
+                                    .multilineTextAlignment(.leading)
+                                    
+                                    Spacer()
+                                } // HStack
+                            } // NavigationLink
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .onAppear {
+                                // Paging Logic
+                                if index % 20 == 15 {
+                                    currentPage += 1
+                                    requestTargetUserFollowerList()
+                                }
+                            } // onAppear
+                            
+                            Divider()
+                        } // LazyVStack
+                    } // ForEach
+                    
+                    if isLoading && currentPage > 1 {
+                        ProgressView()
+                    }
+                    
+                } // VStack
+            } // if-else
+        } // ScrollView
         .navigationBarTitle("\(followers) followers", displayMode: .inline)
         .task {
             requestTargetUserFollowerList()
         }
         .refreshable {
+            // 값 초기화
+            followerViewModel.followers.removeAll()
+            followerViewModel.temporaryFollowers.removeAll()
+            currentPage = 1
+            
             requestTargetUserFollowerList()
         }
     } // body
     
     private func requestTargetUserFollowerList() -> Void {
-        followerViewModel.followers.removeAll()
-        followerViewModel.temporaryFollowers.removeAll()
         
         withAnimation(.easeInOut) {
             isLoading = true
         }
         
         Task {
-            let followerListResult = await followerViewModel.requestFollowers(user: targetUserLogin, perPage: 100, page: 1)
+            let followerListResult = await followerViewModel.requestFollowers(user: targetUserLogin, perPage: perPage, page: currentPage)
             
             switch followerListResult {
             case .success():
@@ -112,69 +136,92 @@ struct TargetUserFollowingListView: View {
     }
     
     @State var isLoading: Bool = true
+    @State var perPage: Int = 20
+    @State var currentPage: Int = 1
     
     var body: some View {
         ScrollView {
-            if !isLoading {
-                VStack {
-                    ForEach(followingViewModel.followings) { user in
-                        
-                        NavigationLink(destination: TargetUserProfileView(user: user)) {
-                            HStack(spacing: 20) {
-                                GithubProfileImage(urlStr: user.avatar_url, size: 50)
-                                
-                                VStack(alignment: .leading, spacing: 3) {
-                                    /* 유저네임 */
-                                    GSText.CustomTextView(
-                                        style: .title3,
-                                        string: user.name ?? user.login)
-                                    
-                                    /* 유저ID */
-                                    GSText.CustomTextView(
-                                        style: .caption1,
-                                        string: user.login)
-                                    
-                                    /* 유저 Bio */
-                                    if user.bio != nil {
-                                        Text("\(user.bio!)")
-                                            .font(.system(size: 12))
-                                            .foregroundColor(.primary)
-                                    }
-                                } // VStack
-                                .multilineTextAlignment(.leading)
-                                
-                                Spacer()
-                            } // HStack
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
-                        
-                        Divider()
-                    }
-                } // VStack
-            } else {
+            if isLoading && currentPage == 1 {
                 FollowingSkeletonView()
-            }
-        }
+            } else {
+                VStack {
+                    ForEach(Array(zip(followingViewModel.followings.indices, followingViewModel.followings)), id: \.0) { index, user in
+                        
+                        LazyVStack {
+                            NavigationLink(destination: TargetUserProfileView(user: user)) {
+                                HStack(spacing: 20) {
+                                    
+                                    VStack {
+                                        GithubProfileImage(urlStr: user.avatar_url, size: 50)
+                                        Spacer()
+                                    } // VStack
+                                    
+                                    VStack(alignment: .leading, spacing: 3) {
+                                        /* 유저네임 */
+                                        GSText.CustomTextView(
+                                            style: .title3,
+                                            string: user.name ?? user.login)
+                                        
+                                        /* 유저ID */
+                                        GSText.CustomTextView(
+                                            style: .caption1,
+                                            string: user.login)
+                                        
+                                        /* 유저 Bio */
+                                        if user.bio != nil {
+                                            Text("\(user.bio!)")
+                                                .font(.system(size: 12))
+                                                .foregroundColor(.primary)
+                                        }
+                                    } // VStack
+                                    .multilineTextAlignment(.leading)
+                                    
+                                    Spacer()
+                                } // HStack
+                            } // NavigationLink
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .onAppear {
+                                // Paging Logic
+                                if index % 20 == 15 {
+                                    currentPage += 1
+                                    requestTargetUserFollowingList()
+                                }
+                            } // onAppear
+                            
+                            Divider()
+                        } // LazyVStack
+                    } // ForEach
+                    
+                    if isLoading && currentPage > 1 {
+                        ProgressView()
+                    }
+                    
+                } // VStack
+            } // if-else
+        } // ScrollView
         .navigationBarTitle("\(following) following", displayMode: .inline)
         .task {
             requestTargetUserFollowingList()
         }
         .refreshable {
+            // 값 초기화
+            followingViewModel.followings.removeAll()
+            followingViewModel.temporaryFollowings.removeAll()
+            currentPage = 1
+            
             requestTargetUserFollowingList()
         }
     } // body
     
     private func requestTargetUserFollowingList() -> Void {
-        followingViewModel.followings.removeAll()
-        followingViewModel.temporaryFollowings.removeAll()
         
         withAnimation(.easeInOut) {
             isLoading = true
         }
         
         Task {
-            let followingListResult = await followingViewModel.requestFollowings(user: targetUserLogin, perPage: 100, page: 1)
+            let followingListResult = await followingViewModel.requestFollowings(user: targetUserLogin, perPage: perPage, page: currentPage)
             
             switch followingListResult {
             case .success():
