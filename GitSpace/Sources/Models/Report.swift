71d1ea002df0da5569a061df59dc1777d59484ee
valselee <sollleky72@gmail.com>
2023-04-17 00:32:55 +0900

    [Feat] #365 Report 구조체 구현, 내부 열거형 타입으로 휴먼에러 최소화

--- GitSpace/Sources/Models/Report.swift
+++ GitSpace/Sources/Models/Report.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct Report: Codable {
+    var id: String = UUID().uuidString
+    var reason: String
+    var reporterID: String
+    var targetUserID: String
+    var date: Timestamp
+    // Knock 메시지, Chat 메시지 등등
+    var content: String?
+    var type: String
+    
+    enum ReportType: String {
+        case user = "User"
+        case chat = "Chat"
+        case knock = "Knock"
+    }
+    
+    enum ReportReason: String {
+        case spamming = "Spamming"
+        case offensive = "Verbal Abuse, Offensive Language"
+        case sexual = "Sexual Description(Activity)"
+        case cheating = "Cheating"
+        case bullying = "Cyberbullying or Harassment"
+    }
+}
