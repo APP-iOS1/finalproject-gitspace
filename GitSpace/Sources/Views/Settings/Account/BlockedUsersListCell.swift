de14356ad9fb851b4443d2cf9896f2c14026e5c0
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-20 21:56:55 +0900

    [Add] #368 BlockedUsersListCell 생성
    # BlockedUsersListCell 생성

---
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListCell.swift
@@ -0,0 +1,20 @@
+//
+//  BlockedUsersListCell.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct BlockedUsersListCell: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BlockedUsersListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockedUsersListCell()
+    }
+}
