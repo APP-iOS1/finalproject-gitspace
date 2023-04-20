c78013cd8a7c2eb767e41f1aa15309e273a9f3fe
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-20 21:51:38 +0900

    [Add] #368 BlockedUsersListView 생성
    # BlockedUsersListView 생성

---
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListView.swift
@@ -0,0 +1,23 @@
+//
+//  BlockedUsersListView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct BlockedUsersListView: View {
+    var body: some View {
+        
+        ScrollView {
+            
+        }
+    }
+}
+
+struct BlockedUsersListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockedUsersListView()
+    }
+}
