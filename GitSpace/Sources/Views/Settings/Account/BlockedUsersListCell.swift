cb9925eb1ac70cf41f96f24101e224f330295af0
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-20 23:37:52 +0900

    [Feat] #368 BlockedUsersListCell 구현
    # BlockedUsersListCell 구현

--- GitSpace/Sources/Views/Settings/Account/BlockedUsersListCell.swift
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListCell.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct BlockedUsersListCell: View {
+    
+    let targetUser: GithubUser
+    
     var body: some View {
-        Text("Hello, World!")
+        GSCanvas.CustomCanvasView.init(style: .primary, content: {
+            HStack(spacing: 15) {
+                
+                NavigationLink(destination: TargetUserProfileView(user: targetUser)) {
+                    /* 유저 프로필 이미지 */
+                    GithubProfileImage(urlStr: targetUser.avatar_url, size: 40)
+                }
+                
+                VStack(alignment: .leading) {
+                    /* 유저네임 */
+                    GSText.CustomTextView(
+                        style: .title3,
+                        string: targetUser.name ?? targetUser.login)
+                    
+                    /* 유저ID */
+                    GSText.CustomTextView(
+                        style: .sectionTitle,
+                        string: targetUser.login)
+                } // VStack
+                .multilineTextAlignment(.leading)
+                
+                Spacer()
+                
+                
+                GSButton.CustomButtonView(
+                    style: .tag(
+                        isAppliedInView: true
+                    )
+                ) {
+                    
+                } label: {
+                    Text("**Unblock**")
+                }
+            } // HStack
+        }) // GSCanvas
     }
 }
 
 struct BlockedUsersListCell_Previews: PreviewProvider {
+    
     static var previews: some View {
-        BlockedUsersListCell()
+        BlockedUsersListCell(targetUser: GithubUser(id: 123, login: "guguhanogu", name: "HanHo Choi", email: "", avatar_url: "https://avatars.githubusercontent.com/u/64696968?v=4", bio: "", company: "", location: "", blog: "", public_repos: 123, followers: 123, following: 123))
     }
 }
