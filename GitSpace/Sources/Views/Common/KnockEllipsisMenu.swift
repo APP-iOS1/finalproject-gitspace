b01e327ff07fb2b08577d4473b66f17e368fb483
valselee <sollleky72@gmail.com>
2023-04-24 11:23:17 +0900

    [Add] #370 노크를 신고하고 수정할 수 있는 메뉴 구현을 위한 파일 생성

---
+++ GitSpace/Sources/Views/Common/KnockEllipsisMenu.swift
@@ -0,0 +1,20 @@
+//
+//  KnockEllipsisMenu.swift
+//  GitSpace
+//
+//  Created by Celan on 2023/04/24.
+//
+
+import SwiftUI
+
+struct KnockEllipsisMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KnockEllipsisMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        KnockEllipsisMenu()
+    }
+}
