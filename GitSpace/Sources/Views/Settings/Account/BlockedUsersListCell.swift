375ba102f0dbfaf4486abda10f69116019c47fc8
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-25 17:36:14 +0900

    [Feat] #368 차단한 유저의 프로필 사진, 이름을 탭 했을 때 TargetUserProfileView로 연결
    # 차단한 유저의 프로필 사진, 이름을 탭 했을 때 TargetUserProfileView로 연결

--- GitSpace/Sources/Views/Settings/Account/BlockedUsersListCell.swift
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListCell.swift
@@ -13,23 +13,26 @@ struct BlockedUsersListCell: View, Blockable {
         GSCanvas.CustomCanvasView.init(style: .primary, content: {
             HStack(spacing: 15) {
                 
-                NavigationLink(destination: TargetUserProfileView(user: targetUser)) {
-                    /* 유저 프로필 이미지 */
-                    GithubProfileImage(urlStr: targetUser.avatar_url, size: 40)
-                }
-                
-                VStack(alignment: .leading) {
-                    /* 유저네임 */
-                    GSText.CustomTextView(
-                        style: .title3,
-                        string: targetUser.name ?? targetUser.login)
+                NavigationLink(destination: TargetUserProfileView(user: gitHubUser)) {
                     
-                    /* 유저ID */
-                    GSText.CustomTextView(
-                        style: .sectionTitle,
-                        string: targetUser.login)
-                } // VStack
-                .multilineTextAlignment(.leading)
+                    HStack(spacing: 15) {
+                        /* 유저 프로필 이미지 */
+                        GithubProfileImage(urlStr: gitHubUser.avatar_url, size: 40)
+                        
+                        VStack(alignment: .leading) {
+                            /* 유저네임 */
+                            GSText.CustomTextView(
+                                style: .title3,
+                                string: gitHubUser.name ?? gitHubUser.login)
+                            
+                            /* 유저ID */
+                            GSText.CustomTextView(
+                                style: .sectionTitle,
+                                string: gitHubUser.login)
+                        } // VStack
+                        .multilineTextAlignment(.leading)
+                    }
+                }
                 
                 Spacer()
                 
