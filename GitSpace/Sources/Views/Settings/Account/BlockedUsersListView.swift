eecb134fc4ef8ca45c0ba4fe688db5055ab80908
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-25 17:44:15 +0900

    [Feat] #368 콘솔의 차단 유저 리스트에서 유저 id 값을 받아와 GitHubUser와 UserInfo로 변환하고 이를 BlockedUserList 배열에 추가 하는 로직 구현
    # 콘솔의 차단 유저 리스트에서 유저 id 값을 받아와 GitHubUser와 UserInfo로 변환하고 이를 BlockedUserList 배열에 추가 하는 로직 구현

--- GitSpace/Sources/Views/Settings/Account/BlockedUsersListView.swift
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct BlockedUsersListView: View {
     var body: some View {
+    func convertUserInfo() async {
+        
+        withAnimation(.easeInOut) {
+            isLoaded = false
+        }
         
         ScrollView {
+        if let currentUser = await userInfoManager.requestUserInfoWithID(userID: userInfoManager.currentUser?.id ?? "") {
             
+            for someUser in currentUser.blockedUserIDs {
+                if let userInfo = await UserStore.requestAndReturnUser(userID: someUser) {
+                    let gitHubUser = gitHubAuthManager.getGithubUser(FBUser: userInfo)
+                    let blockedUser: (userInfo: UserInfo, gitHubUser: GithubUser) = (userInfo, gitHubUser)
+                    
+                    if !blockedUsers.blockedUserList.contains(where: { $0.userInfo.id == userInfo.id }) {
+                        blockedUsers.blockedUserList.append(blockedUser)
+                    }
+                }
+            }
+        }
+        
+        withAnimation(.easeInOut) {
+            isLoaded = true
         }
     }
 }
