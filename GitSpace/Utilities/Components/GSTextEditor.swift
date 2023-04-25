4ff9b8fb7bb9ed01528f963ab97c3c0e8857083d
Taeyoung Won <hexter1994@gmail.com>
2023-04-25 22:17:31 +0900

    [Chore] #372 FIXME 코멘트 변경

--- GitSpace/Utilities/Components/GSTextEditor.swift
+++ GitSpace/Utilities/Components/GSTextEditor.swift
@@ -228,7 +228,7 @@ struct GSTextEditor {
                                     setTextEditorStartHeight()
                                 }
                                 .onChange(of: text.wrappedValue) { n in
-                                    // FIXME: 현재 버퍼값으로는 텍스트 길이와 에디터 길이 사이의 공식을 정확하게 구하지 못해서 당장 작동은 하지만 정확한 값을 구해서 수정 필요 By. 태영
+                                    // FIXME: 현재 버퍼값으로는 텍스트 길이와 에디터 길이 사이의 공식을 정확하게 구하지 못함 -> 당장 작동은 하지만 정확한 값을 구해서 수정 필요 By. 태영
                                     let textEditorWidth = proxy.size.width - (const.TEXTEDITOR_INSET_HORIZONTAL * 2 + 10)
                                     let autoLineBreakCounter = autoLineBreakCount(textEditorWidth: textEditorWidth)
                                     let multiTextEditorWidth = textEditorWidth - CGFloat(autoLineBreakCounter * 2)
