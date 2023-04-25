4bc49694dee1abe69b4cf763d6a6ff550e4f8f63
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-04-25 17:27:05 +0900

    [Add] #368 BlockedUsersListSkeletonView 추가
    # BlockedUsersListSkeletonView 추가

---
+++ GitSpace/Sources/Views/Settings/Account/BlockedUsersListSkeletonView.swift
@@ -0,0 +1,21 @@
+//
+//  BlockedUsersListSkeletonView.swift
+//  GitSpace
+//
+//  Created by 최한호 on 2023/04/23.
+//
+
+import SwiftUI
+
+struct BlockedUsersListSkeletonView: View {
+    
+    
+    var body: some View {
+    }
+}
+
+struct BlockedUsersListSkeletonView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockedUsersListSkeletonView()
+    }
+}
