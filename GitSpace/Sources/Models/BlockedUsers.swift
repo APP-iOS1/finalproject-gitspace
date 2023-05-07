73c739b176ddbb031d168c6ae082350fcc928cb5
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-05-07 23:57:32 +0900

    [Chore] #412 BlockedUsers Class에 주석을 추가합니다.
    # BlockedUsers Class에 주석을 추가합니다.

--- GitSpace/Sources/Models/BlockedUsers.swift
+++ GitSpace/Sources/Models/BlockedUsers.swift
@@ -7,6 +7,10 @@
 import Foundation
 
 final class BlockedUsers: ObservableObject {
-    
+    /**
+     currentUser의 BlockedUsers를 담아줄 배열입니다. ContentView의 retrieveBlockedUserList 메서드를 통해 currentUser의 BlockedUsers를 blockedUserList에 담아줍니다.
+     - Properties: (UserInfo, GithubUser) 형식의 튜플 타입으로 이루어져 있습니다.
+     - Author: 한호
+     */
     @Published var blockedUserList: [(userInfo: UserInfo, gitHubUser: GithubUser)] = []
 }
