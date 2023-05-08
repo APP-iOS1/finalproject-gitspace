69c1052ca27f50010447ebe70422a1e2b3e55ce0
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-05-08 16:24:12 +0900

    [Feat] #411 BeforeSendKnockSection | 구현
    # BeforeSendKnockSection | 구현
    - 노크를 보내기 전에 보여지는 뷰 섹션입니다.

--- GitSpace/Sources/Views/Knock/SendKnock/BeforeSendKnockSection.swift
+++ GitSpace/Sources/Views/Knock/SendKnock/BeforeSendKnockSection.swift
@@ -9,14 +9,50 @@ import SwiftUI
 
 struct BeforeSendKnockSection: View {
     
+    @State var targetGithubUser: GithubUser?
+    @Binding var showKnockGuide: Bool
     
     var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            
+            if let targetGithubUser {
+                HStack(spacing: 5) {
+                    GSText.CustomTextView(
+                        style: .body1,
+                        string: "It's the first message to")
+                    GSText.CustomTextView(
+                        style: .title3,
+                        string: "\(targetGithubUser.login)!")
+                }
+            }
+            
+            HStack(spacing: 5) {
+                Text("Would you like to")
+                Button {
+                    showKnockGuide.toggle()
+                } label: {
+                    Text("Knock")
+                        .bold()
+                }
+                Text("?")
+                    .padding(.leading, -4)
+            }
+            
+            GSText.CustomTextView(
+                style: .caption1,
+                string:
+"""
+Please select the purpose of the chat so that the other person can understand your intention.
+""")
+        }
+        .multilineTextAlignment(.center)
     }
 }
 
 struct BeforeSendKnockSection_Previews: PreviewProvider {
     static var previews: some View {
         BeforeSendKnockSection(
+            showKnockGuide: .constant(false)
         )
     }
 }
