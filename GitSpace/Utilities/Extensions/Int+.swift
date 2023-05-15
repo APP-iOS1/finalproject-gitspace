cbf762b7c354058e6bdcc75c57c5f8e9729d9c25
Taeyoung Won <hexter1994@gmail.com>
2023-05-15 22:45:24 +0900

    [Add] #421 Int 확장 연산프로퍼티 asFloat 추가

---
+++ GitSpace/Utilities/Extensions/Int+.swift
@@ -0,0 +1,14 @@
+//
+//  Int+.swift
+//  GitSpace
+//
+//  Created by 원태영 on 2023/05/15.
+//
+
+import Foundation
+
+extension Int {
+    var asFloat: CGFloat {
+        return CGFloat(self)
+    }
+}
