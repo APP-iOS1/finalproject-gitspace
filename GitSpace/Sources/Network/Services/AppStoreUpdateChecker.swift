346300e31c9ee53069f58b2a92fd20417ff153c4
Taeyoung Won <hexter1994@gmail.com>
2023-07-03 13:53:43 +0900

    [Feat] #451 isNewVersionAvailable 함수 구현
    현재 클라이언트 버전과 앱 스토어 릴리즈 버전을 비교하는 기능

--- GitSpace/Sources/Network/Services/AppStoreUpdateChecker.swift
+++ GitSpace/Sources/Network/Services/AppStoreUpdateChecker.swift
@@ -21,5 +21,54 @@ enum AppStoreUpdateCheckerError: Error {
 
 final class AppStoreUpdateChecker {
     
+    static func isNewVersionAvailable() async -> Result<Bool, AppStoreUpdateCheckerError> {
+        guard let infoDictionary = Bundle.main.infoDictionary else {
+            return .failure(.invalidInfoDictionary)
+        }
+        
+        guard let bundleID = Bundle.main.bundleIdentifier else {
+            return .failure(.invalidBundleID)
+        }
+        
+        guard var currentVersionNumber = infoDictionary["CFBundleShortVersionString"] as? String else {
+            return .failure(.invalidCurrentVersionNumber)
+        }
+        
+        if currentVersionNumber.components(separatedBy: ".").count <= 2 {
+            currentVersionNumber += ".0"
+        }
+        
+        guard let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(bundleID)") else {
+            return .failure(.invalidItunesURL)
+        }
+        
+        do {
+            let (data, urlResponse) = try await URLSession.shared.data(from: url)
+
+            guard let response = urlResponse as? HTTPURLResponse else {
+                return .failure(.invalidResponse)
+            }
+            
+            guard Array(200...299).contains(response.statusCode) else {
+                return .failure(.unexpectedStatusCode)
+            }
+
+            guard let decodedResponse = try? JSONDecoder().decode(
+                AppInfo.self,
+                from: data
+            ) else {
+                return .failure(.failToDecoding)
+            }
+            
+            guard let latestVersionNumber = decodedResponse.results.first?.version else {
+                return .failure(.emptyResponse)
+            }
+            
+            return .success(currentVersionNumber != latestVersionNumber)
+
+        } catch {
+            return .failure(.unknown)
+        }
+    }
     
 }
