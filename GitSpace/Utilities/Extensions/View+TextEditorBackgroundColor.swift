7aa64b192ed25530a2aef7a649a3dc0bbbdebb5d
valselee <sollleky72@gmail.com>
2023-07-06 11:39:02 +0900

    [Feat] Extension에서 iOS 버전 분기 후, TextEditor의 백그라운드 컬러를 수정하는 로직 구현

--- GitSpace/Utilities/Extensions/View+TextEditorBackgroundColor.swift
+++ GitSpace/Utilities/Extensions/View+TextEditorBackgroundColor.swift
@@ -5,4 +5,16 @@
 //  Created by Celan on 2023/07/06.
 //
 
-import Foundation
+import SwiftUI
+
+public extension View {
+    func textEditorBackgroundClear() -> some View {
+        if #available(iOS 16.0, *) {
+            return scrollContentBackground(.hidden)
+        } else {
+            return onAppear {
+                UITextView.appearance().backgroundColor = .clear
+            }
+        }
+    }
+}
